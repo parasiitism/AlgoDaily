517c3d6bbf284b7e4995f1970637b7c878540ad9
calvinchankf <chan9118kin@gmail.com>
2018-09-14 22:06:49 +0800

    day13  Fast Power Algorithm(Exponentiation by squaring)

--- leetcode/50-powx-n/main.go
+++ leetcode/50-powx-n/main.go
@@ -1,12 +1,49 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 // nonsense
-func myPow(x float64, n int) float64 {
+func myPow0(x float64, n int) float64 {
 	return math.Pow(x, float64(n))
 }
 
-func main() {
+// iterative
+// 2^14
+// n, a, b
+// when n=14, 2^2, 1
+// when n=7, 2^2*2^2, 2^2
+// when n=3, 2^4*2^4, 2^4*2^2
+// return 2^8*2^4*2^2=2^14
+func myPow(x float64, n int) float64 {
+	a := x
+	b := 1.0
+	count := n
+	if n < 0 {
+		count = -1 * count
+	} else if n == 0 {
+		return 1
+	}
+	for count > 1 {
+		if count%2 == 0 {
+			a = a * a
+			count = count / 2
+		} else {
+			b = a * b
+			a = a * a
+			count = (count - 1) / 2
+		}
+	}
+	if n < 0 {
+		return 1 / (a * b)
+	} else {
+		return a * b
+	}
+}
 
+func main() {
+	a := myPow(2, 0)
+	fmt.Println(a)
 }
