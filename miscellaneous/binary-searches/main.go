9903bdd4aabeff767b9dc37975e29ea66c9a6470
calvinchankf <chan9118kin@gmail.com>
2018-09-20 22:17:10 +0800

    post pocessing

--- miscellaneous/binary-searches/main.go
+++ miscellaneous/binary-searches/main.go
@@ -37,7 +37,10 @@ func LowerBoundBinarySearch(arr []int, target int) int {
 			min = mean + 1
 		}
 	}
-	return min
+	if arr[min] == target {
+		return min
+	}
+	return -1
 }
 
 // upper bound
@@ -54,13 +57,16 @@ func UpperBoundBinarySearch(arr []int, target int) int {
 			max = mean - 1
 		}
 	}
-	return max
+	if arr[max] == target {
+		return max
+	}
+	return -1
 }
 
 func main() {
 	a := []int{1, 2, 3, 4, 5}
-	b := []int{1, 2, 3, 3, 3, 4, 5}
+	b := []int{1, 2, 3, 3, 3, 4, 6}
 	fmt.Println(CommonBinarySearch(a, 4))
-	fmt.Println(LowerBoundBinarySearch(b, 3))
-	fmt.Println(UpperBoundBinarySearch(b, 3))
+	fmt.Println(LowerBoundBinarySearch(b, 6))
+	fmt.Println(UpperBoundBinarySearch(b, 6))
 }
