7a2a0afb311434d5a50b8a707b8815c1a828b243
calvinchankf <chan9118kin@gmail.com>
2018-09-21 18:03:58 +0800

    day17 binary search as well

--- leetcode/349-intersection-of-two-arrays/main.go
+++ leetcode/349-intersection-of-two-arrays/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
+// hash table, O(n)
 func intersection(nums1 []int, nums2 []int) []int {
 	var hashtable = make(map[int]int)
 
@@ -30,6 +32,39 @@ func intersection(nums1 []int, nums2 []int) []int {
 	}
 
 	return result
+}
+
+// binary search, O(nlogn)
+// not a good solution compared to the hashtable one, but it is better to come up with more than 1 solution
+func intersection1(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums2)
+	var hashtable = make(map[int]int)
+	for i := 0; i < len(nums1); i++ {
+		idx := binary_search(nums2, nums1[i])
+		if idx > -1 {
+			hashtable[nums2[idx]] = 1
+		}
+	}
+	result := []int{}
+	for key, _ := range hashtable {
+		result = append(result, key)
+	}
+	return result
+}
+func binary_search(arr []int, target int) int {
+	min := 0
+	max := len(arr) - 1
+	for min <= max {
+		mean := (min + max) / 2
+		if target == arr[mean] {
+			return mean
+		} else if target > arr[mean] {
+			min = mean + 1
+		} else if target < arr[mean] {
+			max = mean - 1
+		}
+	}
+	return -1
 }
 
 func main() {
