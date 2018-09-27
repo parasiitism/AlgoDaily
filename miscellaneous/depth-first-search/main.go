dde4b9605208329a80368951ce28d3d0221b6e90
calvinchankf <chan9118kin@gmail.com>
2018-09-27 22:16:27 +0800

    day24

---
+++ miscellaneous/depth-first-search/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// depth first search = pre-order traversal
+// iterative
+func dfs(root *TreeNode) {
+	var stack []*TreeNode
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		pop := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		fmt.Println(pop.Val)
+		if pop.Right != nil {
+			stack = append(stack, pop.Right)
+		}
+		if pop.Left != nil {
+			stack = append(stack, pop.Left)
+		}
+	}
+}
+
+func main() {
+	// 		1
+	//	2		3
+	// 4 5 6 7
+	root := &TreeNode{1,
+		&TreeNode{2,
+			&TreeNode{4, nil, nil},
+			&TreeNode{5, nil, nil},
+		},
+		&TreeNode{3,
+			&TreeNode{6, nil, nil},
+			&TreeNode{7, nil, nil},
+		},
+	}
+	fmt.Println(`sfs`)
+	dfs(root)
+}
