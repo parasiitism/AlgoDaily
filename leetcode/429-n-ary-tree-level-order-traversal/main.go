a7fd4dffed0db4565cc0a43345417b7d9448b242
calvinchankf <chan9118kin@gmail.com>
2018-10-19 21:33:51 +0800

    go version

--- leetcode/429-n-ary-tree-level-order-traversal/main.go
+++ leetcode/429-n-ary-tree-level-order-traversal/main.go
@@ -1,5 +1,35 @@
 package main
 
+type NaryNode struct {
+	Val      int
+	Children []*NaryNode
+}
+
+// bfs, iterative
+func levelOrder(root *NaryNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	var result [][]int
+	queue := []*NaryNode{root}
+	for len(queue) > 0 {
+		lengthOnSameLevel := len(queue)
+		nodesOnSameLevel := []int{}
+		for i := 0; i < lengthOnSameLevel; i++ {
+			head := queue[0]
+			queue = queue[1:len(queue)]
+			nodesOnSameLevel = append(nodesOnSameLevel, head.Val)
+			for j := 0; j < len(head.Children); j++ {
+				if head.Children[j] != nil {
+					queue = append(queue, head.Children[j])
+				}
+			}
+		}
+		result = append(result, nodesOnSameLevel)
+	}
+	return result
+}
+
 func main() {
 
 }
