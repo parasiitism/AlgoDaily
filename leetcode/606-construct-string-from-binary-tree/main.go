e3553380b3636f893404f750676cbafed9e4ae67
calvinchankf <chan9118kin@gmail.com>
2018-10-26 18:57:20 +0800

    day54

---
+++ leetcode/606-construct-string-from-binary-tree/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func tree2str(t *TreeNode) string {
+	if t == nil {
+		return ""
+	}
+	val := strconv.Itoa(t.Val)
+	left := tree2str(t.Left)
+	right := tree2str(t.Right)
+	if left == "" && right == "" {
+		return val
+	} else if right == "" {
+		return val + "(" + left + ")"
+	} else if left == "" {
+		return val + "()(" + right + ")"
+	} else {
+		return val + "(" + left + ")(" + right + ")"
+	}
+}
+
+func main() {
+	// 		5
+	//	2		8
+	// 1 3 7 9
+	root := &TreeNode{5,
+		&TreeNode{2,
+			&TreeNode{1, nil, nil},
+			&TreeNode{3, nil, nil},
+		},
+		&TreeNode{8,
+			&TreeNode{7, nil, nil},
+			&TreeNode{9, nil, nil},
+		},
+	}
+	ans := tree2str(root)
+	fmt.Println(ans)
+	// 		5
+	//	2		8
+	// n 3 7	n
+	root = &TreeNode{5,
+		&TreeNode{2,
+			nil,
+			&TreeNode{3, nil, nil},
+		},
+		&TreeNode{8,
+			&TreeNode{7, nil, nil},
+			nil,
+		},
+	}
+	ans = tree2str(root)
+	fmt.Println(ans)
+}
