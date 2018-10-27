90276196a622638b280bec950fa2c533b144ae8f
calvinchankf <chan9118kin@gmail.com>
2018-10-27 16:50:32 +0800

    day55 2nd solution

--- leetcode/199-binary-tree-right-side-view/main.go
+++ leetcode/199-binary-tree-right-side-view/main.go
@@ -28,6 +28,7 @@ func rightSideView(root *TreeNode) []int {
 		stack = stack[:len(stack)-1]
 		node := pop.Node
 		route := pop.Route
+		// put left child to the stack first, since we want to read right child in the next iteration
 		if node.Left == nil && node.Right == nil && len(route) > len(result) {
 			result = append(result, route[len(result):]...)
 		}
@@ -41,10 +42,35 @@ func rightSideView(root *TreeNode) []int {
 	return result
 }
 
+// recursive
+func rightSideView1(root *TreeNode) []int {
+	result := []int{}
+	if root == nil {
+		return result
+	}
+	var dfs func(root *TreeNode, route []int)
+	dfs = func(root *TreeNode, route []int) {
+		if root != nil {
+			if root.Left == nil && root.Right == nil && len(route) > len(result) {
+				result = append(result, route[len(result):]...)
+			}
+			// put right child first, since we want the right first
+			if root.Right != nil {
+				dfs(root.Right, append(route, root.Right.Val))
+			}
+			if root.Left != nil {
+				dfs(root.Left, append(route, root.Left.Val))
+			}
+		}
+	}
+	dfs(root, []int{root.Val})
+	return result
+}
+
 func main() {
 	// 		5
 	//	2		8
-	// 1 3 7 9
+	// 1 3 7
 	//10
 	root := &TreeNode{5,
 		&TreeNode{2,
@@ -56,9 +82,11 @@ func main() {
 		},
 		&TreeNode{8,
 			&TreeNode{7, nil, nil},
-			&TreeNode{9, nil, nil},
+			nil,
 		},
 	}
 	ans := rightSideView(root)
+	fmt.Println(ans)
+	ans = rightSideView1(root)
 	fmt.Println(ans)
 }
