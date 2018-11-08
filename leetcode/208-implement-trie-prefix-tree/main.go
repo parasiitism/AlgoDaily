3924c6274eef32d80c12796ef542bdc70d4b9e17
calvinchankf <chan9118kin@gmail.com>
2018-11-08 18:19:53 +0800

    day64

--- leetcode/208-implement-trie-prefix-tree/main.go
+++ leetcode/208-implement-trie-prefix-tree/main.go
@@ -16,10 +16,30 @@ func Constructor() Trie {
 	return root
 }
 
-// /** Inserts a word into the trie. */
-// func (this *Trie) Insert(word string) {
+/** Inserts a word into the trie. */
+func (this *Trie) Insert(word string) {
+	if len(word) == 0 {
+		return
+	}
+	current := this
+	for i := 0; i < len(word); i++ {
 
-// }
+		charactor := string(word[i])
+
+		var temp Trie
+		if _, existed := current.children[charactor]; existed {
+			temp = current.children[charactor]
+		} else {
+			temp = Constructor()
+		}
+
+		if i == len(word)-1 {
+			temp.isWord = true
+		}
+		current.children[charactor] = temp
+		current = &temp
+	}
+}
 
 // /** Returns if the word is in the trie. */
 // func (this *Trie) Search(word string) bool {
@@ -33,17 +53,25 @@ func Constructor() Trie {
 
 func main() {
 	var hashtable = make(map[string]string)
-	fmt.Println(hashtable)
+	// fmt.Println(hashtable)
 	hashtable["a"] = "calvi"
 	hashtable["b"] = "calvin"
-	fmt.Println(hashtable)
+	// fmt.Println(hashtable["c"])
+	// fmt.Println(hashtable)
 
 	// a_node := make(map[string]Trie)
 	// a_node["b"] = nil
 	// b := Trie{false, a_node}
 	a := Trie{false, make(map[string]Trie)}
-	fmt.Println(a)
 	b := Trie{false, make(map[string]Trie)}
 	a.children["a"] = b
-	fmt.Println(a)
+	// fmt.Println(a)
+	if _, ok := a.children["a"]; ok {
+		// fmt.Println(a.children["a"])
+	}
+
+	t := Constructor()
+	t.Insert("abc")
+	t.Insert("app")
+	fmt.Println(t)
 }
