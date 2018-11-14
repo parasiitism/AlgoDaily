98c4d6b7cb953b44533d70134f6cb8691d76f62d
calvinchankf <chan9118kin@gmail.com>
2018-11-14 21:23:59 +0800

    revise

--- leetcode/208-implement-trie-prefix-tree/main.go
+++ leetcode/208-implement-trie-prefix-tree/main.go
@@ -4,27 +4,31 @@ import (
 	"fmt"
 )
 
+/*
+	approach 1: hash table
+*/
+
 // type but, in my opinion, it is a class
 type Trie struct {
 	IsWord   bool
-	Children map[string]*Trie
+	Children map[byte]*Trie
 }
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
-	return Trie{false, make(map[string]*Trie)}
+	return Trie{false, make(map[byte]*Trie)}
 }
 
 /** Inserts a word into the trie. */
 func (this *Trie) Insert(word string) {
 	current := this
 	for i := 0; i < len(word); i++ {
-		charactor := string(word[i])
+		charactor := word[i]
 		var temp *Trie
 		if value, existed := current.Children[charactor]; existed {
 			temp = value
 		} else {
-			temp = &Trie{false, make(map[string]*Trie)}
+			temp = &Trie{false, make(map[byte]*Trie)}
 		}
 		if i == len(word)-1 {
 			temp.IsWord = true
@@ -38,7 +42,7 @@ func (this *Trie) Insert(word string) {
 func (this *Trie) Search(word string) bool {
 	current := this
 	for i := 0; i < len(word); i++ {
-		charactor := string(word[i])
+		charactor := word[i]
 		if value, existed := current.Children[charactor]; existed {
 			if i == len(word)-1 && value.IsWord {
 				return true
@@ -55,7 +59,7 @@ func (this *Trie) Search(word string) bool {
 func (this *Trie) StartsWith(prefix string) bool {
 	current := this
 	for i := 0; i < len(prefix); i++ {
-		charactor := string(prefix[i])
+		charactor := prefix[i]
 		if value, existed := current.Children[charactor]; existed {
 			if i == len(prefix)-1 {
 				return true
