997b515443262a9859281d61259285f3cf3dcb26
calvinchankf <chan9118kin@gmail.com>
2018-11-16 22:50:44 +0800

    better approach

--- leetcode/208-implement-trie-prefix-tree/main.py
+++ leetcode/208-implement-trie-prefix-tree/main.py
@@ -66,6 +66,54 @@ class Trie(object):
         return False
 
 
+class Trie2(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node({}, False)
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = Node({}, False)
+            cur = cur.children[w]
+        cur.is_word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                return False
+            cur = cur.children[w]
+        return cur.is_word
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                return False
+            cur = cur.children[w]
+        return True
+
+
 obj = Trie()
 obj.insert('abc')
 print(obj.root.children)
