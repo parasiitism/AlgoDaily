6b4e7e24ffc7df338d766b85bd3824761953c5e7
calvinchankf <chan9118kin@gmail.com>
2018-11-20 18:24:38 +0800

    day78

---
+++ leetcode/212-word-search-ii/main.go
@@ -0,0 +1,68 @@
+package main
+
+func findWords(board [][]byte, words []string) []string {
+	hashtable := make(map[string]bool)
+	for i := 0; i < len(words); i++ {
+		word := words[i]
+		if exist(board, word) {
+			hashtable[word] = true
+		}
+	}
+	result := []string{}
+	for k := range hashtable {
+		result = append(result, k)
+	}
+	return result
+}
+
+func exist(board [][]byte, word string) bool {
+	if len(word) == 0 {
+		return false
+	}
+	// find the "heads" in the matrix
+	head := word[0]
+	byte_word := []byte(word)
+	for row := 0; row < len(board); row++ {
+		for col := 0; col < len(board[0]); col++ {
+			if board[row][col] == head {
+				if dfs(row, col, byte_word, board) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func dfs(row int, col int, tail []byte, board [][]byte) bool {
+	if row < 0 || row >= len(board) {
+		return false
+	}
+	if col < 0 || col >= len(board[row]) {
+		return false
+	}
+	if board[row][col] != tail[0] {
+		return false
+	}
+	// board[row][col] == tail[0] now
+	if len(tail) == 1 {
+		return true
+	}
+
+	temp := board[row][col]
+	board[row][col] = '.'
+
+	t := tail[1:]
+	found := dfs(row-1, col, t, board) ||
+		dfs(row+1, col, t, board) ||
+		dfs(row, col-1, t, board) ||
+		dfs(row, col+1, t, board)
+
+	board[row][col] = temp
+
+	return found
+}
+
+func main() {
+
+}
--- readme.md
+++ readme.md
@@ -109,3 +109,4 @@ tree | [leetcode 889](https://leetcode.com/problems/serialize-and-deserialize-bi
 | 75 | [Design Search Autocomplete System](/leetcode/642-design-search-autocomplete-system) | trie | [leetcode 642](https://leetcode.com/problems/design-search-autocomplete-system/)| do the same thing in python and i got accepted, fuck leetcode |
 | 76 | [Design Search Autocomplete System](/leetcode/642-design-search-autocomplete-system) | trie | [leetcode 642](https://leetcode.com/problems/design-search-autocomplete-system/)| i finally made it in golang and I BEAT 100% submissions |
 | 77 | [Word Search](/leetcode/79-word-search) | trie | [leetcode 79](https://leetcode.com/problems/word-search/)| my 1st attempt got TLE, it is weird cos the notion is correct |
+| 78 | [Word Search II](/leetcode/79-word-search-ii) | trie | [leetcode 212](https://leetcode.com/problems/word-search-ii/)| its actually a follow-up of day77 problem, tho my snippet can only beats 14% 😭 |
