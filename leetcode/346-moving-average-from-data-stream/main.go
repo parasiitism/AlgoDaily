78a1a3897ddd261a5294a2d2d5fcb104985eae85
calvinchankf <chan9118kin@gmail.com>
2018-11-21 20:33:30 +0800

    day79 solution2

--- leetcode/346-moving-average-from-data-stream/main.go
+++ leetcode/346-moving-average-from-data-stream/main.go
@@ -18,13 +18,37 @@ func (this *MovingAverage) Next(val int) float64 {
 		for i := 0; i < len(this.Arr); i++ {
 			sum += this.Arr[i]
 		}
-		return float64(sum) / float64(this.Size)
+		return float64(sum) / float64(len(this.Arr))
 	}
 	sum := 0
 	for i := len(this.Arr) - this.Size; i < len(this.Arr); i++ {
 		sum += this.Arr[i]
 	}
 	return float64(sum) / float64(this.Size)
+}
+
+// Attempt 2: cache the sum
+type MovingAverage1 struct {
+	Size int
+	Arr  []int
+	Sum  int
+}
+
+func Constructor1(size int) MovingAverage1 {
+	return MovingAverage1{size, []int{}, 0}
+}
+
+func (this *MovingAverage1) Next(val int) float64 {
+	if len(this.Arr) < this.Size {
+		this.Sum += val
+		this.Arr = append(this.Arr, val)
+		return float64(this.Sum) / float64(len(this.Arr))
+	}
+	this.Sum -= this.Arr[0]
+	this.Arr = this.Arr[1:]
+	this.Arr = append(this.Arr, val)
+	this.Sum += val
+	return float64(this.Sum) / float64(this.Size)
 }
 
 func main() {
