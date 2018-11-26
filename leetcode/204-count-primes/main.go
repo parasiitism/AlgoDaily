5b64750edaa35e9baf14b10a406b54e8a464017e
calvinchankf <chan9118kin@gmail.com>
2018-11-27 00:24:12 +0800

    day85 dynamic programming approach

--- leetcode/204-count-primes/main.go
+++ leetcode/204-count-primes/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// TLE
 func countPrimes(n int) int {
 	if n < 3 {
 		return 0
@@ -25,8 +26,27 @@ func countPrimes(n int) int {
 	return result
 }
 
+//	learned from the others:
+//	1. dummy arry for prime or not
+//	2. for each number, set it multiples as noPrimes to speed the further calculations
+func countPrimes1(n int) int {
+	if n < 2 {
+		return 0
+	}
+	notPrime := make([]bool, n)
+	count := 0
+	for i := 2; i < n; i++ {
+		if !notPrime[i] {
+			count++
+			for j := 2; i*j < n; j++ {
+				notPrime[i*j] = true
+			}
+		}
+	}
+	return count
+}
+
 func main() {
 	// 499979
-
-	fmt.Println(countPrimes(10))
+	fmt.Println(countPrimes1(499979))
 }
