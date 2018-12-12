cdef55435a6050e6a7bb5b81eed4341207384287
calvinchankf <chan9118kin@gmail.com>
2018-12-12 16:13:50 +0800

    day102 2nd attempt

--- leetcode/263-ugly-number/main.go
+++ leetcode/263-ugly-number/main.go
@@ -5,6 +5,8 @@ import (
 )
 
 // approach 1: just math
+// beats 5.43%
+// however when i use "temp == 0 || r != 0", it beats 100% and I dont know why
 func isUgly(num int) bool {
 	// 2
 	for {
@@ -34,6 +36,34 @@ func isUgly(num int) bool {
 		num = temp
 	}
 	return num == 1
+}
+
+// 2nd attempt
+// dfs recursive
+// again beats 5.43%
+// however when i use "temp == 0 || r != 0", it beats 100% and I dont know why
+func isUgly1(num int) bool {
+
+	// base cases
+	if num == 0 {
+		return false
+	}
+	if num == 1 || num == 2 || num == 3 || num == 5 {
+		return true
+	}
+
+	// check the children
+	if num%2 == 0 {
+		return isUgly(num / 2)
+	}
+	if num%3 == 0 {
+		return isUgly(num / 3)
+	}
+	if num%5 == 0 {
+		return isUgly(num / 5)
+	}
+
+	return false
 }
 
 func main() {
