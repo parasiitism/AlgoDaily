a03da25df68abd22811f04f81e3e864356a46002
calvinchankf <chan9118kin@gmail.com>
2018-12-13 16:31:08 +0800

    day103 try O(2n) and O(n^2)

--- leetcode/1-two-sum/main.go
+++ leetcode/1-two-sum/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 )
 
-// classical solutino
+// classical solution
+// O(n)
 // beats 100%
 func twoSum(nums []int, target int) []int {
 	hash := make(map[int]int)
@@ -18,6 +19,38 @@ func twoSum(nums []int, target int) []int {
 	return []int{}
 }
 
+// mediocre solution
+// O(2n)
+// beats 55%
+func twoSum1(nums []int, target int) []int {
+	hash := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		hash[nums[i]] = i
+	}
+	for i := 0; i < len(nums); i++ {
+		remain := target - nums[i]
+		v, e := hash[remain]
+		if e && v != i {
+			return []int{v, i}
+		}
+	}
+	return []int{}
+}
+
+// naive solution
+// O(n^2)
+// bests 14%
+func twoSum2(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if i != j && nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
 func main() {
-	fmt.Println(twoSum([]int{9, 2, 3, 4}, 12))
+	fmt.Println(twoSum1([]int{9, 2, 3, 4}, 12))
 }
