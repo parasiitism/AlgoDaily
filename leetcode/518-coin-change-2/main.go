fa3a5b0401616619c22bb222490c14a2e5a5b1ea
calvinchankf <chan9118kin@gmail.com>
2018-12-22 13:00:31 +0800

    day113

--- leetcode/518-coin-change-2/main.go
+++ leetcode/518-coin-change-2/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-// 1st attempt
+// 1st attempt: LTE
 // dfs with not-duplicate paths
 // see euler/31/explanation.jpeg
 // O(all paths) <- TLE on leetcode. Obviously, leetcode doesn't want us to traverse all the combination paths
-func change(amount int, coins []int) int {
+func change_(amount int, coins []int) int {
 	return dfs(coins, amount, 0)
 }
 func dfs(coins []int, n int, cur int) int {
@@ -21,6 +21,30 @@ func dfs(coins []int, n int, cur int) int {
 		cnt += dfs(coins, n-coin, i)
 	}
 	return cnt
+}
+
+// 2nd attempt: DP, learned from others
+// the idea is to divide the problem into subproblems:
+// for each amount, calculate the number of different combinations using the result from smaller amount
+// e.g.
+// dp[amount] = dp[amount] + dp[amount-coin]
+// dp[4] = 1 + dp[2]
+// it means 4 can be came up with 1111 and the dp[2](the combination of 2), which is 11 and 2
+// therefore
+// dp[4] = 1 + 2 = 3
+// see ./explanation.jpeg
+func change(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+	for i := 0; i < len(coins); i++ {
+		coin := coins[i]
+		for j := 1; j <= amount; j++ {
+			if j-coin >= 0 {
+				dp[j] += dp[j-coin]
+			}
+		}
+	}
+	return dp[amount]
 }
 
 func main() {
