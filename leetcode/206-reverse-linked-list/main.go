3eb46361e0809ebe3daf679db0c80ae4cb5173d0
calvinchankf <chan9118kin@gmail.com>
2018-12-25 12:52:55 +0800

    day115 q2 2nd approach

--- leetcode/206-reverse-linked-list/main.go
+++ leetcode/206-reverse-linked-list/main.go
@@ -9,8 +9,8 @@ type ListNode struct {
 
 // naive approach
 // pu the nodes into an array and reversively put the items into the result
-// time O(2n)
-// space O(n)
+// time		O(2n)
+// space	O(n)
 // but it beats 100% LOL
 func reverseList(head *ListNode) *ListNode {
 	if head == nil {
@@ -31,6 +31,28 @@ func reverseList(head *ListNode) *ListNode {
 	return dump.Next
 }
 
+// 2nd approach: inplace replacement(learned from others)
+// https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1204/
+// 1->2->3
+// 2->1->3
+// tine		O(n)
+// space 	O(1) since in-place
+// beats 100%
+func reverseList1(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	cur := head
+	for head.Next != nil { // since we are replacing the head.next, the loop should end when there is no next
+		temp := head.Next     // the middle node
+		head.Next = temp.Next // or head.Next.Next
+		temp.Next = cur       // put the middle node in front of cur
+		cur = temp            // assign cur as head
+	}
+	return cur
+}
+
+// helper
 func arr2list(arr []int) *ListNode {
 	dump := &ListNode{0, nil}
 	dumpCur := dump
@@ -51,6 +73,6 @@ func printList(l *ListNode) {
 
 func main() {
 	a := arr2list([]int{1, 2, 3, 4, 5})
-	b := reverseList(a)
+	b := reverseList1(a)
 	printList(b)
 }
