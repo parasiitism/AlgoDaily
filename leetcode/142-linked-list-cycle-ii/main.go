80ed43be2fa4548e93eea6493318e427769eb5d2
calvinchankf <chan9118kin@gmail.com>
2018-12-26 11:24:48 +0800

    day116 q2 2nd

--- leetcode/142-linked-list-cycle-ii/main.go
+++ leetcode/142-linked-list-cycle-ii/main.go
@@ -22,6 +22,40 @@ func detectCycle(head *ListNode) *ListNode {
 	return nil
 }
 
+// classic approach
+// 1. check if there is a cycle
+// 2. slow pointer => a+b+c+b= 2(a+b) <= fast pointer
+// 													a=c
+// see ./idea.jpeg
+// time 	O(n)
+// space	O(1)
+// beats 100%
+func detectCycle1(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	fast := head.Next
+	slow := head
+	for fast != nil && fast.Next != nil && slow != nil {
+		if fast == slow {
+			break
+		}
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	// no cycle
+	if fast != slow {
+		return nil
+	}
+	slow = head
+	fast = head.Next
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow // fast
+}
+
 func main() {
 
 }
