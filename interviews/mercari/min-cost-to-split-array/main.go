b3e8b5434466bc59178cdd19c4ff8dbe51dadc4d
calvinchankf <chan9118kin@gmail.com>
2018-12-29 14:32:31 +0800

    more comment

--- interviews/mercari/min-cost-to-split-array/main.go
+++ interviews/mercari/min-cost-to-split-array/main.go
@@ -5,6 +5,9 @@ import (
 	"math"
 )
 
+// naively search for the min for each element
+// time 	O(n^2)
+// space	O(1)
 func Solution(A []int) int {
 	l := len(A)
 	min := math.MaxUint32
@@ -19,6 +22,10 @@ func Solution(A []int) int {
 	return min
 }
 
+// 2 pointers
+// P=slow, Q=fast, compare the arr[p]+arr[q] with calcuated minimum
+// time 	O(n)
+// space	O(1)
 func Solution1(A []int) int {
 	p := 1
 	l := len(A)
@@ -28,6 +35,7 @@ func Solution1(A []int) int {
 		if temp < min {
 			min = temp
 		}
+		// only set slow pointer(P) when the previous element of faster pointer is smaller
 		if A[p] > A[q-1] {
 			p = q - 1
 		}
@@ -36,7 +44,10 @@ func Solution1(A []int) int {
 }
 
 func main() {
-	a := []int{5, 2, 4, 6, 3, 7}
+	// a := []int{5, 2, 4, 6, 3, 7}
+	// a := []int{5, 2, 1, 6, 3, 7}
+	// a := []int{5, 2, 4, 1, 6, 3, 7}
+	a := []int{5, 2, 1, 4, 6, 3, 7}
 	fmt.Println(Solution(a))
 	fmt.Println(Solution1(a))
 }
