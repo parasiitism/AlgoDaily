a1a3fde6e49ba46fb1b2865243c4bc84a3f50d86
calvinchankf <chan9118kin@gmail.com>
2019-01-02 15:25:46 +0800

    day122 q2 s2

--- miscellaneous/combinations/main.go
+++ miscellaneous/combinations/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 /*
-	Implementation similar to permutations
+	Recursive implementation similar to permutations
 	Time O(nCk)
 	Space O(nCk) due to the recrusion
-	beats 1.22% (python version beats 13.88%, i assume that initializing an array is too slow in golang
+	beats 1.22% (python version beats 13.88%)
 */
 func combine(n int, k int) [][]int {
 	if k < 1 || k > n {
@@ -36,8 +36,42 @@ func combine(n int, k int) [][]int {
 	return result
 }
 
+/*
+	Recursive implementation similar to permutations
+	Time O(nCk)
+	Space O(nCk) due to the recrusion
+	beats 1.22% (python version beats 21.53%) WTF
+*/
+func combine1(n int, k int) [][]int {
+	if k < 1 || k > n {
+		return [][]int{}
+	}
+	result := [][]int{}
+	var dfs func(start int, path []int)
+	dfs = func(start int, path []int) {
+		if len(path) == k {
+			result = append(result, path)
+		} else {
+			for i := start; i <= n; i++ {
+				// copy thing
+				nextpath := []int{}
+				nextpath = append(nextpath, path...)
+				nextpath = append(nextpath, i)
+				// dfs(nums[i+1:], path+[nums[i]])
+				dfs(i+1, nextpath)
+			}
+		}
+	}
+	dfs(1, []int{})
+	return result
+}
+
 func main() {
 	fmt.Println(combine(4, 2))
 	fmt.Println(combine(4, 3))
 	fmt.Println(combine(5, 4))
+
+	fmt.Println(combine1(4, 2))
+	fmt.Println(combine1(4, 3))
+	fmt.Println(combine1(5, 4))
 }
--- miscellaneous/combinations/main.py
+++ miscellaneous/combinations/main.py
@@ -32,3 +32,36 @@ r = Solution().combine(4, 3)
 print(r)
 r = Solution().combine(5, 4)
 print(r)
+
+
+class Solution1(object):
+    """
+    Implementation similar to permutations
+    Time O(nCk)
+    Space O(nCk) due to the recrusion
+    beats 21.53%
+    """
+
+    def __init__(self):
+        self.result = []
+
+    def combine(self, n, k):
+        if k < 1 or k > n:
+            return []
+        self.dfs(1, n, [], k)
+        return self.result
+
+    def dfs(self, start, n, path, k):
+        if len(path) == k:
+            self.result.append(path)
+        else:
+            for i in range(start, n+1):
+                self.dfs(i+1, n, path + [i], k)
+
+
+r = Solution1().combine(4, 2)
+print(r)
+r = Solution1().combine(4, 3)
+print(r)
+r = Solution1().combine(5, 4)
+print(r)
