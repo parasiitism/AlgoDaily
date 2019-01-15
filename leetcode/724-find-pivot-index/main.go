0767eb5f4b7c1e7446b4a68d643cd0a53d9cef3e
calvinchankf <chan9118kin@gmail.com>
2019-01-15 11:25:30 +0800

    day135

---
+++ leetcode/724-find-pivot-index/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+/*
+	i failed to come up with a correct approach to this question
+	learned from others
+	1.index: 0, num: 1, left: 0, right: 27
+	2. index: 1, num: 7, left: 1, right: 20
+	3. index: 2, num: 3, left: 8, right: 17
+	4. index: 3, num: 6, left: 11, right: 11 <-- Found!!!
+	Time	O(n)
+	Space O(1)
+*/
+func pivotIndex(nums []int) int {
+	right := 0
+	for i := 0; i < len(nums); i++ {
+		right += nums[i]
+	}
+	left := 0
+	for i := 0; i < len(nums); i++ {
+		right -= nums[i]
+		if left == right {
+			return i
+		}
+		left += nums[i]
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(pivotIndex([]int{}))
+	fmt.Println(pivotIndex([]int{1}))
+	fmt.Println(pivotIndex([]int{1, 1}))
+	fmt.Println(pivotIndex([]int{1, 2, 1}))
+	fmt.Println(pivotIndex([]int{1, -1, 2, 2}))
+	fmt.Println(pivotIndex([]int{1, -1, 2, 0, 4}))
+	fmt.Println(pivotIndex([]int{1, 7, 3, 6, 5, 6}))
+	fmt.Println(pivotIndex([]int{1, 7, 0, 0, 2, 6}))
+	fmt.Println(pivotIndex([]int{1, 7, -1, 7, 0, 0, 2, 6}))
+	fmt.Println(pivotIndex([]int{1, 1, 1, 1, 1}))
+	fmt.Println(pivotIndex([]int{1, 1, 1, 1, 1, 1}))
+	fmt.Println(pivotIndex([]int{-1, -1, -1, -1, -1}))
+	fmt.Println(pivotIndex([]int{-1, -1, -1, -1, -1, -1}))
+}
--- readme.md
+++ readme.md
@@ -202,3 +202,4 @@ tree | [leetcode 889](https://leetcode.com/problems/serialize-and-deserialize-bi
 | 134 | [Minimum Absolute Difference in BST](/leetcode/530-minimum-absolute-difference-in-bst) | BST | [leetode 530](https://leetcode.com/problems/minimum-absolute-difference-in-bst/) | |
 | 134 | [Find Bottom Left Tree Value](/leetcode/513-find-bottom-left-tree-value) | tree | [leetode 513](https://leetcode.com/problems/find-bottom-left-tree-value/) | both bfs & dfs run 12ms and beat 100% |
 | 134 | [Path Sum III](/leetcode/437-path-sum-iii) | tree | [leetode 437](https://leetcode.com/problems/find-bottom-left-tree-value/) | 1st LTE. 2nd 24ms beats 28.57% |
+| 135 | [Find Pivot Index](/leetcode/724-find-pivot-index) | array, math | [leetode 724](https://leetcode.com/problems/find-pivot-index/) | i failed to come up with a correct approach, learned from others |
