495ce0abff624320dd9e49b5d219c3dc021562a5
calvinchankf <chan9118kin@gmail.com>
2019-01-19 09:11:08 +0800

    correct flights route 2nd attempt

--- miscellaneous/correct-flights-route/main.py
+++ miscellaneous/correct-flights-route/main.py
@@ -9,6 +9,8 @@ def correctFlights(flights):
     """
     flights: ["SJC-LAX", "SFO-SJC", "LAX-MIA"]
     return: ["SFO-SJC", "SJC-LAX", "LAX-MIA"]
+    Time    O(n^2)
+    Space   O(n) result
     """
     result = []
     while len(flights) > 0:
@@ -27,7 +29,44 @@ def correctFlights(flights):
     return result
 
 
-print(correctFlights(["SFO-SJC", "SJC-SFO"]))
+def correctFlights(flights):
+    """
+    flights: ["SJC-LAX", "SFO-SJC", "LAX-MIA"]
+    return: ["SFO-SJC", "SJC-LAX", "LAX-MIA"]
+    Time    O(3n)
+    Space   O(n) result
+    19jan2019
+    """
+    if len(flights) == 0:
+        return []
+
+    forward = {}
+    backwarod = {}
+
+    for flight in flights:
+        src, dst = flight.split("-")
+        forward[src] = dst
+        backwarod[dst] = src
+
+    result = [flights[0]]
+
+    while True:
+        src, dst = result[-1].split("-")
+        if dst in forward:
+            result.append(dst+"-"+forward[dst])
+        else:
+            break
+
+    while True:
+        src, dst = result[0].split("-")
+        if src in backwarod:
+            result = [backwarod[src]+"-"+src] + result
+        else:
+            break
+
+    return result
+
+
 print(correctFlights(["SFO-SJC", "SJC-LAX", "LAX-MIA"]))
 print(correctFlights(["SJC-LAX", "SFO-SJC", "LAX-MIA"]))
 print(correctFlights(["A-B", "B-C", "C-D", "D-E", "E-F"]))
--- readme.md
+++ readme.md
@@ -221,6 +221,6 @@ tree | [leetcode 889](https://leetcode.com/problems/serialize-and-deserialize-bi
 | 137 | [Number of 1 Bits](/leetcode/191-number-of-1-bits) | bit op | [leetode 191](https://leetcode.com/problems/number-of-1-bits/) | 📌 the key here is to practice bit operation, i ignore any attempts using strings |
 | 137 | [Reverse Bits](/leetcode/190-reverse-bits) | bit op | [leetode 190](https://leetcode.com/problems/reverse-bits/) | 📌 the key here is to practice bit operation, i ignore any attempts using strings |
 | 138 | [LRU Cache](/leetcode/146-lru-cache) | hashtable, linked list | [leetode 146](https://leetcode.com/problems/lru-cache/) | 📌📌📌 |
-| 138 | [Correct Flights Route](/miscellaneous/correct-flights-route) | array | interview | 🤔 interesting |
+| 138 | [Correct Flights Route](/miscellaneous/correct-flights-route) | array | interview | 🤔 interesting...1st O(n^2), 2nd O(n) |
 | 138 | [Length of Last Word](/leetcode/58-length-of-last-word) | array | [leetcode 58](https://leetcode.com/problems/length-of-last-word/) | not a good question |
 | 139 | [Count and Say](/leetcode/38-count-and-say) | array | [leetcode 38](https://leetcode.com/problems/count-and-say/) | ⭐️ |
