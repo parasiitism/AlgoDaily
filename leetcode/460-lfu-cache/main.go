4d94fc311732f21be91495494c88a184f0ee3239
calvinchankf <chan9118kin@gmail.com>
2019-01-20 14:08:51 +0800

    refactor

--- leetcode/460-lfu-cache/main.go
+++ leetcode/460-lfu-cache/main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"container/heap"
+	"fmt"
 	"time"
 )
 
 /*
-	i cant figure the efficient way to do it
-	-	learned from others
+	learned from others
 */
-
 type LFUCache struct {
-	// 用于检查 key 的存在性
-	hash map[int]*Entry
-	pq   PQ
+	hash map[int]*Entry // hashtable
+	pq   PQ             // priority queue
 	cap  int
 }
 
-// Constructor 构建了 LFUCache
+// Constructor of LFUCache
 func Constructor(capacity int) LFUCache {
 	return LFUCache{
 		hash: make(map[int]*Entry, capacity),
@@ -26,7 +24,6 @@ func Constructor(capacity int) LFUCache {
 	}
 }
 
-// Get 获取 key 的值
 func (this *LFUCache) Get(key int) int {
 	ep, ok := this.hash[key]
 	if ok {
@@ -36,14 +33,14 @@ func (this *LFUCache) Get(key int) int {
 	return -1
 }
 
-// Put 把 key， value 成对地放入 LFUCache
-// 如果 LFUCache 已满的话，会删除掉 LFUCache 中使用最少的 key
+// Put key&value into LFUCache
+// if LFUCache is full，remove the least frequent key&valu in LFUCache
 func (this *LFUCache) Put(key int, value int) {
 	if this.cap <= 0 {
 		return
 	}
 	ep, ok := this.hash[key]
-	// key 已存在，就更新 value
+	// if key is present, update value
 	if ok {
 		this.hash[key].value = value
 		this.pq.update(ep)
@@ -51,7 +48,9 @@ func (this *LFUCache) Put(key int, value int) {
 	}
 
 	ep = &Entry{key: key, value: value}
-	// pq 已满的话，需要先删除，再插入
+	// if pq is full
+	// 1. first remove the least frequently used key&value
+	// 2. insert the new key&value
 	if len(this.pq) == this.cap {
 		temp := heap.Pop(&this.pq).(*Entry)
 		delete(this.hash, temp.key)
@@ -60,18 +59,10 @@ func (this *LFUCache) Put(key int, value int) {
 	heap.Push(&this.pq, ep)
 }
 
-/**
- * Your LFUCache object will be instantiated and called as such:
- * obj := Constructor(capacity);
- * param_1 := obj.Get(key);
- * obj.Put(key,value);
- */
-
-// entry 是 priorityQueue 中的元素
+// entry is a node of Priority Queue
 type Entry struct {
-	key   int
-	value int
-	// 以下是辅助参数，由 heap.Interface 实现的函数自动处理
+	key       int
+	value     int
 	frequence int
 	index     int
 	date      time.Time
@@ -80,51 +71,50 @@ type Entry struct {
 // PQ implements heap.Interface and holds entries.
 type PQ []*Entry
 
-func (pq PQ) Len() int { return len(pq) }
+func (this PQ) Len() int { return len(this) }
 
-func (pq PQ) Less(i, j int) bool {
-	if pq[i].frequence == pq[j].frequence {
-		return pq[i].date.Before(pq[j].date)
+func (this PQ) Less(i, j int) bool {
+	if this[i].frequence == this[j].frequence {
+		return this[i].date.Before(this[j].date)
 	}
 
-	return pq[i].frequence < pq[j].frequence
+	return this[i].frequence < this[j].frequence
 }
 
-func (pq PQ) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
+func (this PQ) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+	this[i].index = i
+	this[j].index = j
 }
 
-// Push 往 pq 中放 entry
-func (pq *PQ) Push(x interface{}) {
-	n := len(*pq)
+func (this *PQ) Push(x interface{}) {
+	n := len(*this)
 	entry := x.(*Entry)
 	entry.index = n
 	entry.date = time.Now()
-	*pq = append(*pq, entry)
+	*this = append(*this, entry)
 }
 
-// Pop 从 pq 中取出最优先的 entry
-func (pq *PQ) Pop() interface{} {
-	old := *pq
+func (this *PQ) Pop() interface{} {
+	old := *this
 	n := len(old)
 	entry := old[n-1]
 	entry.index = -1 // for safety
-	*pq = old[0 : n-1]
+	*this = old[0 : n-1]
 	return entry
 }
 
 // update modifies the priority of an entry in the queue.
-func (pq *PQ) update(entry *Entry) {
+func (this *PQ) update(entry *Entry) {
 	entry.frequence++
 	entry.date = time.Now()
-	heap.Fix(pq, entry.index)
+	heap.Fix(this, entry.index)
 }
 
 func main() {
 	c := Constructor(2)
 	c.Put(1, 1)
-	// c.Put(2, 2)
-	// c.Put(3, 3)
+	c.Put(2, 2)
+	c.Put(3, 3)
+	fmt.Println(c.Get(2))
 }
