4a7e1a07f22d5ffe4d9cb45252f1ddcf209bcb7e
calvinchankf <chan9118kin@gmail.com>
2019-01-23 14:01:39 +0800

    day143 q4 s1

---
+++ leetcode/209-minimum-size-subarray-sum/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+	questions to ask:
+	- will there be no subarray add up to the s?
+	- will there be negative?
+	- will there be s=3, nums = [1,2,3], so the ans = 1?
+	- let's say if s=0, nums = [0,0,0,0,0], the ans = 1?
+*/
+
+/*
+	1st approach
+	- store an array for each item, find the target in the subarray
+	e.g.
+  [23, 2, 4, 6, 7]
+	23	25 29 35	42
+			2		6	12	19
+					4	10	13
+							6	 7
+	^		^		^	 ^	 ^
+	if i-j+1 < res, res = i-j+1
+	Time	O(n^2) n(n+1)/2
+	Space O(n)
+	2488 ms beats 5.8%
+	23jan2019
+*/
+func minSubArrayLen(s int, nums []int) int {
+	res := math.MaxInt64
+	layer := []int{}
+	for i := 0; i < len(nums); i++ {
+		num := nums[i]
+		temp := []int{}
+		for j := 0; j < len(layer); j++ {
+			x := layer[j] + num
+			if x >= s && i-j+1 < res {
+				res = i - j + 1
+			}
+			temp = append(temp, x)
+		}
+		if num >= s {
+			res = 1
+		}
+		temp = append(temp, num)
+		layer = temp
+	}
+	if res == math.MaxInt64 {
+		return 0
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(minSubArrayLen(0, []int{2, 3, 1, 2, 4, 3}))
+}
