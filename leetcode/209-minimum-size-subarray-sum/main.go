c7127343bdfd80620dfb2453bdab8db27711e6b1
calvinchankf <chan9118kin@gmail.com>
2019-01-23 15:38:14 +0800

    day143 q4 s3

--- leetcode/209-minimum-size-subarray-sum/main.go
+++ leetcode/209-minimum-size-subarray-sum/main.go
@@ -59,11 +59,11 @@ func minSubArrayLen(s int, nums []int) int {
 	- for each item, calculate from arr[i] to arr[j], if sum >= s and j-1+1<res, res = j - 1 + 1
 	Time	O(n^2)
 	Space O(1)
-	2488 ms beats 20.29%
+	96 ms beats 20.29%
 	23jan2019
 */
 func minSubArrayLen1(s int, nums []int) int {
-	res := math.MaxInt64
+	res := len(nums) + 1
 	for i := 0; i < len(nums); i++ {
 		sum := 0
 		for j := i; j < len(nums); j++ {
@@ -76,12 +76,47 @@ func minSubArrayLen1(s int, nums []int) int {
 			}
 		}
 	}
-	if res == math.MaxInt64 {
+	if res == len(nums)+1 {
+		return 0
+	}
+	return res
+}
+
+/*
+	3rd approach
+	- learned from others https://www.geeksforgeeks.org/minimum-length-subarray-sum-greater-given-value/
+	- 2 pointers
+	- fast pointer to find the next item which sum up > target
+	- once each the target, move the slow pointer to the right to see if the sum persist if sum = sum - nums[slow]
+	Time	O(n) < x < O(n^2)
+	Space O(1)
+	8ms ms beats 100%
+	23jan2019
+*/
+func minSubArrayLen2(s int, nums []int) int {
+	res := len(nums) + 1
+	sum := 0
+	slow := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		for sum >= s {
+			if i-slow+1 < res {
+				res = i - slow + 1
+			}
+			sum -= nums[slow]
+			slow++
+		}
+	}
+	if res == len(nums)+1 {
 		return 0
 	}
 	return res
 }
 
 func main() {
-	fmt.Println(minSubArrayLen1(6, []int{2, 3, 1, 2, 4, 3}))
+	fmt.Println(minSubArrayLen2(5, []int{2, 3, 1, 2, 4, 3}))
+	fmt.Println(minSubArrayLen2(6, []int{2, 3, 1, 2, 4, 3}))
+	fmt.Println(minSubArrayLen2(7, []int{2, 3, 1, 2, 4, 3}))
+	fmt.Println(minSubArrayLen2(9, []int{2, 3, 1, 2, 4, 3}))
+	fmt.Println(minSubArrayLen2(15, []int{1, 2, 3, 4, 5}))
 }
