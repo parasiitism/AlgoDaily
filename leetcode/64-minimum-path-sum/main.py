7a9e61d0cac44fe10a76323d0f976edd2e5e62a1
calvinchankf <chan9118kin@gmail.com>
2019-01-25 16:20:50 +0800

    Minimum Path Sum optimization

--- leetcode/64-minimum-path-sum/main.py
+++ leetcode/64-minimum-path-sum/main.py
@@ -25,9 +25,42 @@ class Solution(object):
                     dp[i][j] = grid[i][j] + min(dp[i][j-1], dp[i-1][j])
         return dp[len(grid)-1][len(grid[0])-1]
 
-print(Solution().minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
+class Solution1(object):
+    def minPathSum(self, grid):
+        """
+        3rd approach: optimize the 2nd approach
+	    - dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+        - actually u just need the previous layer
+	    - https://leetcode.com/articles/minimum-path-sum/
+        Time    O(m*n)
+        Space   O(n)
+        16jan2019
+        """
+        if len(grid) == 0 or len(grid[0]) == 0:
+            return 0
+        dp = []
+        for i in range(len(grid[0])):
+            dp.append(0)
 
-print(Solution().minPathSum(
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if i-1 < 0 and j-1 < 0:
+                    dp[j] = grid[i][j]
+                elif i-1 < 0:
+                    dp[j] = grid[i][j] + dp[j-1]
+                elif j-1 < 0:
+                    dp[j] = grid[i][j] + dp[j]
+                else:
+                    dp[j] = grid[i][j] + min(dp[j-1], dp[j])
+        return dp[-1]
+
+
+print(Solution1().minPathSum([]))
+print(Solution1().minPathSum([[]]))
+print(Solution1().minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
+print(Solution1().minPathSum([[1, 3, 1,4], [1, 5, 1,1], [4, 2, 1,2]]))
+
+print(Solution1().minPathSum(
 [
     [0, 3, 8, 5, 5, 1, 5, 3, 3, 6, 7, 5, 9, 4, 6, 5, 6, 8, 2, 4, 2, 5, 5, 3, 7, 4, 8, 5, 0, 2, 5, 2, 5, 5, 7, 2, 6, 3, 5, 1, 9, 4, 8, 8, 9, 5, 6, 7, 5, 0, 3, 9, 6, 9, 2, 5, 5, 2, 0, 8, 4, 7, 0, 2, 2, 0, 4, 1, 3, 2, 2, 4, 6, 1, 5, 7, 8, 1, 7, 3, 1, 0, 4, 9, 1, 6, 4, 9, 8, 6, 7, 2, 3, 9, 6, 8, 9, 3, 9, 4, 7, 3, 9, 3, 6, 4, 3, 4, 5, 2, 9, 8, 2, 3, 8, 4, 9, 4, 3, 9, 1, 2, 4, 4, 1, 0, 4, 3, 5, 5, 7, 2, 9, 6, 8, 5, 0, 1, 2, 7, 3, 1, 8, 7, 5, 8, 1, 6, 2, 6, 8, 3, 8, 4, 0, 2, 6, 4, 5, 2, 0, 2, 4, 9, 1, 2, 6, 3, 5, 8, 3, 0, 2, 1, 8, 9, 9, 1, 5, 1, 8, 5, 5, 8, 9, 5, 0, 6, 1, 7, 8, 1, 2, 4, 2, 3, 7, 9, 8, 2],
     [9, 1, 3, 1, 4, 1, 0, 3, 4, 8, 7, 2, 3, 4, 1, 5, 1, 3, 3, 2, 1, 3, 6, 3, 7, 8, 8, 4, 9, 6, 9, 9, 0, 2, 0, 4, 5, 1, 0, 9, 9, 7, 4, 4, 1, 7, 1, 4, 0, 4, 8, 3, 7, 4, 6, 5, 3, 4, 1, 4, 3, 0, 3, 3, 4, 4, 7, 1, 7, 7, 1, 8, 6, 7, 2, 9, 4, 4, 3, 4, 0, 2, 0, 8, 8, 6, 5, 1, 3, 6, 6, 8, 9, 1, 1, 3, 5, 0, 5, 4, 0, 8, 2, 8, 5, 7, 8, 1, 3, 3, 7, 3, 7, 7, 3, 6, 4, 8, 9, 9, 7, 5, 7, 6, 9, 0, 9, 6, 2, 6, 1, 4, 6, 3, 3, 1, 2, 3, 2, 5, 8, 2, 1, 6, 9, 4, 2, 5, 3, 1, 4, 0, 9, 3, 8, 8, 5, 7, 4, 8, 6, 5, 4, 2, 1, 9, 6, 5, 4, 0, 1, 3, 2, 2, 9, 2, 6, 1, 7, 1, 4, 4, 3, 3, 9, 1, 3, 5, 1, 8, 5, 7, 5, 9, 9, 8, 9, 5, 4, 3],
