1a37275abd3b57c6203fd4db22f9a4c69f15f65d
calvinchankf <chan9118kin@gmail.com>
2019-01-31 10:36:10 +0800

    day151 q1 s2

--- leetcode/396-rotate-function/main.go
+++ leetcode/396-rotate-function/main.go
@@ -46,13 +46,44 @@ func maxRotateFunction(A []int) int {
 	return max
 }
 
+/*
+	2nd approach
+	- learned from others https://leetcode.com/problems/rotate-function/discuss/87842/Java-Solution-O(n)-with-non-mathametical-explaination
+	F(0) = (0A) + (1B) + (2C) + (3D) + (4E)
+				 (-1A) + (1B) + (2C) + (3D) + (4E)
+	F(1) = (4A) + (0B) + (1C) + (2D) + (3E)
+				 (3A) + (-1B) + (0C) + (1D) + (2E)
+	F(2) = (3A) + (4B) + (0C) + (1D) + (2E)
+	Time		O(2n)
+	Space		O(n)
+	8ms beats 100%
+*/
+func maxRotateFunction1(A []int) int {
+	sum := 0
+	f := 0
+	// calculate the sum
+	for i := 0; i < len(A); i++ {
+		sum += A[i]
+		f += i * A[i]
+	}
+	// calculate the iteration
+	max := f
+	for i := 1; i < len(A); i++ {
+		f = f - sum + A[i-1]*len(A)
+		if f > max {
+			max = f
+		}
+	}
+	return max
+}
+
 func main() {
 	a := []int{4, 3, 2, 6, 7}
-	fmt.Println(maxRotateFunction(a))
+	fmt.Println(maxRotateFunction1(a))
 
 	a = []int{}
-	fmt.Println(maxRotateFunction(a))
+	fmt.Println(maxRotateFunction1(a))
 
 	a = []int{math.MinInt32, math.MinInt32}
-	fmt.Println(maxRotateFunction(a))
+	fmt.Println(maxRotateFunction1(a))
 }
