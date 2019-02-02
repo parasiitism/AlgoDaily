6767db0d6f458d9e915f15f99521c0e49b6b5702
calvinchankf <chan9118kin@gmail.com>
2019-02-02 09:56:18 +0800

    day153 q1 s2

--- leetcode/692-top-k-frequent-words/main.go
+++ leetcode/692-top-k-frequent-words/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 )
@@ -64,22 +65,101 @@ func topKFrequent(words []string, k int) []string {
 			}
 		}
 	}
-
 	return res
+}
+
+/*
+	2nd approach
+	1. count num: freq into a hashtable
+	2. use a heap to sort by freq order by alphabet
+	3. the first k elements are the top k elements in the heap
+
+	takeaway: sort things with priority queue using frequency WITH other params, such as 'Word'
+	to accomplish `sort and order by`
+
+	Time	O(nlogn)
+	Space	O(n)
+	8ms beats 100%
+	2feb2019
+*/
+func topKFrequent1(words []string, k int) []string {
+	if k > len(words) {
+		return []string{}
+	}
+	ht := make(map[string]int)
+	pq := &PriorityQueue{}
+	heap.Init(pq)
+	// count the freq for each num
+	for _, word := range words {
+		if _, x := ht[word]; x {
+			ht[word]++
+		} else {
+			ht[word] = 1
+		}
+	}
+	// put the num: freq into a prioroity queue
+	for key, value := range ht {
+		heap.Push(pq, &Item{value, key})
+	}
+	// pop the first k element from the priority queue
+	res := []string{}
+	for i := 0; i < k; i++ {
+		item := heap.Pop(pq).(*Item)
+		res = append(res, item.Word)
+	}
+	return res
+}
+
+/*
+	Implement Priority Queue
+*/
+
+type Item struct {
+	Freq int
+	Word string
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	if pq[i].Freq == pq[j].Freq {
+		return pq[i].Word < pq[j].Word
+	}
+	return pq[i].Freq > pq[j].Freq
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
 }
 
 func main() {
 
 	a := []string{}
-	fmt.Println(topKFrequent(a, 3))
+	fmt.Println(topKFrequent1(a, 3))
 
 	a = []string{
 		"i", "love", "leetcode", "i", "love", "coding", "cat", "cat",
 	}
-	fmt.Println(topKFrequent(a, 2))
+	fmt.Println(topKFrequent1(a, 3))
 
 	a = []string{
 		"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is",
 	}
-	fmt.Println(topKFrequent(a, 4))
+	fmt.Println(topKFrequent1(a, 4))
 }
