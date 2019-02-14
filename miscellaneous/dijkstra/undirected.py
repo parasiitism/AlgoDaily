5dd7445233c99e33dda90fb19ce303ee295f2e50
calvinchankf <chan9118kin@gmail.com>
2019-02-14 18:54:40 +0800

    update day161 dijkstra on undirected graph

--- miscellaneous/dijkstra/undirected.py
+++ miscellaneous/dijkstra/undirected.py
@@ -20,12 +20,16 @@ def dijkstra(edges, p, q):
     nodes = list(nodeSet)
     # adjacent list
     connections = {}
-    # edge with cost (from, to, cost)
-    for src, dest, cost in edges:
-        if src in connections:
-            connections[src].append((src, dest, cost))
+    # edge with cost (a, a, cost)
+    for a, b, cost in edges:
+        if a in connections:
+            connections[a].append((a, b, cost))
         else:
-            connections[src] = [(src, dest, cost)]
+            connections[a] = [(a, b, cost)]
+        if b in connections:
+            connections[b].append((b, a, cost))
+        else:
+            connections[b] = [(b, a, cost)]
     # dijkstra's
     # put (cost, node, visitedpath) into the heap
     heap = [(0, p, [])]
@@ -47,15 +51,17 @@ def dijkstra(edges, p, q):
 
 
 # normal case: https://www.youtube.com/watch?v=_lHSawdgXpI
+# but
+# 1. change it to a undirected graph
+# 2. change C-E from 5 to 4
 r = [
     ['A', 'B', 4],
     ['A', 'C', 2],
     ['B', 'C', 3],
-    ['C', 'B', 1],
     ['B', 'D', 2],
     ['B', 'E', 3],
     ['C', 'D', 4],
-    ['C', 'E', 5],
+    ['C', 'E', 4],
     ['D', 'E', 1],
 ]
 print(dijkstra(r, 'A', 'E'))
--- readme.md
+++ readme.md
@@ -318,6 +318,7 @@ tree | [leetcode 889](https://leetcode.com/problems/serialize-and-deserialize-bi
 | 160 | [Find Distinct Substrings with Exactly K Distinct Characters](/interviews/amazon/distinct-substrings-k-distinct-characters) | array | | 📌1st O(n^3), 2nd O(n^2) |
 | 161 | [Reorder Log Files](/leetcode/937-reorder-log-files) | array | [leetcode 937](https://leetcode.com/problems/reorder-log-files/) | 1st O(nlogn) |
 | 161 | [Dijkstra's Algorithm on a Directed Graph](/miscellaneous/dijkstra) | graph | study | 📌 |
+| 161 | [Dijkstra's Algorithm on an Undirected Graph](/miscellaneous/dijkstra) | graph | study | 📌 |
 | 161 | [Find All Anagrams in a String](/leetcode/438-find-all-anagrams-in-a-string) | string | [leetcode 438](https://leetcode.com/problems/find-all-anagrams-in-a-string/) | 1st O(kn) |
 | 162 | [Optimal Flights](/interviews/amazon/closest-2-sum) | binary search | interview | 1st O(n^2), 2nd O(nlogn) |
 | 162 | [Max Area of Island](/leetcode/695-max-area-of-island) | array | [leetcode 695](https://leetcode.com/problems/max-area-of-island/) | 1st O(n) |
