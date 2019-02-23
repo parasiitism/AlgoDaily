d283f7a5c2b877bc18db00d8bc41a24813c4410a
calvinchankf <chan9118kin@gmail.com>
2019-02-23 16:00:44 +0800

    day171 q2 s3

--- leetcode/15-3sum/main.go
+++ leetcode/15-3sum/main.go
@@ -89,7 +89,53 @@ func threeSum1(nums []int) [][]int {
 	return res
 }
 
+/*
+	3rd approach: optimize the 2nd approach and dont use the hashtable to deduplicate
+	- https://leetcode.com/problems/3sum/discuss/7380/Concise-O(N2)-Java-solution
+
+	Time	O(n^2)
+	Space O(n) <-result array
+	932 ms, faster than 64.31%
+*/
+func threeSum2(nums []int) [][]int {
+	sort.Ints(nums)
+	res := [][]int{}
+	for i := 0; i < len(nums); i++ {
+		// deduplicate:
+		// dont do 2sum if the current num == last num
+		// but dont skip when index == 0, cos nums[i-1] will be out of boundary
+		if i == 0 || nums[i] > nums[i-1] {
+			j := i + 1
+			k := len(nums) - 1
+			for j < k {
+				temp := nums[i] + nums[j] + nums[k]
+				if temp == 0 {
+					o := []int{nums[i], nums[j], nums[k]}
+					res = append(res, o)
+					// deduplicate:
+					// imagine [1,1,2,3,4,4], target 5
+					// we need to skip the 1(index 1) and 4(index 4) and check if 2+3 = 5
+					for j < k && nums[j] == nums[j+1] {
+						j++
+					}
+					for j < k && nums[k-1] == nums[k] {
+						k--
+					}
+					j++
+					k--
+				} else if temp < 0 {
+					j++
+				} else {
+					k--
+				}
+			}
+		}
+	}
+	return res
+}
+
 func main() {
 	fmt.Println(threeSum1([]int{-1, 0, 1, 2, -1, -4}))
 	fmt.Println(threeSum1([]int{-2, 0, 1, 1, 2}))
+	fmt.Println(threeSum1([]int{-13, 5, 13, 12, -2, -11, -1, 12, -3, 0, -3, -7, -7, -5, -3, -15, -2, 14, 14, 13, 6, -11, -11, 5, -15, -14, 5, -5, -2, 0, 3, -8, -10, -7, 11, -5, -10, -5, -7, -6, 2, 5, 3, 2, 7, 7, 3, -10, -2, 2, -12, -11, -1, 14, 10, -9, -15, -8, -7, -9, 7, 3, -2, 5, 11, -13, -15, 8, -3, -7, -12, 7, 5, -2, -6, -3, -10, 4, 2, -5, 14, -3, -1, -10, -3, -14, -4, -3, -7, -4, 3, 8, 14, 9, -2, 10, 11, -10, -4, -15, -9, -1, -1, 3, 4, 1, 8, 1}))
 }
--- readme.md
+++ readme.md
@@ -353,4 +353,4 @@ tree | [leetcode 889](https://leetcode.com/problems/serialize-and-deserialize-bi
 | 170 | [Find Duplicate Subtrees](/leetcode/652-find-duplicate-subtrees) | hashtable, tree | [leetcode 652](https://leetcode.com/problems/find-duplicate-subtrees/) | 1st O(n) |
 | 170 | [Group Shifted Strings](/leetcode/249-group-shifted-strings) | hashtable | [leetcode 249](https://leetcode.com/problems/group-shifted-strings/) | O(n) |
 | 171 | [K-th Symbol in Grammar](/leetcode/779-k-th-symbol-in-grammar) | hashtable | [leetcode 779](https://leetcode.com/problems/k-th-symbol-in-grammar/) | 1st LTE. 📌2nd tricky recursion. see ./idea.jpeg |
-| 171 | [3Sum](/leetcode/15-3sum) | hashtable, 2pointers | [leetcode 779](https://leetcode.com/problems/3sum/) | 📌1st hashtable O(n^2) beats 18%. 2nd 2pointers O(n^2) beats 27% |
+| 171 | [3Sum](/leetcode/15-3sum) | hashtable, 2pointers | [leetcode 779](https://leetcode.com/problems/3sum/) | 📌1st hashtable O(n^2) beats 18%. 📌2nd 2pointers O(n^2) beats 27%, 📌3rd 2pointers O(n^2) beats 54% |
