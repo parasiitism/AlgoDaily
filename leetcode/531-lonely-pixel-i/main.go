0db7886887cc079b96e20b86b9b4d447b4e00e9f
calvinchankf <chan9118kin@gmail.com>
2019-02-25 15:39:38 +0800

    day173 q2 s2

--- leetcode/531-lonely-pixel-i/main.go
+++ leetcode/531-lonely-pixel-i/main.go
@@ -51,6 +51,47 @@ func calColCache(picture [][]byte, col int, cache map[int]int) {
 	cache[col] = count
 }
 
+/*
+	1st approach: hashtable
+	1. for row, if there are 'B', calculate the number of 'B' for those columns
+	2. if that row only has 1 'B', check if there is only one 'B' on that column
+	3. be careful of 'one column checking'
+
+	Time		O(m*n)
+	Space		O(n)
+	4 ms, faster than 100.00%
+*/
+func findLonelyPixel1(picture [][]byte) int {
+	if len(picture) == 0 || len(picture[0]) == 0 {
+		return 0
+	}
+	rowCount := []int{}
+	for i := 0; i < len(picture); i++ {
+		rowCount = append(rowCount, 0)
+	}
+	colCount := []int{}
+	for i := 0; i < len(picture[0]); i++ {
+		colCount = append(colCount, 0)
+	}
+	for i := 0; i < len(picture); i++ {
+		for j := 0; j < len(picture[0]); j++ {
+			if picture[i][j] == 'B' {
+				rowCount[i]++
+				colCount[j]++
+			}
+		}
+	}
+	res := 0
+	for i := 0; i < len(picture); i++ {
+		for j := 0; j < len(picture[0]); j++ {
+			if picture[i][j] == 'B' && rowCount[i] == 1 && colCount[j] == 1 {
+				res++
+			}
+		}
+	}
+	return res
+}
+
 func main() {
 
 }
