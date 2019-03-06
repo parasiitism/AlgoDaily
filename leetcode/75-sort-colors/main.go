8a462c894d10677b8af9cf1900fb60e99b423f26
calvinchankf <chan9118kin@gmail.com>
2019-03-06 14:04:02 +0800

    day183 q4 s2

--- leetcode/75-sort-colors/main.go
+++ leetcode/75-sort-colors/main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 /*
-	naive approach: built-in sort / merge sort
+	questions to ask:
+	- only 0,1,2? yes
 */
 
 /*
@@ -29,9 +30,52 @@ func sortColors(nums []int) {
 	}
 }
 
+/*
+	2nd approach: merge sort
+
+	Time	O(nlogn)
+	Space	O(n)
+	0 ms, faster than 100.00%
+*/
+func sortColors1(nums []int) {
+	temp := mergeSort(nums)
+	for i := 0; i < len(nums); i++ {
+		nums[i] = temp[i]
+	}
+}
+
+func mergeSort(nums []int) []int {
+	// divide
+	if len(nums) == 1 {
+		return nums
+	}
+	mean := len(nums) / 2
+	arr1 := mergeSort(nums[:mean])
+	arr2 := mergeSort(nums[mean:])
+	// merge 2 sorted arrays
+	i, j := 0, 0
+	result := []int{}
+	for i < len(arr1) && j < len(arr2) {
+		if arr1[i] < arr2[j] {
+			result = append(result, arr1[i])
+			i++
+		} else {
+			result = append(result, arr2[j])
+			j++
+		}
+	}
+	if i < len(arr1) {
+		result = append(result, arr1[i:]...)
+	}
+	if j < len(arr2) {
+		result = append(result, arr2[j:]...)
+	}
+	return result
+}
+
 func main() {
 	a := []int{2, 0, 1, 2, 0, 1}
-	sortColors(a)
+	sortColors1(a)
 	fmt.Println(a)
 
 	a = []int{}
