f6f6f3beadd28076105daf0ae0092b28199df5fd
calvinchankf <chan9118kin@gmail.com>
2019-03-10 14:35:56 +0800

    revise leetcode973 s4

--- leetcode/973-k-closest-points-to-origin/main.py
+++ leetcode/973-k-closest-points-to-origin/main.py
@@ -1,4 +1,5 @@
 import heapq
+import random
 
 
 class Solution(object):
@@ -46,7 +47,7 @@ class Solution(object):
 
         2nd approach: use builtin timsort
 
-        Time    O(nlogn)
+        Time    O(2n+nlogn)
         Space   O(n)
         396 ms, faster than 69.73%
         10mar2019
@@ -76,17 +77,73 @@ class Solution(object):
         :rtype: List[List[int]]
 
 
-        3rd approach: 
+        3rd approach:
         - optimize the 2nd
         - use builtin timsort
 
         Time    O(nlogn)
         Space   O(n)
-        392 ms, faster than 72.84% 
+        392 ms, faster than 72.84%
         10mar2019
         """
         points = sorted(
             points, key=lambda x: x[0]*x[0] + x[1]*x[1])
+        return points[:K]
+
+
+print(Solution().kClosest([[1, 3], [-2, 2]], 1))
+print(Solution().kClosest([[1, 3], [-2, 2]], 3))
+print(Solution().kClosest([[3, 3], [5, -1], [-2, 4]], 2))
+
+
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+
+
+        3rd approach:
+        - optimize the 2nd
+        - use builtin timsort
+
+        Time    O(nlogn) -> O(n)
+        Space   O(n)
+        436 ms, faster than 43.33%
+        10mar2019
+        """
+        def dist(x): return points[x][0]**2 + points[x][1]**2
+
+        def quickselect(l, r, K):
+            while l < r:
+                # To avoid the worst case
+                i = random.randint(l, r)
+                points[i], points[l] = points[l], points[i]
+                mid = partition(l, r)
+                if K > mid:
+                    l = mid+1
+                elif K < mid:
+                    r = mid-1
+                else:
+                    break
+
+        def partition(l, r):
+            i = l  # start index
+            pivot = dist(i)
+            l += 1
+            while True:
+                while l < r and dist(l) < pivot:
+                    l += 1
+                while l <= r and dist(r) >= pivot:
+                    r -= 1
+                if l >= r:
+                    break
+                points[l], points[r] = points[r], points[l]
+            points[r], points[i] = points[i], points[r]
+            return r
+
+        quickselect(0, len(points)-1, K)
         return points[:K]
 
 
