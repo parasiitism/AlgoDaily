c3ca9abe5b68991dcbba8f4f934417630a615ba2
calvinchankf <chan9118kin@gmail.com>
2019-03-20 10:49:49 +0800

    revise binary search

--- miscellaneous/binary-searches/main.go
+++ miscellaneous/binary-searches/main.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 )
 
-// common
-// 12345, search for 4
-// ans = index = 3
+/*
+	my related article
+	- https://medium.com/@CalvinChankf/how-to-deal-with-lower-upper-bound-binary-search-b9ce744673df
+*/
+
+/*
+	common
+ 	12345, search for 4
+ 	ans = index = 3
+*/
 func CommonBinarySearch(arr []int, target int) int {
 	min := 0
 	max := len(arr) - 1
@@ -23,13 +30,39 @@ func CommonBinarySearch(arr []int, target int) int {
 	return -1
 }
 
-// find the number than smaller or equal to the target
-// e.g. 1
-// 12345, search for 4
-// ans = index = 3
-// e.g. 2
-// 1357, search for 4
-// ans = index = 1
+// recursive common
+func RecursiveBinarySearch(arr []int, target int) int {
+	min := 0
+	max := len(arr) - 1
+	return recursion(arr, min, max, target)
+}
+
+// same as above but go over less paths and more concise
+func recursion(arr []int, min int, max int, target int) int {
+	if min > max {
+		return -1
+	}
+	mean := (min + max) / 2
+	if arr[mean] == target {
+		return mean
+	}
+	if target < arr[mean] {
+		return recursion(arr, min, mean-1, target)
+	}
+	return recursion(arr, mean+1, max, target)
+}
+
+/*
+	find the number than smaller or equal to the target
+
+	e.g. 1
+	12345, search for 4
+	ans = index = 3
+
+	e.g. 2
+	1357, search for 4
+	ans = index = 1
+*/
 func CommonBinarySearchNoLargerThanTarget(arr []int, target int) int {
 	min := 0
 	max := len(arr) - 1
@@ -46,13 +79,17 @@ func CommonBinarySearchNoLargerThanTarget(arr []int, target int) int {
 	return max
 }
 
-// find the number than smaller or equal to the target
-// e.g. 1
-// 12345, search for 4
-// ans = index = 3
-// e.g. 2
-// 1357, search for 4
-// ans = index = 2
+/*
+	EndingPositionBinarySearch find the number than smaller or equal to the target
+
+	e.g. 1
+	12345, search for 4
+	ans = index = 3
+
+	e.g. 2
+	1357, search for 4
+	ans = index = 2
+*/
 func CommonBinarySearchNoSmallerThanTarget(arr []int, target int) int {
 	min := 0
 	max := len(arr) - 1
@@ -72,10 +109,12 @@ func CommonBinarySearchNoSmallerThanTarget(arr []int, target int) int {
 	return min
 }
 
-// lower bound
-// 1233345, search for 3
-// ans = index = 2
-func LowerBoundBinarySearch(arr []int, target int) int {
+/*
+	Starting Position Binary Search, also known as Lower Bound Binary Search
+	1233345, search for 3
+	ans = index = 2
+*/
+func StartingPositionBinarySearch(arr []int, target int) int {
 	min := 0
 	max := len(arr) - 1
 	for min < max {
@@ -86,16 +125,41 @@ func LowerBoundBinarySearch(arr []int, target int) int {
 			min = mean + 1
 		}
 	}
+	// we an either return min or max because they equal to each other after the forloop
 	if arr[min] == target {
 		return min
 	}
 	return -1
 }
 
-// upper bound
-// 1233345, search for 3
-// ans = index = 4
-func UpperBoundBinarySearch(arr []int, target int) int {
+// recursive Starting Position Binary Search
+func RecursiveStartingPositionBinarySearch(arr []int, target int) int {
+	min := 0
+	max := len(arr) - 1
+	res := startPosHelper(arr, target, min, max)
+	if arr[res] == target {
+		return res
+	}
+	return -1
+}
+
+func startPosHelper(arr []int, target, min, max int) int {
+	if min >= max {
+		return min
+	}
+	mean := (min + max) / 2
+	if target <= arr[mean] {
+		return startPosHelper(arr, target, min, mean)
+	}
+	return startPosHelper(arr, target, mean+1, max)
+}
+
+/*
+	Ending Position Binary Search = upper bound binary search - 1
+	1233345, search for 3
+	ans = index = 4
+*/
+func EndingPositionBinarySearch(arr []int, target int) int {
 	min := 0
 	max := len(arr) - 1
 	for min < max {
@@ -106,53 +170,33 @@ func UpperBoundBinarySearch(arr []int, target int) int {
 			max = mean - 1
 		}
 	}
+	// we an either return min or max because they equal to each other after the forloop
 	if arr[max] == target {
 		return max
 	}
 	return -1
 }
 
-// recursive
-func RecursiveBinarySearch(arr []int, target int) int {
+// recursive Ending Position Binary Search
+func RecursiveEndingPositionBinarySearch(arr []int, target int) int {
 	min := 0
 	max := len(arr) - 1
-	return recursion(arr, min, max, target)
+	res := endingPosHelper(arr, target, min, max)
+	if arr[res] == target {
+		return res
+	}
+	return -1
 }
 
-// write this outside the main function,
-// so to understand the logic of returning
-
-/* go over all the paths
-func recursion(arr []int, min int, max int, target int) int {
-	if min > max {
-		return -1
+func endingPosHelper(arr []int, target, min, max int) int {
+	if min >= max {
+		return min
 	}
-	mean := (min + max) / 2
-	if arr[mean] == target {
-		return mean
+	mean := (min + max + 1) / 2
+	if target >= arr[mean] {
+		return endingPosHelper(arr, target, mean, max)
 	}
-	left := recursion(arr, min, mean-1, target)
-	right := recursion(arr, mean+1, max, target)
-	if left != -1 {
-		return left
-	}
-	return right
-}
-*/
-
-// same as above but go over less paths and more concise
-func recursion(arr []int, min int, max int, target int) int {
-	if min > max {
-		return -1
-	}
-	mean := (min + max) / 2
-	if arr[mean] == target {
-		return mean
-	}
-	if target < arr[mean] {
-		return recursion(arr, min, mean-1, target)
-	}
-	return recursion(arr, mean+1, max, target)
+	return endingPosHelper(arr, target, min, mean-1)
 }
 
 func main() {
@@ -186,11 +230,32 @@ func main() {
 	fmt.Println(CommonBinarySearchNoSmallerThanTarget(b, 8))
 
 	// boundaries
-	fmt.Println("----boundaries----")
+	fmt.Println("----lower bound---")
 	c := []int{1, 2, 3, 3, 3, 4, 6}
-	fmt.Println(LowerBoundBinarySearch(c, 6))
-	fmt.Println(UpperBoundBinarySearch(c, 6))
-}
+	fmt.Println(StartingPositionBinarySearch(c, 3))
+	fmt.Println(StartingPositionBinarySearch(c, 1))
+	fmt.Println(StartingPositionBinarySearch(c, 6))
+	fmt.Println(StartingPositionBinarySearch(c, -2))
+	fmt.Println(StartingPositionBinarySearch(c, 7))
 
-// no test cases, i am lazy
-// https://medium.com/@CalvinChankf/how-to-deal-with-lower-upper-bound-binary-search-b9ce744673df
+	fmt.Println("----recursive lower bound---")
+	fmt.Println(RecursiveStartingPositionBinarySearch(c, 3))
+	fmt.Println(RecursiveStartingPositionBinarySearch(c, 1))
+	fmt.Println(RecursiveStartingPositionBinarySearch(c, 6))
+	fmt.Println(RecursiveStartingPositionBinarySearch(c, -2))
+	fmt.Println(RecursiveStartingPositionBinarySearch(c, 7))
+
+	fmt.Println("----upper bound -1 ---")
+	fmt.Println(EndingPositionBinarySearch(c, 3))
+	fmt.Println(EndingPositionBinarySearch(c, 1))
+	fmt.Println(EndingPositionBinarySearch(c, 6))
+	fmt.Println(EndingPositionBinarySearch(c, -2))
+	fmt.Println(EndingPositionBinarySearch(c, 7))
+
+	fmt.Println("----recursive upper bound -1 ---")
+	fmt.Println(RecursiveEndingPositionBinarySearch(c, 3))
+	fmt.Println(RecursiveEndingPositionBinarySearch(c, 1))
+	fmt.Println(RecursiveEndingPositionBinarySearch(c, 6))
+	fmt.Println(RecursiveEndingPositionBinarySearch(c, -2))
+	fmt.Println(RecursiveEndingPositionBinarySearch(c, 7))
+}
