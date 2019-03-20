7e52bea5cd52922ae3acb24e8a84b35ea7444510
calvinchankf <chan9118kin@gmail.com>
2019-03-20 11:12:01 +0800

    revise day196

--- leetcode/378-kth-smallest-element-in-a-sorted-matrix/main.py
+++ leetcode/378-kth-smallest-element-in-a-sorted-matrix/main.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Solution(object):
     def kthSmallest(self, matrix, k):
         """
@@ -9,7 +12,7 @@ class Solution(object):
 
         Time    O(nlogn)
         Space   O(n)
-        244 ms, faster than 15.37%
+        48 ms, faster than 73.06%
         """
         if len(matrix) == 0 or len(matrix[0]) == 0:
             return None
@@ -19,6 +22,41 @@ class Solution(object):
                 arr.append(matrix[i][j])
         arr = sorted(arr)
         return arr[k-1]
+
+
+a = [
+    [1,  5,  9],
+    [10, 11, 13],
+    [12, 13, 15]
+]
+print(Solution().kthSmallest(a, 8))
+
+
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+
+        1st approach: max heap
+        - use a max heap with limited capacity k
+        - when we meet a new element and the heap overflows, 
+        we pop the toppest element such that we can maintain the heap contains the k smallest elements only
+
+        Time    O(nlogn)
+        Space   O(n)
+        248 ms, faster than 14.97%
+        """
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return None
+        arr = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                heapq.heappush(arr, -matrix[i][j])
+                if len(arr) > k:
+                    heapq.heappop(arr)
+        return -arr[0]
 
 
 a = [
