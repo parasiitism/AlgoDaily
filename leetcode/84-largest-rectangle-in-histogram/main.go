2810f302034177020fa127b7969137fb0df9d8c2
calvinchankf <chan9118kin@gmail.com>
2019-03-21 14:39:24 +0800

    day198 q3 s2

--- leetcode/84-largest-rectangle-in-histogram/main.go
+++ leetcode/84-largest-rectangle-in-histogram/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 /*
 	1st approach: brute force
@@ -35,6 +38,47 @@ func findMin(a, b int) int {
 	return b
 }
 
-func main() {
+/*
+	2nd approach: stack <= learned from others
+	- use a stack to store the indeces where the corresponding height can create a potential largest retangle
 
+	ref:
+	- https://www.youtube.com/watch?v=ZmnqCZp9bBs
+
+	Time	O(n)
+	Space	O(1)
+	8 ms, faster than 100.00%
+*/
+func largestRectangleArea1(heights []int) int {
+	res := 0
+	if len(heights) == 0 {
+		return 0
+	}
+	stack := []int{-1, 0}
+	for i := 1; i < len(heights); i++ {
+		cur := heights[i]
+		for len(stack) > 1 && cur < heights[stack[len(stack)-1]] {
+			idx := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			temp := heights[idx] * (i - stack[len(stack)-1] - 1)
+			if temp > res {
+				res = temp
+			}
+		}
+		stack = append(stack, i)
+	}
+	for len(stack) > 1 {
+		idx := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		temp := heights[idx] * (len(heights) - stack[len(stack)-1] - 1)
+		if temp > res {
+			res = temp
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(largestRectangleArea1([]int{2, 1, 5, 6, 2, 3}))
+	fmt.Println(largestRectangleArea1([]int{12, 23, 12, 25, 3, 16}))
 }
--- leetcode/84-largest-rectangle-in-histogram/main.py
+++ leetcode/84-largest-rectangle-in-histogram/main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Solution(object):
     def largestRectangleArea(self, heights):
         """
@@ -21,3 +24,58 @@ class Solution(object):
                 res = max(res, temp)
                 cnt += 1
         return res
+
+
+print(Solution().largestRectangleArea([]))
+print(Solution().largestRectangleArea([2]))
+print(Solution().largestRectangleArea([2, 1]))
+print(Solution().largestRectangleArea([2, 1, 5, 6, 2, 3]))
+print(Solution().largestRectangleArea([12, 23, 12, 25, 3, 16]))
+
+print("-----")
+
+
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+
+        2nd approach: stack <= learned from others 
+        - use a stack to store the indeces where the corresponding height can create a potential largest retangle
+
+        ref:
+        - https://www.youtube.com/watch?v=ZmnqCZp9bBs
+
+        Time	O(n)
+        Space	O(1)
+        168 ms, faster than 5.14% <= the code is similar to the one beats 100% so i am not sure why so slow
+        """
+        res = 0
+        if len(heights) == 0:
+            return 0
+        stack = [-1, 0]
+        # iterate  through the list
+        for i in range(1, len(heights)):
+            cur = heights[i]
+            if cur >= heights[stack[-1]]:
+                stack.append(i)
+            else:
+                while len(stack) > 1 and cur < heights[stack[-1]]:
+                    idx = stack.pop()
+                    # i-stack.peek()-1 is the width
+                    temp = heights[idx] * (i-stack[-1]-1)
+                    res = max(res, temp)
+                stack.append(i)
+        while len(stack) > 1:
+            idx = stack.pop()
+            temp = heights[idx] * (len(heights)-stack[-1]-1)
+            res = max(res, temp)
+        return res
+
+
+print(Solution().largestRectangleArea([]))
+print(Solution().largestRectangleArea([2]))
+print(Solution().largestRectangleArea([2, 1]))
+print(Solution().largestRectangleArea([2, 1, 5, 6, 2, 3]))
+print(Solution().largestRectangleArea([12, 23, 12, 25, 3, 16]))
