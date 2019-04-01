7e5e633cbc2b5f5a2e138815fa865229e8775ed5
calvinchankf <chan9118kin@gmail.com>
2019-04-01 18:01:43 +0800

    revise lc480

--- leetcode/480-sliding-window-median/main.py
+++ leetcode/480-sliding-window-median/main.py
@@ -1,3 +1,6 @@
+import bisect
+
+
 class Solution(object):
     def medianSlidingWindow(self, nums, k):
         """
@@ -8,33 +11,27 @@ class Solution(object):
         1st approach
         - similar to window sum
         - but for each iteration, sort the nums
-        Time    O(n*nlogn)
-        Space   O(kn)
-        7688ms beats 21.74%
+        Time    O(n*klogk)
+        Space   O(k)
+        TLE
         """
         if len(nums) == 0 or k <= 0 or k > len(nums):
             return 0
-        acc = []
         res = []
-        # first item
-        temp = []
+        # first result(first k items' median)
+        window = []
         for i in range(k):
-            temp.append(nums[i])
-        acc.append(temp)
-        res.append(self.calMedian(temp))
+            window.append(nums[i])
+        res.append(self.calMedian(window[:]))
 
         # 2nd item -> end
         for i in range(k, len(nums)):
-            # extract the last one from acc
-            # remove the first num from acc and add the current num to the acc
+            # remove the first num from window and add the current num to the window
             # e.g. [1,2,3], 4=> 1, [2,3,4]
-            clone = acc[-1][:]
-            clone = clone[1:]
-            clone.append(nums[i])
-            # append to acc
-            acc.append(clone)
-            # cal median
-            res.append(self.calMedian(clone))
+            window.pop(0)
+            window.append(nums[i])
+            # copy window and cal median of the copy
+            res.append(self.calMedian(window[:]))
         return res
 
     def calMedian(self, nums):
@@ -44,6 +41,62 @@ class Solution(object):
             return (sortNums[half-1]+sortNums[half])/2.0
         half = len(sortNums)/2
         return float(sortNums[half])
+
+
+print(Solution().medianSlidingWindow([1, 2, 3, 4, 5, 6, 7], 3))
+print(Solution().medianSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
+print(Solution().medianSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 4))
+
+print("----------------------------------------")
+
+
+class Solution(object):
+    def medianSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[float]
+
+        2nd approach
+        - similar to 1st approach but use binary search to 
+            - pop the left most item
+            - push the right most item to the window
+        - therefore to optimize the window sliding
+
+        Time    O(n*2logk)
+        Space   O(k)
+
+        200 ms, faster than 57.24%
+        """
+        if len(nums) == 0 or k <= 0 or k > len(nums):
+            return 0
+        medians, window = [], []
+
+        for i in range(len(nums)):
+
+            # find position where outgoing element should be removed from
+            if i >= k:
+                # O(n) remove
+                # window.remove(nums[i-k])
+
+                # O(logn) remove with binary search
+                idx = bisect.bisect_left(window, nums[i - k])
+                window.pop(idx)
+
+            # maintain the sorted invariant while inserting incoming element with binary search
+            bisect.insort(window, nums[i])
+
+            # find the medians
+            if i >= k - 1:
+                half = k/2
+                temp = 0.0
+                if k % 2 == 0:
+                    temp = (window[half-1] + window[half]) / 2.0
+                else:
+                    temp = float(window[half])
+                medians.append(temp)
+
+        return medians
 
 
 print(Solution().medianSlidingWindow([1, 2, 3, 4, 5, 6, 7], 3))
--- readme.md
+++ readme.md
@@ -287,7 +287,7 @@ tree | [leetcode 889](https://leetcode.com/problems/serialize-and-deserialize-bi
 | 151 | [Rotate List](/leetcode/61-rotate-list/) | linked list | [leetcode 61](https://leetcode.com/problems/rotate-list/) | ️1st O(n) beats 100% |
 | 151 | [Arithmetic Slices](/leetcode/413-arithmetic-slices/) | array | [leetcode 413](https://leetcode.com/problems/arithmetic-slices/) | ️1st O(n) beats 100% |
 | 152 | [Round Robin](/interviews/amazon/round-robin/) | queue | glassdoor | ️📌📌📌 |
-| 152 | [Sliding Window Median](/leetcode/480-sliding-window-median/) | array | [leetcode 480](https://leetcode.com/problems/sliding-window-median/) | ⭐️ 1st O(n*nlogn) 100% |
+| 152 | [Sliding Window Median](/leetcode/480-sliding-window-median/) | array, binary search | [leetcode 480](https://leetcode.com/problems/sliding-window-median/) | 1st TLE 📌2nd binary search O(n2logn) |
 | 152 | [Permutation in String](/leetcode/567-permutation-in-string/) | hashtable, array, string | [leetcode 480](https://leetcode.com/problems/permutation-in-string/) | ⭐️ 1st O(n^2) hashtable beats  0%, 2nd O(n^2) `[26]int{}` beats 40% |
 | 152 | [Set Matrix Zeroes](/leetcode/73-set-matrix-zeroes/) | hashtable, array | [leetcode 73](https://leetcode.com/problems/set-matrix-zeroes/) | 1st O(n) beats 52% |
 | 152 | [Top K Frequent Elements](/leetcode/347-top-k-frequent-elements/) | hashtable, bucket sort | [leetcode 347](https://leetcode.com/problems/top-k-frequent-elements/) | 1st O(nlogn) beats 64%. 2nd O(n) beats 64%. takeaway: use bucket sort for `frequency` questions, 3rd ~O(n) quick select |
