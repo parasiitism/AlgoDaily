babb7f3cabd00c566c989951574f3cba6bfa347c
calvinchankf <chan9118kin@gmail.com>
2019-04-08 13:44:10 +0800

    revise bst lowest common ancestor

--- leetcode/235-lowest-common-ancestor-of-a-binary-search-tree/main.py
+++ leetcode/235-lowest-common-ancestor-of-a-binary-search-tree/main.py
@@ -19,17 +19,21 @@ class Solution(object):
         :type p: TreeNode
         :type q: TreeNode
         :rtype: TreeNode
+
+        1nd approach: iterative bst dfs
+
+        Time    O(logn)
+        Time    O(logn) height of the bst
+        68 ms, faster than 99.7%
         """
-        curr = root
-        left = p if p.val < q.val else q
-        right = q if q.val > p.val else p
+        parent = root
         while True:
-            if curr.left != None and right.val < curr.val:
-                curr = curr.left
-            elif curr.right != None and left.val > curr.val:
-                curr = curr.right
+            if p.val < parent.val and q.val < parent.val:
+                parent = parent.left
+            elif p.val > parent.val and q.val > parent.val:
+                parent = parent.right
             else:
-                return curr
+                return parent
 
 # checkout main_test
 
@@ -45,12 +49,17 @@ class Solution(object):
         :type p: TreeNode
         :type q: TreeNode
         :rtype: TreeNode
+
+        2nd approach: recursive bst dfs
+
+        Time    O(logn)
+        Time    O(logn) height of the bst
+        72 ms, faster than 68.62%
         """
-        parent = root
-        while True:
-            if p.val < parent.val and q.val < parent.val:
-                parent = parent.left
-            elif p.val > parent.val and q.val > parent.val:
-                parent = parent.right
-            else:
-                return parent
+        if root == None or root == p or root == q:
+            return root
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return root
