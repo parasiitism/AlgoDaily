5ecf97127dc80037579af63805760f96d6427acf
calvinchankf <chan9118kin@gmail.com>
2019-04-09 13:07:58 +0800

    revise lc243,245

--- leetcode/245-shortest-word-distance-iii/main.py
+++ leetcode/245-shortest-word-distance-iii/main.py
@@ -1,16 +1,5 @@
-"""
-  questions to ask:
-  - will word1 == word2?
-  - will either word1, word2 be in the list always? 
-"""
-
-"""
-    naive solution: brute force O(n^2)
-"""
-
-
 class Solution(object):
-    def shortestDistance(self, words, word1, word2):
+    def shortestWordDistance(self, words, word1, word2):
         """
         :type words: List[str]
         :type word1: str
@@ -18,11 +7,21 @@ class Solution(object):
         :rtype: int
 
         1st approach: 2pointers
+        - same idea as lc143
 
         Time    O(n)
         Space   O(n)
-        52 ms, faster than 27.57%
+        48 ms, faster than 21.34%
         """
+        if word1 == word2:
+            last = -1
+            res = len(words)
+            for i in range(len(words)):
+                if words[i] == word1:
+                    if last != -1:
+                        res = min(res, i-last)
+                    last = i
+            return res
         p1 = -1
         p2 = -1
         res = len(words)
@@ -35,9 +34,3 @@ class Solution(object):
             if p1 != -1 and p2 != -1:
                 res = min(res, abs(p1-p2))
         return res
-
-
-a = ["practice", "makes", "perfect", "coding", "makes"]
-b = "makes"
-c = "coding"
-print(Solution().shortestDistance(a, b, c))
