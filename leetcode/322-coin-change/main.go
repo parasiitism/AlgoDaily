5ffa51f9fe314b2378dee62d49e812577ee2249d
calvinchankf <chan9118kin@gmail.com>
2019-04-10 14:43:56 +0800

    revise lc322 comment

--- leetcode/322-coin-change/main.go
+++ leetcode/322-coin-change/main.go
@@ -10,9 +10,11 @@ type Queue struct {
 	steps  int
 }
 
-// 1st attempt:
-// bfs(from top to bottom) + hashtable(avoid redundant calculation)
-// 188 ms, faster than 15.89%
+/*
+	1st approach:
+	- bfs(from top to bottom) + hashtable(avoid redundant calculation)
+	188 ms, faster than 15.89%
+*/
 func coinChange(coins []int, amount int) int {
 	var q []Queue
 	hash := make(map[int]bool)
@@ -78,19 +80,21 @@ func dfs(coins []int, amount int, hash map[int]int) int {
 	return min
 }
 
-// 3rd attempt
-// learned from others: bottom-up
-// e.g. coins = [1,2,5], amount = 7
-// f(0) = 0 // so if f(100-100), it output 0+1=1
-// f(1) = min(f(1)) + 1
-// f(2) = min(f(1)+f(0)) + 1
-// f(3) = min(f(2)+f(1)) + 1
-// f(4) = min(f(3)+f(2)) + 1
-// f(5) = min(f(4)+f(3)+f(0)) + 1
-// f(6) = min(f(5)+f(4)+f(1)) + 1
-// f(7) = min(f(6)+f(5)+f(2)) + 1
-// ...
-// this beats 96.2%
+/*
+	3rd attempt
+	learned from others: bottom-up
+	e.g. coins = [1,2,5], amount = 7
+	f(0) = 0  so if f(100-100), it output 0+1=1
+	f(1) = min(f(1)) + 1
+	f(2) = min(f(1)+f(0)) + 1
+	f(3) = min(f(2)+f(1)) + 1
+	f(4) = min(f(3)+f(2)) + 1
+	f(5) = min(f(4)+f(3)+f(0)) + 1
+	f(6) = min(f(5)+f(4)+f(1)) + 1
+	f(7) = min(f(6)+f(5)+f(2)) + 1
+	...
+	this beats 96.2%
+*/
 func coinChange2(coins []int, amount int) int {
 	dp := make([]int, amount+1) // store the min-steps from bottpm to top
 	// initialize them by setting each of them a big value
