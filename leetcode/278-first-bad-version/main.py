b19dcec6f0b082c8c149d925a9075f1ad9860ae9
calvinchankf <chan9118kin@gmail.com>
2019-04-10 17:04:20 +0800

    day218 q2

---
+++ leetcode/278-first-bad-version/main.py
@@ -0,0 +1,27 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+
+        1st approach: lower bound binary search
+
+        Time    O(logn)
+        Space   O(1)
+        20 ms, faster than 60.60%
+        """
+        left = 1
+        right = n
+        while left < right:
+            mid = int((left + right)/2)
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
