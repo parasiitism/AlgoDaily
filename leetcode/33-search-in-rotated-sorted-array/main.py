a963a71a22dd3d261bd3dfa462f684a8ecd0cf3f
calvinchankf <chan9118kin@gmail.com>
2019-04-10 20:49:07 +0800

    revise lc33

---
+++ leetcode/33-search-in-rotated-sorted-array/main.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+
+        2nd approach: still binary search but refactor a bit for understanding
+
+        Time	O(logn)
+        Space	O(1)
+        48 ms, faster than 22.10%
+        """
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left + right)/2
+            if nums[mid] < target:
+                if target <= nums[right] or nums[mid] > nums[right]:
+                    # search right when the pivot is here OR normally nums[mid] < nums[right] and target <= nums[right]
+                    left = mid + 1
+                else:
+                    # otherwise search in another half
+                    right = mid - 1
+            elif nums[mid] > target:
+                if target >= nums[left] or nums[left] > nums[mid]:
+                    # search left when the pivot is here OR normally nums[left] < nums[mid] and target >= nums[left]
+                    right = mid - 1
+                else:
+                    # otherwise search in another half
+                    left = mid + 1
+            else:
+                return mid
+        return -1
