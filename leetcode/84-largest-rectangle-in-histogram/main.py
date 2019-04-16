1d481e9e328c39fe027f29de38348a3c6a922190
calvinchankf <chan9118kin@gmail.com>
2019-04-16 17:17:29 +0800

    revise lc84

--- leetcode/84-largest-rectangle-in-histogram/main.py
+++ leetcode/84-largest-rectangle-in-histogram/main.py
@@ -17,12 +17,10 @@ class Solution(object):
         res = 0
         for i in range(len(heights)):
             maxheight = sys.maxsize
-            cnt = 1
             for j in range(i, -1, -1):
                 maxheight = min(maxheight, heights[j])
-                temp = maxheight * cnt
+                temp = maxheight * (i-j+1)
                 res = max(res, temp)
-                cnt += 1
         return res
 
 
@@ -32,45 +30,48 @@ print(Solution().largestRectangleArea([2, 1]))
 print(Solution().largestRectangleArea([2, 1, 5, 6, 2, 3]))
 print(Solution().largestRectangleArea([12, 23, 12, 25, 3, 16]))
 
-print("-----")
+print("----------")
+
+
+"""
+    2nd approach: stack <= learned from others 
+    - use a stack to store the indeces where the corresponding height can create a potential largest retangle
+
+    ref:
+    - https://www.youtube.com/watch?v=ZmnqCZp9bBs
+
+    Time	O(n)
+    Space	O(n)
+    100 ms, faster than 31.99% 
+"""
 
 
 class Solution(object):
     def largestRectangleArea(self, heights):
-        """
-        :type heights: List[int]
-        :rtype: int
-
-        2nd approach: stack <= learned from others 
-        - use a stack to store the indeces where the corresponding height can create a potential largest retangle
-
-        ref:
-        - https://www.youtube.com/watch?v=ZmnqCZp9bBs
-
-        Time	O(n)
-        Space	O(1)
-        168 ms, faster than 5.14% <= the code is similar to the one beats 100% so i am not sure why so slow
-        """
         res = 0
-        if len(heights) == 0:
-            return 0
-        stack = [-1, 0]
-        # iterate  through the list
-        for i in range(1, len(heights)):
+        # begining with -1, such that we can calculate the range, i - stack[-1][0] - 1, easier
+        # (index, height)
+        stack = [(-1, 0)]
+        for i in range(len(heights)):
             cur = heights[i]
-            if cur >= heights[stack[-1]]:
-                stack.append(i)
+            if cur > stack[-1][1]:
+                # if current height is taller, append to the stack
+                stack.append((i, cur))
             else:
-                while len(stack) > 1 and cur < heights[stack[-1]]:
-                    idx = stack.pop()
-                    # i-stack.peek()-1 is the width
-                    temp = heights[idx] * (i-stack[-1]-1)
-                    res = max(res, temp)
-                stack.append(i)
+                # pop the stack if the items which is > cur height
+                while stack[-1][1] > cur:
+                    popIdx, popH = stack.pop()
+                    width = i - stack[-1][0] - 1
+                    area = popH * width
+                    res = max(res, area)
+                # put the current item to the array
+                stack.append((i, cur))
+        # if the stack is not empty, pop the items
         while len(stack) > 1:
-            idx = stack.pop()
-            temp = heights[idx] * (len(heights)-stack[-1]-1)
-            res = max(res, temp)
+            popIdx, popH = stack.pop()
+            width = len(heights) - stack[-1][0] - 1
+            area = popH * width
+            res = max(res, area)
         return res
 
 
@@ -79,3 +80,5 @@ print(Solution().largestRectangleArea([2]))
 print(Solution().largestRectangleArea([2, 1]))
 print(Solution().largestRectangleArea([2, 1, 5, 6, 2, 3]))
 print(Solution().largestRectangleArea([12, 23, 12, 25, 3, 16]))
+
+print("----------")
