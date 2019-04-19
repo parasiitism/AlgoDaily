3613202e676d3a7cd92e97daf933a519610f962e
calvinchankf <chan9118kin@gmail.com>
2019-04-19 11:37:04 +0800

    revise lc122

--- leetcode/122-best-time-to-buy-and-sell-stock-ii/main.py
+++ leetcode/122-best-time-to-buy-and-sell-stock-ii/main.py
@@ -34,3 +34,45 @@ print(Solution().maxProfit([1, 3]))
 print(Solution().maxProfit([3, 1]))
 print(Solution().maxProfit([1]))
 print(Solution().maxProfit([1]))
+
+print("-----------------------------------")
+
+"""
+    1st approach: increasing sequence
+    - keep track of the minVal
+    - if current value > minVal, add up the diff = prices[i] - minVal to the result, then reset the minVal
+
+    Time    O(n)
+    Space   O(1)
+    52 ms, faster than 29.52%
+
+    why it is slower than 1st: perform more += operation because it doesn't care abuot the increasing sequence
+"""
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+        res = 0
+        minVal = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < minVal:
+                minVal = prices[i]
+            else:
+                res += prices[i] - minVal
+                minVal = prices[i]
+        return res
+
+
+print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
+print(Solution().maxProfit([1, 2, 3, 4, 5]))
+print(Solution().maxProfit([7, 6, 5, 4, 3]))
+print(Solution().maxProfit([1, 3]))
+print(Solution().maxProfit([3, 1]))
+print(Solution().maxProfit([1]))
+print(Solution().maxProfit([1]))
