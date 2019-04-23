b04fa37dcf97562d6b9e815eda72a17915e9e80f
calvinchankf <chan9118kin@gmail.com>
2019-04-23 16:17:54 +0800

    revise lc708

--- leetcode/708-insert-into-a-cyclic-sorted-list/main.py
+++ leetcode/708-insert-into-a-cyclic-sorted-list/main.py
@@ -8,17 +8,26 @@ class Node(object):
         self.next = next
 
 
+"""
+    classic problem, classic approach
+    - find the minNode
+    - there are 3 cases to insert a node
+        1. cur < target < cur.next
+        2. cur == target == cur
+        3. cur.next == head and insertion no yet happen
+
+    Time    O(2n)
+    Space   O(1)
+    460ms beats 98.10%
+"""
+
+
 class Solution(object):
     def insert(self, head, insertVal):
         """
         :type head: Node
         :type insertVal: int
         :rtype: Node
-
-        classic problem, classic approach
-        Time    O(2n)
-        Space   O(1)
-        460ms beats 98.10%
         """
         # if nil
         if head == None:
@@ -26,7 +35,7 @@ class Solution(object):
             newNode.next = newNode
             return newNode
 
-        # find the min
+        # find the left most node with min value
         minNode = head
         cur = head
         while True:
@@ -39,7 +48,7 @@ class Solution(object):
         # start to find the place from the minnode
         cur = minNode
         while True:
-            if (insertVal > cur.val and insertVal < cur.next.val) \
+            if (cur.val < insertVal and insertVal < cur.next.val) \
                     or (cur.next == minNode) \
                     or (insertVal == cur.val):
                 newNode = Node(insertVal, cur.next)
@@ -53,11 +62,13 @@ class Solution(object):
 
 def printList(node):
     cur = node
+    res = ""
     while True:
-        print(cur.val)
+        res += str(cur.val) + ","
         cur = cur.next
         if cur == node:
             break
+    print(res)
 
 
 # 3->4->1 2
@@ -96,7 +107,7 @@ five.next = one
 s = Solution().insert(one, 0)
 printList(s)
 
-# # 3->5->1 0
+# 3->5->1 0
 three = Node(3, None)
 five = Node(5, None)
 one = Node(1, None)
@@ -105,4 +116,135 @@ five.next = one
 one.next = three
 
 s = Solution().insert(three, 0)
+printList(s)
+
+# 2->2->3->4->1 2
+two1 = Node(2, None)
+two2 = Node(2, None)
+three = Node(3, None)
+four = Node(4, None)
+one = Node(1, None)
+two1.next = two2
+two2.next = three
+three.next = four
+four.next = one
+one.next = two1
+
+
+s = Solution().insert(two1, 2)
+printList(s)
+
+print("--------------------")
+
+"""
+    2nd approach: optimize the above approach
+    - find the minNode
+    - there are 2 cases to insert a node
+        1. cur <= target < cur.next
+        2. cur.next == head and insertion no yet happen
+
+    Time    O(2n)
+    Space   O(1)
+    460ms beats 98.10%
+"""
+
+
+class Solution(object):
+    def insert(self, head, insertVal):
+        """
+        :type head: Node
+        :type insertVal: int
+        :rtype: Node
+        """
+        # if nil
+        if head == None:
+            newNode = Node(insertVal, None)
+            newNode.next = newNode
+            return newNode
+
+        # find the left most node with min value
+        minNode = head
+        cur = head
+        while True:
+            if cur.val < minNode.val:
+                minNode = cur
+            cur = cur.next
+            if cur == head:
+                break
+
+        # start to find the place from the minNode
+        cur = minNode
+        while True:
+            if cur.val <= insertVal and insertVal < cur.next.val:
+                cur.next = Node(insertVal, cur.next)
+                break
+            if cur.next == minNode:
+                cur.next = Node(insertVal, cur.next)
+                break
+            cur = cur.next
+
+        return head
+
+
+# 3->4->1 2
+three = Node(3, None)
+four = Node(4, None)
+one = Node(1, None)
+three.next = four
+four.next = one
+one.next = three
+
+
+s = Solution().insert(three, 2)
+printList(s)
+
+
+# 3->3->3 2
+three = Node(3, None)
+four = Node(3, None)
+one = Node(3, None)
+three.next = four
+four.next = one
+one.next = three
+
+
+s = Solution().insert(three, 2)
+printList(s)
+
+# 1->3->5 0
+one = Node(1, None)
+three = Node(3, None)
+five = Node(5, None)
+one.next = three
+three.next = five
+five.next = one
+
+s = Solution().insert(one, 0)
+printList(s)
+
+# 3->5->1 0
+three = Node(3, None)
+five = Node(5, None)
+one = Node(1, None)
+three.next = five
+five.next = one
+one.next = three
+
+s = Solution().insert(three, 0)
+printList(s)
+
+# 2->2->3->4->1 2
+two1 = Node(2, None)
+two2 = Node(2, None)
+three = Node(3, None)
+four = Node(4, None)
+one = Node(1, None)
+two1.next = two2
+two2.next = three
+three.next = four
+four.next = one
+one.next = two1
+
+
+s = Solution().insert(two1, 2)
 printList(s)
