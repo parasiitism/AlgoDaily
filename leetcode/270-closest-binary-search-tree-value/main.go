4d4c3d7c500d8f8e329ffa67c6c5c4d8a0b5e045
calvinchankf <chan9118kin@gmail.com>
2019-04-24 15:14:21 +0800

    revise 270

--- leetcode/270-closest-binary-search-tree-value/main.go
+++ leetcode/270-closest-binary-search-tree-value/main.go
@@ -10,11 +10,14 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// depth first search
-// Time		O(n)
-// Space	O(height of the tree) the call stack
-// beats 14.29%
-// 10sep2018
+/*
+	1st: brute force depth first search
+
+	Time		O(n)
+	Space	O(height of the tree) the call stack
+	beats 14.29%
+	10sep2018
+*/
 func closestValue(root *TreeNode, target float64) int {
 	closest := root.Val
 	var dfs func(cur *TreeNode)
@@ -31,66 +34,17 @@ func closestValue(root *TreeNode, target float64) int {
 			dfs(cur.Right)
 		}
 	}
-
 	dfs(root)
-
 	return closest
 }
 
-// recursive depth first search with return value
-func closestValue1(root *TreeNode, target float64) int {
-	cur_diff := math.Abs(target - float64(root.Val))
-	left := math.MaxInt64
-	right := math.MaxInt64
-	if root.Left != nil {
-		left = closestValue(root.Left, target)
-	}
-	left_diff := math.Abs(target - float64(left))
-	if root.Right != nil {
-		right = closestValue(root.Right, target)
-	}
-	right_diff := math.Abs(target - float64(right))
-	if cur_diff < left_diff && cur_diff < right_diff {
-		return root.Val
-	} else if left_diff < cur_diff && left_diff < right_diff {
-		return left
-	} else if right_diff < cur_diff && right_diff < left_diff {
-		return right
-	}
-	return root.Val
-}
-
-// iterative dfs
-func closestValue2(root *TreeNode, target float64) int {
-
-	result := math.MaxInt64
-
-	var stack []*TreeNode
-	stack = append(stack, root)
-	for len(stack) > 0 {
-		var pop *TreeNode
-		pop, stack = stack[len(stack)-1], stack[:len(stack)-1]
-
-		if math.Abs(float64(pop.Val)-target) < math.Abs(float64(result)-target) {
-			result = pop.Val
-		}
-
-		if pop.Left != nil {
-			stack = append(stack, pop.Left)
-		}
-		if pop.Right != nil {
-			stack = append(stack, pop.Right)
-		}
-	}
-
-	return result
-}
-
-// BST search
-// Time		O(logn)
-// Space	O(height of the tree) the call stack
-// beats 100%
-// 20jan2019
+/*
+	2nd: BST search
+	Time		O(logn)
+	Space	O(height of the tree) the call stack
+	beats 100%
+	20jan2019
+*/
 func closestValue3(root *TreeNode, target float64) int {
 	closest := root.Val
 	var dfs func(cur *TreeNode)
---
+++ leetcode/270-closest-binary-search-tree-value/main.py
@@ -0,0 +1,77 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+"""
+    2nd: recursive BST search
+	Time		O(logn)
+	Space	O(height of the tree) the call stack
+	beats 100%
+	20jan2019
+"""
+
+
+class Solution(object):
+    def __init__(self):
+        self.res = 0
+
+    def closestValue(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
+        self.res = root.val
+        self.helper(root, target)
+        return self.res
+
+    def helper(self, root, target):
+        if root == None:
+            return
+        a = abs(root.val - target)
+        b = abs(self.res - target)
+        if a < b:
+            self.res = root.val
+        if target < root.val:
+            self.helper(root.left, target)
+        elif target > root.val:
+            self.helper(root.right, target)
+
+
+"""
+    3rd approach: iterative BST search
+    - set the root as intermediate result
+    - bst traverse down to the nearest node and keep updating the result
+
+    Time    O(h)
+    Space   O(1)
+    32 ms, faster than 81.41%
+    24apr2019
+"""
+
+
+class Solution(object):
+    def closestValue(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
+        res = root.val
+        cur = root
+        while cur != None:
+            a = abs(cur.val - target)
+            b = abs(res - target)
+            if a < b:
+                res = cur.val
+            if target < cur.val:
+                cur = cur.left
+            elif target > cur.val:
+                cur = cur.right
+            else:
+                return cur.val
+        return res
