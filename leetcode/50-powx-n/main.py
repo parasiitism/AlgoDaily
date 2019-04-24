9e35d7adf6d6688fb2949081ea9d12ba33739b2c
calvinchankf <chan9118kin@gmail.com>
2019-04-24 17:42:58 +0800

    update lc50

---
+++ leetcode/50-powx-n/main.py
@@ -0,0 +1,47 @@
+"""
+    2nd approach: recurisve split + hashtable(avoid redundant calculation)
+
+               2^10
+            2^5 * 2^5
+        2^2 * 2^3 * 2^2 * 2^3
+    2^2 * 2^2 * 2 * 2^2 * 2^2 * 2
+
+    Time    O(logn)
+    Space   O(logn)
+    24 ms, faster than 39.07%
+"""
+
+
+class Solution(object):
+    def __init__(self):
+        self.cache = {}
+
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1.0/self.helper(x, -n)
+        return self.helper(x, n)
+
+    def helper(self, x, n):
+        if n in self.cache:
+            return self.cache[n]
+        if n == 1:
+            return x
+        left = n/2
+        right = n-left
+        temp = self.helper(x, left) * self.helper(x, right)
+        self.cache[n] = temp
+        return temp
+
+
+print(Solution().myPow(2.0, 10))
+print(Solution().myPow(2.1, 3))
+print(Solution().myPow(2, -2))
+print(Solution().myPow(2, 0))
+print(Solution().myPow(2, 1))
