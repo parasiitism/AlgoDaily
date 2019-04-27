34c4ee1571e207696df37e6bafe8620d85ee553c
calvinchankf <chan9118kin@gmail.com>
2019-04-27 17:54:28 +0800

    day235 q1 s1

---
+++ leetcode/300-longest-increasing-subsequence/main.py
@@ -0,0 +1,27 @@
+"""
+    1st approach: dynamic programming
+
+    ref:
+    - https://www.youtube.com/watch?v=CE2b_-XfVDk
+
+    Time    O(n^2)
+    Space   O(n)
+    1068 ms, faster than 12.81%
+"""
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = len(nums) * [1]
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+        res = 0
+        for x in dp:
+            res = max(res, x)
+        return res
