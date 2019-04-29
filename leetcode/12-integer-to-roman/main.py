9d9fcafdfcfe1278ac9db965b0b8e29725c0cc11
calvinchankf <chan9118kin@gmail.com>
2019-04-29 20:02:42 +0800

    revise lc12,13

--- leetcode/12-integer-to-roman/main.py
+++ leetcode/12-integer-to-roman/main.py
@@ -1,15 +1,18 @@
+"""
+    1st approach:
+    - split cases for 1-3,4,5,6-8,9
+
+    Time    O(n)
+    Space   O(n)
+    48 ms, faster than 100.00%
+"""
+
+
 class Solution(object):
     def intToRoman(self, num):
         """
         :type num: int
         :rtype: str
-
-        1st approach:
-        - split cases for 1-3,4,5,6-8,9
-
-        Time    O(n)
-        Space   O(n)
-        48 ms, faster than 100.00%
         """
         m = ["I", "V", "X", "L", "C", "D", "M"]
         res = ""
@@ -38,8 +41,65 @@ class Solution(object):
         return res
 
 
-a = 3*"I"
-print(a)
+print(Solution().intToRoman(0))
+print(Solution().intToRoman(10))
+print(Solution().intToRoman(58))
+print(Solution().intToRoman(200))
+print(Solution().intToRoman(120))
+print(Solution().intToRoman(508))
+print(Solution().intToRoman(203))
+print(Solution().intToRoman(3999))
+print("-----")
+
+"""
+    2nd approach: hashtable
+    - concise and less cases
+
+    Time    O(n)
+    Space   O(n)
+    48 ms, faster than 96.43%
+"""
+
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        m = {
+            1: 'I',
+            4: 'IV',
+            5:  'V',
+            9: 'IX',
+            10: 'X',
+            40: 'XL',
+            50: 'L',
+            90: 'XC',
+            100: 'C',
+            400: 'CD',
+            500: 'D',
+            900: 'CM',
+            1000: 'M'
+        }
+        res = ""
+        i = 0
+        count = 0
+        while num > 0:
+            digit = num % 10
+            num /= 10
+            cur = digit * 10 ** count
+            if cur in m:
+                res = m[cur] + res
+            elif digit < 4:
+                res = digit * m[10**count] + res
+            elif digit < 9:
+                five = m[5*10**count]
+                one = m[10**count]
+                res = five + (digit-5)*one + res
+            count += 1
+        return res
+
 
 print(Solution().intToRoman(0))
 print(Solution().intToRoman(10))
@@ -49,3 +109,4 @@ print(Solution().intToRoman(120))
 print(Solution().intToRoman(508))
 print(Solution().intToRoman(203))
 print(Solution().intToRoman(3999))
+print("-----")
--- leetcode/13-roman-to-integer/main.py
+++ leetcode/13-roman-to-integer/main.py
@@ -1,14 +1,17 @@
+"""
+    1st approach: use a var for previous symbol
+
+    Time    O(n)
+    Space   O(1)
+    68 ms, faster than 99.19%
+"""
+
+
 class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
-
-        1st approach: use a var for previous symbol
-
-        Time    O(n)
-        Space   O(1)
-        68 ms, faster than 99.19%
         """
         indeces = {
             "I": 0,
@@ -47,4 +50,47 @@ class Solution(object):
         if prev != None:
             res += m[prev]
 
+        return res
+
+
+"""
+    2nd approach:
+    - compare with prev value and if prev value < cur value
+        1. undo previous addition
+        2. add m[cur] - m[prev]
+    - if no smaller bigger values, just do addition
+
+    Time    O(n)
+    Space   O(1)
+    52 ms, faster than 97.49%
+"""
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0:
+            return 0
+        m = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        res = m[s[0]]
+        for i in range(1, len(s)):
+            prev = s[i-1]
+            cur = s[i]
+            if m[prev] < m[cur]:
+                res -= m[prev]
+                res += m[cur] - m[prev]
+            else:
+                res += m[cur]
         return res
