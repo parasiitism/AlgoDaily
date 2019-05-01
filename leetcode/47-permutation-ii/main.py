dcdbdf35c885ef8abbafd92785562c0cff9d1d56
calvinchankf <chan9118kin@gmail.com>
2019-05-01 22:16:07 +0800

    revise subset,combination,permutation

--- leetcode/47-permutation-ii/main.py
+++ leetcode/47-permutation-ii/main.py
@@ -1,17 +1,19 @@
-# optimize the naive approach
-# recursively dfs all the possibilities and avoid duplicate paths by using a hashtable
+"""
+    optimize the naive approach
+    recursively dfs all the possibilities and avoid duplicate paths by using a hashtable
 
-# e.g. 2, -1, 3, -1
-# sort it such that nums = -1, -1, 2, 3
+    e.g. 2, -1, 3, -1
+    sort it such that nums = -1, -1, 2, 3
 
-# compute permutations of - 1(index 0)
-# skip computation for index 1 becos - 1(index 0) has been considered
-# compute permutations of 2(index 2)
-# compute permutations of 3(index 3)
+    compute permutations of - 1(index 0)
+    skip computation for index 1 becos - 1(index 0) has been considered
+    compute permutations of 2(index 2)
+    compute permutations of 3(index 3)
 
-# time      O(n!) worst case
-# space	    O(n!)
-# beats     46.47%
+    time      O(n!) worst case
+    space	    O(n!)
+    beats     46.47%
+"""
 
 
 class Solution(object):
@@ -35,17 +37,55 @@ class Solution(object):
             self.dfs(nums[:i] + nums[i+1:], prefix + [nums[i]])
 
 
+"""
+    optimize the naive approach
+    recursively dfs all the possibilities and avoid duplicate paths by using a hashtable
+
+    e.g. 2, -1, 3, -1
+    sort it such that nums = -1, -1, 2, 3
+
+    compute permutations of - 1(index 0)
+    skip computation for index 1 becos - 1(index 0) has been considered
+    compute permutations of 2(index 2)
+    compute permutations of 3(index 3)
+
+    time      O(n!) worst case
+    space	    O(n!)
+    44 ms, faster than 96.93%
+"""
+
+
+class Solution(object):
+
+    def __init__(self):
+        self.result = []
+
+    def permuteUnique(self, nums):
+        nums = sorted(nums)
+        self.dfs(nums, [])
+        return self.result
+
+    def dfs(self, nums, prefix):
+        if len(nums) == 0:
+            self.result.append(prefix)
+        for i in range(len(nums)):
+            if i == 0 or (i > 0 and nums[i-1] != nums[i]):
+                self.dfs(nums[:i] + nums[i+1:], prefix + [nums[i]])
+
+
 # print(Solution().permuteUnique([1, 1, 2]))
 
-# iterative insertion using a hashtable to avoid duplicate result
-#   1
-#  ^ ^
-#  2 2
-#
-#   2,1         1,2
-#  ^ ^ ^       ^ ^ ^
-#  3 3 3       3 3 3
-# beats 29.92%
+"""
+    iterative insertion using a hashtable to avoid duplicate result
+    1
+    ^ ^
+    2 2
+
+    2,1         1,2
+    ^ ^ ^       ^ ^ ^
+    3 3 3       3 3 3
+    beats 29.92%
+"""
 
 
 def permuteUnique(nums):
--- leetcode/90-subsets-ii/main.py
+++ leetcode/90-subsets-ii/main.py
@@ -1,11 +1,16 @@
-class Solution(object):
-    """
+"""
+    1st approach: recursion + hashtable
+
     Recursive DFS using hashtables to avoid duplicates, see ./idea.jpeg
     Method similar to what I did for Permutation II
+
     Time    O(2^n) worst
     Space   O(2^n) recursion
     24 ms, faster than 100.00%
-    """
+"""
+
+
+class Solution(object):
 
     def __init__(self):
         self.result = []
@@ -29,11 +34,40 @@ class Solution(object):
             self.dfs(nums[i+1:], path+[nums[i]])
 
 
+"""
+    2nd approach: recursion + hashtable
+    - similar to 1st dont need to use a hashtable(since the array is sorted)
+    
+    Time    O(2^n) worst
+    Space   O(2^n) recursion
+    24 ms, faster than 100.00%
+"""
+
+
+class Solution(object):
+
+    def __init__(self):
+        self.result = []
+
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums = sorted(nums)
+        self.dfs(nums, [])
+        return self.result
+
+    def dfs(self, nums, path):
+        self.result.append(path)
+        for i in range(len(nums)):
+            if i == 0 or (i > 0 and nums[i-1] != nums[i]):
+                self.dfs(nums[i+1:], path+[nums[i]])
+
+
 print(Solution().subsetsWithDup([1, 2, 2, 3]))
 
-
-class Solution1(object):
-    """
+"""
     Iteratively append the next item to calculated items with a constraint
     e.g. [1,2,2,3]
     []
@@ -45,8 +79,10 @@ class Solution1(object):
     Space   O(1)
     beats   13.12%
     ref: https://leetcode.com/problems/subsets-ii/discuss/30145/Accepted-java-iterative-solution
-    """
+"""
 
+
+class Solution1(object):
     def subsetsWithDup(self, nums):
         nums = sorted(nums)
         res = [[]]
