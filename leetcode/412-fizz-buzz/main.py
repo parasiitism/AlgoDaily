bb4b74b50d0d772ccbe5b6d1468b55fd6b15ad69
calvinchankf <chan9118kin@gmail.com>
2019-05-01 17:47:42 +0800

    revise lc412

---
+++ leetcode/412-fizz-buzz/main.py
@@ -0,0 +1,26 @@
+"""
+    1st approach: math
+
+    Time    O(n)
+    Space   O(1)
+    48 ms, faster than 23.34%
+"""
+
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        for i in range(1, n+1):
+            s = ''
+            if i % 3 == 0:
+                s += 'Fizz'
+            if i % 5 == 0:
+                s += 'Buzz'
+            if len(s) == 0:
+                s = str(i)
+            res.append(s)
+        return res
