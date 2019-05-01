04f185f9a3b363b8a2bb9a74e33402c032e2de01
calvinchankf <chan9118kin@gmail.com>
2019-05-01 17:24:43 +0800

    revise lc344

---
+++ leetcode/344-reverse-string/main.py
@@ -0,0 +1,21 @@
+"""
+    1st approach: 2 pointers
+
+    Time  O(n)
+    Space O(1)
+    188 ms, faster than 86.90%
+"""
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
