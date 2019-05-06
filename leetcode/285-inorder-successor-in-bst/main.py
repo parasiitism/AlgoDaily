dac9dffcb8fea310c4b1cc86cf9c9783c8987e57
calvinchankf <chan9118kin@gmail.com>
2019-05-06 15:49:15 +0800

    revise 285 + followup predecessor

--- leetcode/285-inorder-successor-in-bst/main.py
+++ leetcode/285-inorder-successor-in-bst/main.py
@@ -11,167 +11,18 @@ class TreeNode(object):
         self.right = None
 
 
-class Solution(object):
-    dest = None
-    found = False
+"""
+    3rd: decent recursive approach:
 
-    def inorderSuccessor(self, root, p):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :rtype: TreeNode
-
-        1st, my intuitive thought:
-        - find the smallest node which is just larger than the target
-
-        Time    O(logn)
-        Space   O(h)
-        """
-        if root == None:
-            return None
-        self.check(root, p)
-        return self.dest if self.found else None
-
-    def check(self, root, p):
-        x = root.val - p.val
-        y = sys.maxsize
-
-        if self.dest != None:
-            y = self.dest.val - p.val
-
-        if x == 0:
-            self.found = True
-
-        if x < y and x > 0:
-            self.dest = root
-
-        if root.val <= p.val and root.right != None:
-            self.check(root.right, p)
-        if root.val > p.val and root.left != None:
-            self.check(root.left, p)
-
-
-a = TreeNode(5)
-b = TreeNode(3)
-c = TreeNode(6)
-d = TreeNode(2)
-e = TreeNode(4)
-f = TreeNode(1)
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-d.left = f
-
-# try to search every number
-ans = Solution().inorderSuccessor(a, e)
-print("ans=", ans)
-if ans != None:
-    print("ans.val=", ans.val)
-
-
-class Solution(object):
-    def __init__(self):
-        self.res = None
-        self.next = False
-
-    def inorderSuccessor(self, root, p):
-        """
-        2nd approach:
-        - the result, successor, is actually the next item in inorder traversal
-
-        Time    O(n)
-        Space   O(h)
-        92 ms, faster than 14.18%
-        """
-        self.dfs(root, p)
-        return self.res
-
-    def dfs(self, node, target):
-        if node == None:
-            return
-        self.dfs(node.left, target)
-        if self.next == True:
-            self.res = node
-            self.next = False
-        if target == node:
-            self.next = True
-        self.dfs(node.right, target)
-
-
-a = TreeNode(5)
-b = TreeNode(3)
-c = TreeNode(6)
-d = TreeNode(2)
-e = TreeNode(4)
-f = TreeNode(1)
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-d.left = f
-
-# try to search every number
-ans = Solution().inorderSuccessor(a, e)
-print("ans=", ans)
-if ans != None:
-    print("ans.val=", ans.val)
+    Time    O(logn)
+    Space   O(h)
+    64 ms, faster than 85.19% 
+    21feb2019
+"""
 
 
 class Solution(object):
     def inorderSuccessor(self, root, p):
-        """
-        3rd: very similar to upper bound binary search BUT
-        - here is BSTsearch, only record the sucessor when we go left
-        - then the sucessor is either your parent OR your left most leaf in right subtree
-
-        see ./idea.jpeg
-
-        Time    O(logn)
-        Space   O(h)
-        68 ms, faster than 61.86%
-        24apr2019
-        """
-        node = root
-        suc = None
-        while node != None:
-            if p.val >= node.val:
-                node = node.right
-            else:
-                suc = node
-                node = node.left
-        return suc
-
-
-a = TreeNode(5)
-b = TreeNode(3)
-c = TreeNode(6)
-d = TreeNode(2)
-e = TreeNode(4)
-f = TreeNode(1)
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-d.left = f
-
-# try to search every number
-ans = Solution().inorderSuccessor(a, e)
-print("ans=", ans)
-if ans != None:
-    print("ans.val=", ans.val)
-
-
-class Solution(object):
-    def inorderSuccessor(self, root, p):
-        """
-        4th, decent recursive approach:
-
-        Time    O(logn)
-        Space   O(h)
-        64 ms, faster than 85.19% 
-        21feb2019
-        """
         if root == None:
             return None
         if p.val < root.val:
@@ -206,3 +57,76 @@ ans = Solution().inorderSuccessor(a, e)
 print("ans=", ans)
 if ans != None:
     print("ans.val=", ans.val)
+
+"""
+    3rd: very similar to upper bound binary search BUT
+    - here is BSTsearch, only record the sucessor when we go left
+    - then the sucessor is either your parent OR your left most leaf in right subtree
+
+    see ./idea.jpeg
+
+    Time    O(logn)
+    Space   O(h)
+    68 ms, faster than 61.86%
+    24apr2019
+"""
+
+
+class Solution(object):
+    def inorderSuccessor(self, root, p):
+        node = root
+        suc = None
+        while node != None:
+            if p.val >= node.val:
+                node = node.right
+            else:
+                suc = node
+                node = node.left
+        return suc
+
+
+a = TreeNode(5)
+b = TreeNode(3)
+c = TreeNode(6)
+d = TreeNode(2)
+e = TreeNode(4)
+f = TreeNode(1)
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+d.left = f
+
+# try to search every number
+ans = Solution().inorderSuccessor(a, e)
+print("ans=", ans)
+if ans != None:
+    print("ans.val=", ans.val)
+
+"""
+    followup: inorder predecessor
+
+    very similar to lower bound binary search BUT
+    - exclude the lower bound
+    - here is BSTsearch, only record the sucessor when we go right
+    - then the sucessor is either your parent OR your right most leaf in left subtree
+"""
+
+
+class Solution(object):
+
+    def inorderPredecessor(self, root, p):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+        node = root
+        pred = None
+        while node != None:
+            if p.val <= node.val:
+                node = node.left
+            else:
+                pred = node
+                node = node.right
+        return pred
