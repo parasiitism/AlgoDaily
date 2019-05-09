81cc99686292231845d1dda0bebe6079551386b7
calvinchankf <chan9118kin@gmail.com>
2019-05-09 15:52:31 +0800

    revise lc108

--- leetcode/108-convert-sorted-array-to-binary-search-tree/main.py
+++ leetcode/108-convert-sorted-array-to-binary-search-tree/main.py
@@ -7,7 +7,7 @@ class TreeNode(object):
 
 
 """
-    revise:
+    revision:
     0. to create a balence tree, u must sort the array first
     1. sort the array
     2. build the tree recursively in the way of binary search
@@ -30,4 +30,26 @@ class Solution(object):
         node = TreeNode(nums[mean])
         node.left = self.buildBST(nums, left, mean - 1)
         node.right = self.buildBST(nums, mean + 1, right)
+        return node
+
+"""
+    revision:
+    0. to create a balence tree, u must sort the array first
+    1. sort the array
+    2. build the tree recursively in the way of binary search
+"""
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if len(nums) == 0:
+            return None
+        mid = (len(nums)-1)//2
+        left = nums[:mid]
+        right = nums[mid+1:]
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(left)
+        node.right = self.sortedArrayToBST(right)
         return node
