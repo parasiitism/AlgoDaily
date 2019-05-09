5e83d8cc313a201f5c9dea08926a9a76d7912bff
calvinchankf <chan9118kin@gmail.com>
2019-05-09 17:04:12 +0800

    revise lc445

--- leetcode/445-add-two-numbers-ii/main.py
+++ leetcode/445-add-two-numbers-ii/main.py
@@ -24,41 +24,37 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        # put values into stacks
         stack1 = []
-        stack2 = []
         cur = l1
         while cur != None:
             stack1.append(cur.val)
             cur = cur.next
+        stack2 = []
         cur = l2
         while cur != None:
             stack2.append(cur.val)
             cur = cur.next
-
+        # start from a dump node
+        dump = ListNode(-1)
         carry = 0
-        dump = ListNode(0)
+        # pop the stacks
         while len(stack1) > 0 or len(stack2) > 0:
-            # extract numbers
-            val1 = 0
+            a = 0
             if len(stack1) > 0:
-                val1 = stack1.pop()
-            val2 = 0
+                a = stack1.pop()
+            b = 0
             if len(stack2) > 0:
-                val2 = stack2.pop()
-            # add them up
-            combo = val1 + val2 + carry
-            # construct new node and carry
-            carry = combo/10
-            newNode = ListNode(combo % 10)
-            # reassign head
-            temp = dump.next
-            dump.next = newNode
-            newNode.next = temp
-
+                b = stack2.pop()
+            temp = a + b + carry
+            carry = temp/10
+            num = temp%10
+            node = ListNode(num)
+            node.next = dump.next
+            dump.next = node
+        # if unfortunately there is an extra one
         if carry > 0:
-            newNode = ListNode(carry)
-            # reassign head
-            temp = dump.next
-            dump.next = newNode
-            newNode.next = temp
+            node = ListNode(1)
+            node.next = dump.next
+            dump.next = node
         return dump.next
