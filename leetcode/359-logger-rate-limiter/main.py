c918e692f56863dcae9fea6f927529e3b62847c6
calvinchankf <chan9118kin@gmail.com>
2019-05-10 19:35:39 +0800

    revise lc359

---
+++ leetcode/359-logger-rate-limiter/main.py
@@ -0,0 +1,40 @@
+"""
+    1st approach: hashtable
+
+    Time    O(1)
+    Space   O(n)
+    136 ms, faster than 51.26%
+"""
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.ht = {}
+        
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        :type timestamp: int
+        :type message: str
+        :rtype: bool
+        """
+        if message not in self.ht:
+            self.ht[message] = timestamp
+            return True
+        else:
+            if timestamp - 10 < self.ht[message]:
+                return False
+            else:
+                self.ht[message] = timestamp
+                return True
+        
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
