2c671f03361ce4fa826bb258479faa2cf74a18bf
calvinchankf <chan9118kin@gmail.com>
2019-05-11 15:13:15 +0800

    revise lc480

--- leetcode/480-sliding-window-median/main.py
+++ leetcode/480-sliding-window-median/main.py
@@ -1,5 +1,17 @@
 import bisect
 
+"""
+    1st approach
+    - binary search to 
+        - pop the left most item
+        - push the right most item to the window
+    - therefore to optimize the window sliding
+
+    Time    O(n*2k)
+    Space   O(k)
+    112 ms, faster than 80.00%
+"""
+
 
 class Solution(object):
     def medianSlidingWindow(self, nums, k):
@@ -7,40 +19,36 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: List[float]
-
-        1st approach
-        - similar to window sum
-        - but for each iteration, sort the nums
-        Time    O(n*klogk)
-        Space   O(k)
-        TLE
         """
         if len(nums) == 0 or k <= 0 or k > len(nums):
             return 0
         res = []
-        # first result(first k items' median)
-        window = []
-        for i in range(k):
-            window.append(nums[i])
-        res.append(self.calMedian(window[:]))
+        window = sorted(nums[:k])
+        res.append(self.calMedian(window))
 
-        # 2nd item -> end
         for i in range(k, len(nums)):
-            # remove the first num from window and add the current num to the window
-            # e.g. [1,2,3], 4=> 1, [2,3,4]
-            window.pop(0)
-            window.append(nums[i])
-            # copy window and cal median of the copy
-            res.append(self.calMedian(window[:]))
+            num = nums[i]
+            # Find position where outgoing element should be removed from
+            # O(n) remove window.remove(nums[i-k])
+            # though it is O(logn) remove but pop(idx) takes O(n)...
+            idx = bisect.bisect_left(window, nums[i-k])
+            window.pop(idx)
+
+            # Maintain the sorted invariant while inserting incoming element
+            idx = bisect.bisect_right(window, num)
+            window.insert(idx, num)
+
+            # Find the median
+            res.append(self.calMedian(window))
+
         return res
 
-    def calMedian(self, nums):
-        sortNums = sorted(nums)
-        if len(sortNums) % 2 == 0:
-            half = len(sortNums)/2
-            return (sortNums[half-1]+sortNums[half])/2.0
-        half = len(sortNums)/2
-        return float(sortNums[half])
+    def calMedian(self, window):
+        half = (len(window)-1)/2
+        if len(window) % 2 == 0:
+            return (window[half] + window[half+1]) / 2.0
+        else:
+            return window[half]*1.0
 
 
 print(Solution().medianSlidingWindow([1, 2, 3, 4, 5, 6, 7], 3))
@@ -49,6 +57,19 @@ print(Solution().medianSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 4))
 
 print("----------------------------------------")
 
+"""
+    2nd approach
+    - similar to 1st approach but use binary search to 
+        - pop the left most item
+        - push the right most item to the window
+    - therefore to optimize the window sliding
+
+    Time    O(n*2k)
+    Space   O(k)
+
+    200 ms, faster than 57.24%
+"""
+
 
 class Solution(object):
     def medianSlidingWindow(self, nums, k):
@@ -56,17 +77,6 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: List[float]
-
-        2nd approach
-        - similar to 1st approach but use binary search to 
-            - pop the left most item
-            - push the right most item to the window
-        - therefore to optimize the window sliding
-
-        Time    O(n*2logk)
-        Space   O(k)
-
-        200 ms, faster than 57.24%
         """
         if len(nums) == 0 or k <= 0 or k > len(nums):
             return 0
@@ -74,12 +84,10 @@ class Solution(object):
         for i in range(len(nums)):
             # find position where outgoing element should be removed from
             if i >= k:
-                # O(logk) remove with binary search
+                # O(logk) + O(k) remove with binary search
                 idx = bisect.bisect_left(window, nums[i - k])
                 window.pop(idx)
             # maintain the sorted invariant while inserting incoming element with binary search
-            # O(logk)
-            # or x = bisect.bisect_right(window, nums[i]) and window.insert(x, nums[i])
             bisect.insort(window, nums[i])
             # find the medians starting from kth item
             if i+1 >= k:
