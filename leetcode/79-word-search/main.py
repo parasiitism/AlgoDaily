d1b60adcd8f13b24dfe026310484ba292ec45e51
calvinchankf <chan9118kin@gmail.com>
2019-05-13 13:22:43 +0800

    revise lc79

--- leetcode/79-word-search/main.py
+++ leetcode/79-word-search/main.py
@@ -30,43 +30,7 @@
 
 class Solution(object):
     def exist(self, board, word):
-        """
-        :type board: List[List[str]]
-        :type word: str
-        :rtype: bool
-        """
-        if len(word) == 0 or len(board) == 0 or len(board[0]) == 0:
-            return True
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if word[0] == board[i][j]:
-                    if self.dfs(i, j, word, board):
-                        return True
-        return False
-
-    def dfs(self, i, j, remain, board):
-        if len(remain) == 0:
-            return True
-        if i < 0 or i+1 > len(board) or j < 0 or j+1 > len(board[0]):
-            return False
-        if board[i][j] != remain[0]:
-            return False
-
-        temp = board[i][j]
-        board[i][j] = "#"
-
-        trim = remain[1:]
-        res = self.dfs(i-1, j, trim, board) or self.dfs(i+1, j, trim, board)\
-            or self.dfs(i, j-1, trim, board) or self.dfs(i, j+1, trim, board)
-
-        board[i][j] = temp
-
-        return res
-
-
-class Solution(object):
-    def exist(self, board, word):
-        if not board:
+        if len(board) == 0 or len(board[0]) == 0:
             return False
         for i in range(len(board)):
             for j in range(len(board[0])):
@@ -89,6 +53,68 @@ class Solution(object):
             or self.dfs(board, i-1, j, word[1:])\
             or self.dfs(board, i, j+1, word[1:])\
             or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = tmp
+        return res
+
+
+a = [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
+]
+print(Solution().exist(a, "ABCCED"))
+print(Solution().exist(a, "SEE"))
+print(Solution().exist(a, "ABAB"))
+
+
+a = [
+    ["A", "B", "C", "E"],
+    ["S", "F", "E", "S"],
+    ["A", "D", "E", "E"],
+]
+print(Solution().exist(a, "ABCESEEEFS"))
+
+print("-----")
+
+"""
+    1st approach B: similar to the 1st but takes up fewer space
+
+    Time    O(4^L) L: length of target word
+    Space   O(M*N + L)
+    368 ms, faster than 36.74%
+"""
+
+
+class Solution(object):
+    def exist(self, board, word):
+        if len(board) == 0 or len(board[0]) == 0:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word, 0):
+                    return True
+        return False
+
+    # check whether can find word, start at (i,j) position
+    def dfs(self, board, i, j, word, idx):
+        # if all the characters are checked
+        if len(word) == idx:
+            return True
+        # check boundaries
+        if i < 0 or i+1 > len(board) or j < 0 or j+1 > len(board[0]):
+            return False
+        # if can go forward
+        if word[idx] != board[i][j]:
+            return False
+        # first character is found, check the remaining part
+        tmp = board[i][j]
+        # avoid visit agian
+        board[i][j] = "#"
+        # check whether can find "word" along one direction
+        res = self.dfs(board, i+1, j, word, idx+1)\
+            or self.dfs(board, i-1, j, word, idx+1)\
+            or self.dfs(board, i, j+1, word, idx+1)\
+            or self.dfs(board, i, j-1, word, idx+1)
         board[i][j] = tmp
         return res
 
