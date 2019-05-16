659ab823dd6f9c8c211a6b790d190f9e4b4ad796
calvinchankf <chan9118kin@gmail.com>
2019-05-16 09:50:13 +0800

    revise lc958 comment

--- leetcode/958-check-completeness-of-a-binary-tree/main.py
+++ leetcode/958-check-completeness-of-a-binary-tree/main.py
@@ -1,3 +1,29 @@
+"""
+    2nd approach:
+	1. bfs and the queue finally should have all the leafs including null
+	2. for a complete binary tree, there should not be any node after we met a null node in the final level
+
+	e.g.1
+				1
+			2		3
+		4 5	 6
+	queue = [null, null, null, null, null, null, null]
+	parent = 3		4	 4		5	 5		6	 6
+	so it is complete
+
+	e.g.2
+				1
+			2		3
+		4 5	 		6
+	queue = [null, 6, null, null, null, null]
+	parent = 3	   3	4	4	   5	5
+
+	Time	O(n)
+	Space	O(h)
+	0 ms, faster than 100.00%
+"""
+
+
 class Solution(object):
     def isCompleteTree(self, root):
         """
