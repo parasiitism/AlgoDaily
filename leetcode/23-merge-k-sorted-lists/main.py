7f27edd5c9cd11f3a141c8c6c726e0e7b8605351
calvinchankf <chan9118kin@gmail.com>
2019-05-16 23:06:43 +0800

    revision

--- leetcode/23-merge-k-sorted-lists/main.py
+++ leetcode/23-merge-k-sorted-lists/main.py
@@ -8,6 +8,48 @@ class ListNode(object):
 
 
 """
+    2nd approach: reuse the sorted 2 lists
+
+    Time    O(nk)
+    Space   O(nk)
+	5344 ms, faster than 7.60%
+"""
+
+
+class Solution(object):
+
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        dump = ListNode(0)
+        for x in lists:
+            temp = self.merge2Lists(dump.next, x)
+            dump.next = temp
+        return dump.next
+
+    def merge2Lists(self, l1, l2):
+        cur1 = l1
+        cur2 = l2
+        dump = ListNode(0)
+        curDump = dump
+        while cur1 != None and cur2 != None:
+            if cur1.val < cur2.val:
+                curDump.next = cur1
+                cur1 = cur1.next
+            else:
+                curDump.next = cur2
+                cur2 = cur2.next
+            curDump = curDump.next
+        if cur1 != None:
+            curDump.next = cur1
+        if cur2 != None:
+            curDump.next = cur2
+        return dump.next
+
+
+"""
     3rd approach: heap (its actually=sort)
 	- iterate the list and put all the values into an array
 	- sort the array
