31a66303a1ca186a9163dd14dd3bd8dd1731259c
calvinchankf <chan9118kin@gmail.com>
2019-05-25 16:50:35 +0800

    revise lc155

--- leetcode/155-min-stack/main.py
+++ leetcode/155-min-stack/main.py
@@ -1,3 +1,14 @@
+"""
+    1st approach:
+    - add a class for StackItem
+    - for each push(), calculate the curMin and put it with the x in stack
+    - for each pop(), pop the top item
+
+    Time    O(n)
+    Space   O(n)
+"""
+
+
 class MinStack(object):
 
     def __init__(self):
@@ -50,3 +61,67 @@ class StackItem(object):
     def __init__(self, val, minVal):
         self.val = val
         self.minVal = minVal
+
+
+"""
+    2nd approach:
+    - just use tuple instead of StackItem
+    - for each push(), calculate the curMin and put it with the x in stack
+    - for each pop(), pop the top item
+
+    Time    O(n)
+    Space   O(n)
+"""
+
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        if len(self.stack) == 0:
+            self.stack.append((x, x))
+            return
+        lastMin = self.stack[-1][1]
+        self.stack.append((x, min(x, lastMin)))
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if len(self.stack) == 0:
+            return None
+        pop, lastMin = self.stack.pop()
+        return pop
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
