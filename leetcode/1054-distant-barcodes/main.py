cba447996b469a7ea7afad34d764c79a388f9e01
calvinchankf <chan9118kin@gmail.com>
2019-06-01 13:05:47 +0800

    day270 q4 s1,2

--- leetcode/1054-distant-barcodes/main.py
+++ leetcode/1054-distant-barcodes/main.py
@@ -63,7 +63,68 @@ class Solution(object):
 
 
 print(Solution().rearrangeBarcodes([1, 3, 3, 3, 4, 5]))
-# print(Solution().rearrangeBarcodes([1, 1, 1, 2, 2]))
-# print(Solution().rearrangeBarcodes([1, 1, 2, 2, 2]))
-# print(Solution().rearrangeBarcodes([1, 1, 1, 2, 2, 2]))
-# print(Solution().rearrangeBarcodes([1, 2, 2, 3, 3, 3, 4, 5]))
+print(Solution().rearrangeBarcodes([1, 1, 1, 2, 2]))
+print(Solution().rearrangeBarcodes([1, 1, 2, 2, 2]))
+print(Solution().rearrangeBarcodes([1, 1, 1, 2, 2, 2]))
+print(Solution().rearrangeBarcodes([1, 2, 2, 3, 3, 3, 4, 5]))
+
+print("-----")
+
+"""
+    2nd approach: heap + hashtable but pop 2 at a time so dont need to use a buffer
+    - similar to lc767
+
+    1. count the occurence of characters
+    2. put the characters into the maxheap
+    3. pop the maxheap and construct the result
+    4. if the pop item == res[-1], we save this to the buffer, and append the next item
+    5. after we append the next item, we put the buffer back to the heap
+    6. loop until there is no item in the heap
+    7. if there is an item in the buffer, it means that we cannot constuct the reuslt
+    8. else we just return the result
+
+    Time    O(nlogn)
+    Space   O(n)
+    552 ms, faster than 42.33%
+"""
+
+
+class Solution(object):
+    def rearrangeBarcodes(self, barcodes):
+        """
+        :type barcodes: List[int]
+        :rtype: List[int]
+        """
+        pq = []
+        ht = {}
+        for c in barcodes:
+            if c not in ht:
+                ht[c] = 1
+            else:
+                ht[c] += 1
+        for key in ht:
+            heapq.heappush(pq, (-ht[key], key))
+        # construct result by putting 2 items at a time
+        res = []
+        while len(pq) > 0:
+            ocurA, popA = heapq.heappop(pq)
+            res.append(popA)
+            if len(pq) == 0:
+                break
+            ocurB, popB = heapq.heappop(pq)
+            res.append(popB)
+            # we put back the character if occurence > 1
+            if abs(ocurA) > 1:
+                heapq.heappush(pq, (ocurA+1, popA))
+            if abs(ocurB) > 1:
+                heapq.heappush(pq, (ocurB+1, popB))
+        return res
+
+
+print(Solution().rearrangeBarcodes([1, 3, 3, 3, 4, 5]))
+print(Solution().rearrangeBarcodes([1, 1, 1, 2, 2]))
+print(Solution().rearrangeBarcodes([1, 1, 2, 2, 2]))
+print(Solution().rearrangeBarcodes([1, 1, 1, 2, 2, 2]))
+print(Solution().rearrangeBarcodes([1, 2, 2, 3, 3, 3, 4, 5]))
+
+print("-----")
--- leetcode/767-reorganize-string/main.py
+++ leetcode/767-reorganize-string/main.py
@@ -77,3 +77,74 @@ print(Solution().reorganizeString(a))
 
 a = "zrhmhyevkojpsegvwolkpystdnkyhcjrdvqtyhucxdcwmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
 print(Solution().reorganizeString(a))
+
+print("-----")
+
+"""
+    1st approach: pq + hashtable but pop 2 at a time so dont need to use a buffer
+    - similar to lc1054
+
+    1. count the occurence of characters
+    2. put the characters into the maxheap
+    3. pop the maxheap and construct the result
+    4. if the pop item == res[-1], we save this to the buffer, and append the next item
+    5. after we append the next item, we put the buffer back to the heap
+    6. loop until there is no item in the heap
+    7. if there is an item in the buffer, it means that we cannot constuct the reuslt
+    8. else we just return the result
+
+    Time    O(nlogn) becos heap for O(logn)
+    Space   O(n)
+    16 ms, faster than 98.18%
+"""
+
+
+class Solution(object):
+    def reorganizeString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        # counter
+        pq = []
+        ht = {}
+        for c in S:
+            if c not in ht:
+                ht[c] = 1
+            else:
+                ht[c] += 1
+        for key in ht:
+            heapq.heappush(pq, (-ht[key], key))
+        # construct result by putting 2 items at a time
+        res = ""
+        while len(pq) > 0:
+            ocurA, popA = heapq.heappop(pq)
+            res += popA
+            if len(pq) == 0:
+                if abs(ocurA) > 1:
+                    return ""
+                break
+            ocurB, popB = heapq.heappop(pq)
+            res += popB
+            # we put back the character if occurence > 1
+            if abs(ocurA) > 1:
+                heapq.heappush(pq, (ocurA+1, popA))
+            if abs(ocurB) > 1:
+                heapq.heappush(pq, (ocurB+1, popB))
+        return res
+
+
+print(Solution().reorganizeString("aab"))
+print(Solution().reorganizeString("aaab"))
+
+print(Solution().reorganizeString("aabc"))
+print(Solution().reorganizeString("aaabc"))
+
+print(Solution().reorganizeString("aabcc"))
+print(Solution().reorganizeString("aaaabc"))
+
+a = "zrhmhyevkojpsegvwolkpystdnkyhcjrdvqtyhucxdcwm"
+print(Solution().reorganizeString(a))
+
+a = "zrhmhyevkojpsegvwolkpystdnkyhcjrdvqtyhucxdcwmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
+print(Solution().reorganizeString(a))
