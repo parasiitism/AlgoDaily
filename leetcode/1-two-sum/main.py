e40a9487b288ff68c75d979e202e831c4250f6c6
calvinchankf <chan9118kin@gmail.com>
2019-06-02 17:42:19 +0800

    revise lc1

--- leetcode/1-two-sum/main.py
+++ leetcode/1-two-sum/main.py
@@ -23,3 +23,40 @@ class Solution(object):
             else:
                 ht[num] = i
         return []
+
+
+"""
+    followup:
+    - what if here might be no solution or more than one solution
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ht = {}
+        res = []
+        for i in range(len(nums)):
+            num = nums[i]
+            remain = target - num
+            if remain in ht:
+                res.append((ht[remain], i))
+            else:
+                ht[num] = i
+        return res
+
+
+"""
+     0  1  2  3  4  5  6  7  8  9
+    [5, 2, 3, 6, 7, 2, 4, 5, 8, 6]
+
+    target = 9, pairs = (1,3), (0,2), (5, 9)
+    target = 100, pairs = empty
+"""
+print(Solution().twoSum([5, 2, 3, 6, 7, 2, 4, 5, 8, 6], 8))
+
+print(Solution().twoSum([5, 2, 3, 6, 7, 2, 4, 5, 8, 6], 100))
