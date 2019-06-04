c7220e9e90437cf8e8e38c82676baa57cee35d00
calvinchankf <chan9118kin@gmail.com>
2019-06-04 15:03:52 +0800

    revise lc3

--- leetcode/3-longest-substring-without-repeating-characters/main.py
+++ leetcode/3-longest-substring-without-repeating-characters/main.py
@@ -7,7 +7,7 @@
 
 	Time	O(2n)
 	Space	O(n)
-    56 ms, faster than 68.14%
+    60 ms, faster than 52.92%
 """
 
 
@@ -22,17 +22,88 @@ class Solution(object):
         res = 0
         for i in range(len(s)):
             c = s[i]
+            # count with hashtable
             if c not in ht:
                 ht[c] = 1
-                res = max(res, i-slow+1)
             else:
                 ht[c] += 1
-                while ht[c] > 1:
-                    last = s[slow]
-                    ht[last] -= 1
-                    if ht[last] == 0:
-                        del ht[last]
-                    slow += 1
+            # remove the character from the left if count > 1
+            while ht[c] > 1:
+                last = s[slow]
+                ht[last] -= 1
+                if ht[last] == 0:
+                    del ht[last]
+                slow += 1
+            # update the result if necessary
+            res = max(res, i-slow+1)
+        return res
+
+
+a = "abcabcbb"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "bbbbb"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "pwwkew"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "abcdef"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "abcbdef"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "abcbdee"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "abb"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "ab"
+print(Solution().lengthOfLongestSubstring(a))
+
+a = "a"
+print(Solution().lengthOfLongestSubstring(a))
+
+print("-----")
+
+"""
+    followup: print that substring
+
+    Time	O(2n)
+	Space	O(n)
+    64 ms, faster than 46.35%
+"""
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ht = {}
+        slow = 0
+        res = ""
+        for i in range(len(s)):
+            c = s[i]
+            # count with hashtable
+            if c not in ht:
+                ht[c] = 1
+            else:
+                ht[c] += 1
+            # remove the character from the left if count > 1
+            while ht[c] > 1:
+                last = s[slow]
+                ht[last] -= 1
+                if ht[last] == 0:
+                    del ht[last]
+                slow += 1
+            # update the result if necessary
+            temp = s[slow:i+1]
+            if len(temp) > len(res):
+                res = temp
         return res
 
 
