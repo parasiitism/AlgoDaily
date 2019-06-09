2ff6e62f191e3842d3bbb325371645ccd94de2d1
calvinchankf <chan9118kin@gmail.com>
2019-06-09 11:37:28 +0800

    day278 q2 s2

--- leetcode/320-generalized-abbreviation/main.py
+++ leetcode/320-generalized-abbreviation/main.py
@@ -12,7 +12,7 @@
 
     The result should be a14c but not a23c
 
-    Time    O(2^n)
+    Time    O(n2^n) <= O(2^n): each character has 2 options(number OR to stay). O(n): find the prefix and suffix number in each iteration
     Space   O(2^n)
     900 ms, faster than 5.35%
 """
@@ -69,3 +69,73 @@ print(Solution().generateAbbreviations("word"))
 print(Solution().generateAbbreviations("calvin"))
 # 2048
 print(len(Solution().generateAbbreviations("interaction")))
+
+
+print("-----")
+
+"""
+    2nd approach: recursion similar to 1st
+    - BUT here we used # represent the count so that we dont have to deal with the corner cases like b13 -> 14
+
+    Time    O(n2^n) <= O(2^n) each character has 2 options(number OR to stay). O(n) transform a###b##c# to a3b2c1
+    Space   O(2^n)
+    924 ms, faster than 5.35%
+"""
+
+
+class Solution(object):
+
+    def __init__(self):
+        self.res = []
+        self.seen = set()
+
+    def generateAbbreviations(self, word):
+        """
+        :type word: str
+        :rtype: List[str]
+        """
+        self.recur(word)
+        return self.res
+
+    def recur(self, word):
+        # avoid redundant calculation
+        if word in self.seen:
+            return
+        self.seen.add(word)
+        # similar to subset, we need to record all paths
+        self.res.append(self.transform(word))
+        # the base case is there is no more alphabet character we can continue
+        for i in range(len(word)):
+            c = word[i]
+            if c.isdigit() == False:
+                temp = word[:i] + '#' + word[i+1:]
+                self.recur(temp)
+
+    def transform(self, s):
+        """
+        e.g. a###b##c# -> a3b2c1
+        """
+        count = 0
+        res = ""
+        for c in s:
+            if c == '#':
+                count += 1
+            else:
+                if count > 0:
+                    res += str(count)
+                    count = 0
+                res += c
+        if count > 0:
+            res += str(count)
+        return res
+
+
+print(Solution().generateAbbreviations(""))
+print(Solution().generateAbbreviations("a"))
+print(Solution().generateAbbreviations("word"))
+print(Solution().generateAbbreviations("calvin"))
+# 2048
+print(len(Solution().generateAbbreviations("interaction")))
+
+
+print("-----")
