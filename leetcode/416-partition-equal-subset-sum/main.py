e4b7c161a820fdfbe040e551e1d6e606be08f10f
calvinchankf <chan9118kin@gmail.com>
2019-06-13 15:06:30 +0800

    day282 q1 s3

--- leetcode/416-partition-equal-subset-sum/main.py
+++ leetcode/416-partition-equal-subset-sum/main.py
@@ -79,11 +79,10 @@ class Solution(object):
             return True
         if remain < 0 or len(cands) == 0 or curIdx >= len(cands):
             return False
+        # avoid redundant calculation
         key = (remain, curIdx)
-
         if key in seen:
             return seen[key]
-
         # choose or not choose
         b = self.ifCanPartition(cands, remain - cands[curIdx], curIdx + 1, seen)\
             or self.ifCanPartition(cands, remain, curIdx + 1, seen)
@@ -109,3 +108,145 @@ a = [1, 2, 3, 5]
 print(s.canPartition(a))
 
 print("-----")
+
+
+"""
+    3rd approach: top-down recursion with memoization using hashtable
+    - every number has 2 options
+    - but we can cache the result of the combinations of remainTarget and currentIdx to avoid redundant calculation
+
+    ref:
+    - https://en.wikipedia.org/wiki/Partition_problem
+    - https://www.educative.io/collection/page/5668639101419520/5633779737559040/5752754626625536
+
+    Time    O(NS) N: number of nums, S: the total sum of all the numbers
+    Space   O(h)
+    92 ms, faster than 78.84%
+"""
+
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total//2
+        seen = []
+        for i in range(len(nums)):
+            temp = (target+1) * [None]
+            seen.append(temp)
+        return self.ifCanPartition(nums, target, 0, seen)
+
+    def ifCanPartition(self, cands, remain, curIdx, seen):
+        if remain == 0:
+            return True
+        if remain < 0 or len(cands) == 0 or curIdx >= len(cands):
+            return False
+        # avoid redundant calculation
+        if seen[curIdx][remain] != None:
+            return seen[curIdx][remain]
+        # choose or not choose
+        b = self.ifCanPartition(cands, remain - cands[curIdx], curIdx + 1, seen)\
+            or self.ifCanPartition(cands, remain, curIdx + 1, seen)
+        seen[curIdx][remain] = b
+        return b
+
+
+s = Solution()
+
+a = [1, 2, 3, 4]
+print(s.canPartition(a))
+
+a = [1, 1, 3, 4, 7]
+print(s.canPartition(a))
+
+a = [2, 3, 4, 6]
+print(s.canPartition(a))
+
+a = [1, 5, 11, 5]
+print(s.canPartition(a))
+
+a = [1, 2, 3, 5]
+print(s.canPartition(a))
+
+print("-----")
+
+"""
+    4th approach: bottom-up iterative 2d array
+    - create a 2d array to store if we can add up to target by including/excluding some numbers
+    - the array should have number of row with the size of 'nums'
+    - since target = sum/2, we create row with target size of 'target'
+    - to determind dp[i][remain_sum]
+        - if dp[i-1][remain_sum] == True, it means that already add up to 'target', exclude the nums[i], 
+        i.e. dp[i][j] = dp[i-1][j]
+        - elif j >= nums[i], it means we can substract the j withe nums[i](include nums[i]), to see if later we can subtract the remain_sum down to 0, 
+        i.e.e dp[i][j] = dp[i-1][j - nums[i]]
+
+    Time    O(NS)
+    Space   O(NS)
+    1856 ms, faster than 21.08%
+"""
+
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = sum(nums)
+
+        # if 's' is a an odd number, we can't have two subsets with same total
+        if s % 2 != 0:
+            return False
+
+        # we are trying to find a subset of given numbers that has a total sum of 's/2'
+        target = s//2
+        dp = []
+        for i in range(len(nums)):
+            temp = (target+1) * [False]
+            dp.append(temp)
+
+        # populate the s=0 columns, as we can always for '0' sum with an empty set
+        # because dp[i][s-nums[i]] might equal to dp[i][0]
+        for i in range(0, len(nums)):
+            dp[i][0] = True
+
+        # first row
+        for j in range(1, target+1):
+            dp[0][j] = nums[0] == j
+
+        # process all subsets for all sums
+        for i in range(1, len(nums)):
+            for j in range(1, target+1):
+                if dp[i - 1][j] == True:
+                    # exclude nums[i]
+                    dp[i][j] = dp[i-1][j]
+                elif j >= nums[i]:
+                    # include the nums[i]
+                    dp[i][j] = dp[i-1][j - nums[i]]
+
+        # the bottom-right corner will have our answer.
+        return dp[-1][target]
+
+
+s = Solution()
+
+a = [1, 2, 3, 4]
+print(s.canPartition(a))
+
+a = [1, 1, 3, 4, 7]
+print(s.canPartition(a))
+
+a = [2, 3, 4, 6]
+print(s.canPartition(a))
+
+a = [1, 5, 11, 5]
+print(s.canPartition(a))
+
+a = [1, 2, 3, 5]
+print(s.canPartition(a))
