bc9bbd4e33e091b56edf3605d0803841fb420ec9
calvinchankf <chan9118kin@gmail.com>
2019-06-15 15:46:06 +0800

    mth to the last

---
+++ interviews/jpmorgan/mth-to-the-last-element/main.py
@@ -0,0 +1,34 @@
+def mth2last(nums):
+    if len(nums) == 0:
+        return ''
+    n = len(nums)
+    m = int(nums[-1])
+    if 0 <= n-m-1 < len(nums):
+        return nums[n-m-1]
+    return ''
+
+
+a = [100, 12, 33, 44, 55, 3]
+print(mth2last(a))
+
+a = []
+print(mth2last(a))
+
+a = [100, 12, 33, 44, 55, 5]
+print(mth2last(a))
+
+a = [100, 12, 33, 44, 55, 6]
+print(mth2last(a))
+
+print("-----")
+
+# input() reads a string with a line of input, stripping the ' ' (newline) at the end.
+# This is all you need for most Code Jam problems.
+# read input to know N
+t = int(input())
+# read N lines
+for i in range(t):
+    ri = input()  # raw_input() for python2.7
+    nums = ri.split()
+    result = mth2last(nums)
+    print(result)
