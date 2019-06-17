87f6a691122e80c0c966eea577433da70b60addf
calvinchankf <chan9118kin@gmail.com>
2019-06-17 12:15:34 +0800

    revise 2sum closest

--- interviews/amazon/closest-2-sum/2arr.py
+++ interviews/amazon/closest-2-sum/2arr.py
@@ -13,13 +13,23 @@
     forwardRouteList = [[1, 8], [2, 15], [3, 9]]
     returnRouteList = [[1, 8], [2, 11], [3, 12]]
     return [[1, 3], [3, 2]]
+
+    e.g.3
+    maxTravelDist = 20
+    forwardRouteList = [[1, 8], [2, 15], [3, 9], [4, 8]]
+    returnRouteList = [[1, 8], [2, 11], [3, 12], [4, 12]]
+    return [[1, 3], [3, 2], [1, 4], [4, 3], [4, 4]]
   
     Questions to ask:
     - will there be 2 or more pairs have the same sum? yes
+    - on each array, will there be more than one item have the same travel distance? maybe
     - the result must contains 2 items? what if [1,2,10] and 10, should i just return 10?
     - will there be no result?
     - all numbers in nums > 0?
     - maxCap > 0?
+
+    Time    O(mlogn) -> O(mn) because it might happen that there are duplicates travel distances in returnRoutes
+    Space   O(n)
 """
 
 
@@ -31,25 +41,33 @@ def optimalUtilization(maxTravelDist, forwardRouteList, returnRouteList):
     res = []
     best = 0
     for forward in forwardRouteList:
-        returnIdx = bsearch(returnRouteList, maxTravelDist - forward[1])
+        returnIdx = upperbsearch(returnRouteList, maxTravelDist - forward[1])
         if returnIdx < 0:
             continue
-        backward = returnRouteList[returnIdx]
-        dist = forward[1] + backward[1]
-        if dist > best:
-            # reset the intermediate result
-            best = dist
-            res = [[forward[0], backward[0]]]
-        elif dist == best:
-            res.append([forward[0], backward[0]])
+        # iterate the returnRouteList if the curNum == prevNum
+        while True:
+            backward = returnRouteList[returnIdx]
+            dist = forward[1] + backward[1]
+            if dist > best:
+                # reset the intermediate result
+                best = dist
+                res = [[forward[0], backward[0]]]
+            elif dist == best:
+                res.append([forward[0], backward[0]])
+            if returnRouteList[returnIdx-1][1] != returnRouteList[returnIdx][1]:
+                break
+            returnIdx -= 1
+
     return res
 
 
+"""
+# if no duplicate travel distance in returnRoutes, use this basic binary search
 def bsearch(routes, target):
     left = 0
     right = len(routes) - 1
     while left <= right:
-        mid = int((left + right) / 2)
+        mid = (left + right) // 2
         if routes[mid][1] < target:
             left = mid + 1
         elif routes[mid][1] > target:
@@ -57,21 +75,48 @@ def bsearch(routes, target):
         else:
             return mid
     return right
+"""
 
 
-print(
-    optimalUtilization(20, [[1, 8], [2, 7], [3, 14]],
-                       [[1, 5], [2, 10], [3, 14]]))
+def upperbsearch(routes, target):
+    left = 0
+    right = len(routes)
+    while left < right:
+        mid = (left + right) // 2
+        if target >= routes[mid][1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left - 1
 
-print(
-    optimalUtilization(20, [[1, 8], [2, 15], [3, 9]],
-                       [[1, 8], [2, 11], [3, 12]]))
+
+a = 20
+b = [[1, 8], [2, 7], [3, 14]]
+c = [[1, 5], [2, 10], [3, 14]]
+print(optimalUtilization(a, b, c))
+
+a = 20
+b = [[1, 8], [2, 15], [3, 9]]
+c = [[1, 8], [2, 11], [3, 12]]
+print(optimalUtilization(a, b, c))
+
+a = 20
+b = [[1, 8], [2, 15], [3, 9], [4, 8]]
+c = [[1, 8], [2, 11], [3, 12], [4, 12]]
+print(optimalUtilization(a, b, c))
 
 # corner cases
-print(
-    optimalUtilization(-20, [[1, 8], [2, 7], [3, 14]],
-                       [[1, 5], [2, 10], [3, 14]]))
+a = -20
+b = [[1, 8], [2, 7], [3, 14]]
+c = [[1, 5], [2, 10], [3, 14]]
+print(optimalUtilization(a, b, c))
 
-print(optimalUtilization(20, [], [[1, 5], [2, 10], [3, 14]]))
+a = 20
+b = []
+c = [[1, 5], [2, 10], [3, 14]]
+print(optimalUtilization(a, b, c))
 
-print(optimalUtilization(20, [[1, 21], [2, 22], [3, 23]], [[1, 24], [2, 25], [3, 26]]))
+a = 20
+b = [[1, 21], [2, 22], [3, 23]]
+c = [[1, 24], [2, 25], [3, 26]]
+print(optimalUtilization(a, b, c))
