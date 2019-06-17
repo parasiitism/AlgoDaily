d4862bc2bccdd7a8b1bd0223615e099d8cacb95f
calvinchankf <chan9118kin@gmail.com>
2019-06-17 13:41:39 +0800

    2sum combo followup: >=

--- interviews/amazon/2sum-combinations/main.py
+++ interviews/amazon/2sum-combinations/main.py
@@ -1,5 +1,5 @@
 """
-    Find out the total number of 2 numbers that can sum up to a value which is less than or equal to the target
+    Find out the total number of 2 numbers that can sum up to a value which is <= target
 
     Note:
     - will there be an empty array? yes
@@ -65,7 +65,7 @@ def bsearch(nums, start, target):
             left = mid + 1
         else:
             return mid
-    # to find number that no larger than target
+    # to find number that <= target
     return right
 
 
@@ -81,7 +81,7 @@ print("-----")
 
 
 """
-    follow-up: what if we want to find out all combinations?
+    follow-up1: what if we want to find out all combinations?
 
     Time    O(2^n)
     Space   O(2^n)
@@ -107,3 +107,57 @@ class Solution(object):
 
 a = [1, 3, 5, 7, 9]
 print(Solution().allCombo(a, 10))
+
+print("-----")
+
+"""
+    potential follow-up2: 
+    - find out the total number of 2 numbers that can sum up to a value which is >= target
+
+    e.g.1 [1,3,5,7,9], target=10
+
+    return 6
+
+    because
+    [1,9]
+    [3,7]
+    [3,9]
+    [5,7]
+    [5,9]
+    [7,9]
+
+    Time    O(nlogn)
+    Space   O(n)
+"""
+
+
+def twoSumCombo(nums, target):
+    nums = sorted(nums)
+    res = 0
+    for i in range(len(nums)):
+        num = nums[i]
+        idx = bsearch(nums, i+1, target-num)
+        if idx > - 0 and idx < len(nums):
+            res += len(nums) - idx
+    return res
+
+
+def bsearch(nums, start, target):
+    left = start
+    right = len(nums)-1
+    while left <= right:
+        mid = (left + right)/2
+        if target < nums[mid]:
+            right = mid - 1
+        elif target > nums[mid]:
+            left = mid + 1
+        else:
+            return mid
+    # to find number that >= target
+    return left
+
+
+a = [1, 3, 5, 7, 9]
+print(twoSumCombo(a, 10))
+print(twoSumCombo(a, 16))
+print(twoSumCombo(a, 1))
