4ec759eca17ec2fbc6f0b25daece033af3ad4890
calvinchankf <chan9118kin@gmail.com>
2019-06-23 17:50:30 +0800

    day292 q4 sql detail

--- leetcode/176-second-highest-salary/main.sql
+++ leetcode/176-second-highest-salary/main.sql
@@ -1,5 +1,6 @@
 -- 1. nest query
--- 2. limit 1, 1
+-- 2. limit a, b == Limit b Offset a
+--  http://www.sqltutorial.org/seeit/query/sql-limit/#1
 select ifnull(
     (select distinct Salary
     from Employee 
