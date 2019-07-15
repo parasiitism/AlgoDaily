21b79e8cd78c8d542edd354f29a0cd3d5244f199
calvinchankf <chan9118kin@gmail.com>
2019-07-15 10:30:00 +0800

    day313

---
+++ leetcode/241-different-ways-to-add-parentheses/main.py
@@ -0,0 +1,78 @@
+"""
+    1st approach: recursion
+    - similar to wordbreak2
+    - slice a string into 2 parts when there is a +-*, compute each part recursively
+    - in the recursive function, the base case is when string == one number
+    
+    e.g. 2*3-4*5
+    on the highest level, f(2*3-4*5), can be splitted into
+    1. f(2) * f(3-4*5) = 2 * {-17, -5}
+    2. f(2*3) - f(4*5) = ...
+    3. f(2*3-4) * f(5) = {-10, 10}
+
+    f(3-4*5) can be splitted into
+    1. f(3) - f(4*5)
+    2. f(3-4) * f(5)
+
+    f(2*3-4) can be splitted into
+    1. f(2) * f(3-4)
+    2. f(2*3) - f(4)
+
+    one sign:
+    f(4*5) = f(4) * f(5) = 20
+    f(3-4) = f(3) - f(4) = -1
+    f(2*3) = f(2) * f(3) = 6
+
+    base case:
+    f(4) = 4...etc
+
+    So imagine if both size have different possibilities
+    e.g. { a, b, c } * { d, e }
+    the final result wil be { ad, ae, bd, be, cd, ce }
+
+    ref:
+    - https://www.youtube.com/watch?v=gxYV8eZY0eQ
+    - ./idea.jpg
+
+    Time    O(2^n) n is the number of operators in the input
+    Space   O(2^n) recursion tree
+    24 ms, faster than 73.46%
+"""
+
+
+class Solution(object):
+    def diffWaysToCompute(self, s):
+        """
+        :type input: str
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(len(s)):
+            cur = s[i]
+            if cur == '+' or cur == '-' or cur == '*':
+                res1 = self.diffWaysToCompute(s[:i])
+                res2 = self.diffWaysToCompute(s[i+1:])
+                print(res1, res2)
+                for a in res1:
+                    for b in res2:
+                        if cur == '+':
+                            result.append(a+b)
+                        elif cur == '-':
+                            result.append(a-b)
+                        elif cur == '*':
+                            result.append(a*b)
+        if len(result) == 0:
+            result.append(int(s))
+        return result
+
+
+s = Solution()
+
+a = "2-1-1"
+print(s.diffWaysToCompute(a))
+
+a = '2*3-4*5'
+print(s.diffWaysToCompute(a))
+
+a = '3+4*5*3-4*5'
+print(s.diffWaysToCompute(a))
--- readme.md
+++ readme.md
@@ -853,3 +853,4 @@ tree | [leetcode 889](https://leetcode.com/problems/serialize-and-deserialize-bi
 | 310 | [Verify Preorder Serialization of a Binary Tree](/leetcode/331-verify-preorder-serialization-of-a-binary-tree) | stack | [leetcode 331](https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/) | 📌📌📌stack technique |
 | 311 | [Word Subsets](/leetcode/916-word-subsets) | hashtable | [leetcode 916](https://leetcode.com/problems/word-subsets/) | ⭐️think twice |
 | 312 | [Largest Time for Given Digits](/leetcode/949-largest-time-for-given-digits) | recursion | [leetcode 949](https://leetcode.com/problems/largest-time-for-given-digits/) | permutation |
+| 313 | [Different Ways to Add Parentheses](/leetcode/241-different-ways-to-add-parentheses) | recursion | [leetcode 241](https://leetcode.com/problems/different-ways-to-add-parentheses/) | 📌📌📌need to revise again^3 |
