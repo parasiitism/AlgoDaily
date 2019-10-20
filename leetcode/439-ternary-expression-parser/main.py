aeffb395fb6738b1f93ecd143224aa6294319786
calvinchankf <chan9118kin@gmail.com>
2019-10-20 11:49:54 +0800

    day411

--- leetcode/439-ternary-expression-parser/main.py
+++ leetcode/439-ternary-expression-parser/main.py
@@ -1,31 +1,43 @@
+"""
+    1st: stack
+    - reversely traverse the input string
+    - ignore the ':', pile up the stack with digits(+T/F sometimes)
+    - whenever we see a '?'
+        - if it is a 'T', pop the 2nd top item from the stack
+        - if it is a 'F', pop the 1st top item from the stack
+    - the result is the 1st top item from the stack after the iteration
+
+    ref:
+    - ./idea.png
+
+    Time    O(n)
+    Space   O(n)
+    40 ms, faster than 79.17%
+"""
+
+
 class Solution(object):
     def parseTernary(self, expression):
         """
         :type expression: str
         :rtype: str
         """
-        q = [x for x in expression]
         stack = []
-        while len(q) > 0:
-            d = q.pop(0)
-            if d == '?':
-                d = q.pop(0)
-                stack.append(d)
-            elif d == ':':
-                d = q.pop(0)
-                beenFalse = False
-                while len(stack) > 1 and stack[-2] == 'F':
+        i = len(expression)-1
+        while i >= 0:
+            d = expression[i]
+            if d == ':':
+                pass
+            elif d == '?':
+                i -= 1
+                prev = expression[i]
+                if prev == 'T':
+                    stack.pop(-2)
+                else:
                     stack.pop()
-                    beenFalse = True
-                if beenFalse:
-                    stack[-1] = d
-                if len(stack) > 1 and stack[-2] == 'T':
-                    return stack[-1]
-                if beenFalse == False:
-                    stack.append(d)
             else:
                 stack.append(d)
-
+            i -= 1
         return stack[-1]
 
 
@@ -52,7 +64,10 @@ a = 'F?F?F:5:3'
 print(s.parseTernary(a))  # 3
 
 a = 'F?T?F:5:3'
-print(s.parseTernary(a))  # 3 ❌
+print(s.parseTernary(a))  # 3
 
 a = 'T?F?T:5:3'
 print(s.parseTernary(a))  # 5
+
+a = "F?T:F?T?1:2:F?3:4"
+print(s.parseTernary(a))  # 4
