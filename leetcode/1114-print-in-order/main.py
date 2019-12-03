600f71dcd03762a442c0bb9dac0fa78c7b4e5ce1
Calvin Chan <chan9118kin@gmail.com>
2019-12-03 10:41:35 +0800

    Revise day314 452 453 454 (#5)
    * day456
    
    * revise day314,452,453,454

--- leetcode/1114-print-in-order/main.py
+++ leetcode/1114-print-in-order/main.py
@@ -1,8 +1,74 @@
-from threading import Lock
-from threading import Semaphore
+from threading import Lock, Semaphore
 
 """
-    1st approach: use Semaphore to deal with concurrency
+    python threading tutorial
+    - https://www.laurentluce.com/posts/python-threads-synchronization-locks-rlocks-semaphores-conditions-events-and-queues/
+"""
+
+
+"""
+    1st: use Mutex Lock to deal with concurrency
+    20 ms, faster than 99.13%
+"""
+
+
+class Foo(object):
+    def __init__(self):
+        self.lock1 = Lock()
+        self.lock2 = Lock()
+        self.lock1.acquire()
+        self.lock2.acquire()
+
+    def first(self, printFirst):
+        printFirst()
+        self.lock1.release()
+
+    def second(self, printSecond):
+        self.lock1.acquire()
+        printSecond()
+        self.lock1.release()
+        self.lock2.release()
+
+    def third(self, printThird):
+        self.lock2.acquire()
+        printThird()
+        self.lock2.release()
+
+
+"""
+    2nd approach: use Mutex Lock to deal with concurrency
+    - use with to minimize the codebase
+"""
+
+
+class Foo(object):
+    def __init__(self):
+        self.lock1 = Lock()
+        self.lock2 = Lock()
+        self.lock1.acquire()
+        self.lock2.acquire()
+
+    def first(self, printFirst):
+        printFirst()
+        self.lock1.release()
+
+    def second(self, printSecond):
+        with self.lock1:
+            printSecond()
+            self.lock2.release()
+
+    def third(self, printThird):
+        with self.lock2:
+            printThird()
+
+
+"""
+    3rd: use Semaphore to deal with concurrency
+
+    keep in mind:
+    - Semaphore(x) where x is capacity
+    - semaphore.acquire() means locks -= 1
+    - semaphore.release() means locks += 1
 """
 
 
@@ -25,30 +91,4 @@ class Foo(object):
     def third(self, printThird):
         # printThird() outputs "third". Do not change or remove this line.
         with self.three:
-            printThird()
-
-
-"""
-    2nd approach: use Mutex Lock to deal with concurrency
-"""
-
-
-class Foo(object):
-    def __init__(self):
-        self.lock1 = Lock()
-        self.lock2 = Lock()
-        self.lock1.acquire()
-        self.lock2.acquire()
-
-    def first(self, printFirst):
-        printFirst()
-        self.lock1.release()
-
-    def second(self, printSecond):
-        with self.lock1:
-            printSecond()
-            self.lock2.release()
-
-    def third(self, printThird):
-        with self.lock2:
             printThird()
