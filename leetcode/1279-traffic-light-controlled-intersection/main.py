e8c1dd5c6eb55f5fd3d06aca7f3ec52d474e87a0
Calvin Chan <chan9118kin@gmail.com>
2019-12-13 06:37:07 +0800

    Day466 (#11)
    * day461
    
    * day462
    
    * day463 s1,2
    
    * day464 s1,s2
    
    * day465
    
    * day466
    
    * revise lc1279

---
+++ leetcode/1279-traffic-light-controlled-intersection/main.py
@@ -0,0 +1,60 @@
+from threading import Lock
+
+"""
+    1st: make the program single-thread
+    36 ms, faster than 85.71%
+"""
+
+
+class TrafficLight(object):
+
+    def __init__(self):
+        self.currentIsA = 1
+        self.lock = Lock()
+
+    def carArrived(self, carId, roadId, direction, turnGreen, crossCar):
+        """
+        :type roadId: int --> // ID of the car
+        :type carId: int --> // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        :type direction: int --> // Direction of the car
+        :type turnGreen: method --> // Use turnGreen() to turn light to green on current road
+        :type crossCar: method --> // Use crossCar() to make car cross the intersection
+        :rtype: void
+        """
+        if roadId != self.currentIsA:
+            turnGreen()
+            self.currentIsA = roadId
+        with self.lock:
+            crossCar()
+
+
+"""
+    2nd: make the program single-thread
+    - dont use with, use try catch finally
+    36 ms, faster than 85.71%
+"""
+
+
+class TrafficLight(object):
+
+    def __init__(self):
+        self.currentIsA = 1
+        self.lock = Lock()
+
+    def carArrived(self, carId, roadId, direction, turnGreen, crossCar):
+        """
+        :type roadId: int --> // ID of the car
+        :type carId: int --> // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        :type direction: int --> // Direction of the car
+        :type turnGreen: method --> // Use turnGreen() to turn light to green on current road
+        :type crossCar: method --> // Use crossCar() to make car cross the intersection
+        :rtype: void
+        """
+        if roadId != self.currentIsA:
+            turnGreen()
+            self.currentIsA = roadId
+        try:
+            self.lock.acquire()
+            crossCar()
+        finally:
+            self.lock.release()
