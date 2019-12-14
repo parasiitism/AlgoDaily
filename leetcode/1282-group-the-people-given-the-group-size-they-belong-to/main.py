7edf4a84a382ceb7139bded47260c01afb6e4d5c
calvinchankf <chan9118kin@gmail.com>
2019-12-14 09:34:30 +0800

    day468 s2

--- leetcode/1282-group-the-people-given-the-group-size-they-belong-to/main.py
+++ leetcode/1282-group-the-people-given-the-group-size-they-belong-to/main.py
@@ -35,3 +35,40 @@ class Solution(object):
             for i in range(0, len(arr), count):
                 res.append(arr[i:i+count])
         return res
+
+
+"""
+    2nd: hashtable
+    - same logic as 1st but optimize it by using one less iteration
+    - group the indices by group size
+    - iterate each array and put them in a same group with corresponding group size
+    
+    e.g. [2,1,3,3,3,2,3,3,3]
+    1: [1]
+    2: [0,5]
+    3: [2,3,4,6,7,8]
+    
+    result:
+    [[1],[0,5],[2,3,4],[6,7,8]]
+
+    Time    O(N)
+    Space   O(N)
+    52 ms, faster than 99.50%
+"""
+
+
+class Solution(object):
+    def groupThePeople(self, groupSizes):
+        """
+        :type groupSizes: List[int]
+        :rtype: List[List[int]]
+        """
+        ht = defaultdict(list)
+        res = []
+        for i in range(len(groupSizes)):
+            count = groupSizes[i]
+            ht[count].append(i)
+            if len(ht[count]) == count:
+                res.append(ht[count])
+                ht[count] = []
+        return res
