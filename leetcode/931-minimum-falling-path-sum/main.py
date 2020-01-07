d5b8ba49fccb009689e0f52badd7db77ad0d02b2
calvinchankf <chan9118kin@gmail.com>
2020-01-07 23:19:14 +0800

    day493 s2

--- leetcode/931-minimum-falling-path-sum/main.py
+++ leetcode/931-minimum-falling-path-sum/main.py
@@ -10,6 +10,8 @@ import sys
     Space   O(RC)
     160 ms, faster than 9.52%
 """
+
+
 class Solution(object):
     def minFallingPathSum(self, A):
         """
@@ -21,23 +23,53 @@ class Solution(object):
         for j in range(len(A[0])):
             cost = min(cost, self.dfs(A, 0, j))
         return cost
-    
+
     def dfs(self, A, i, j):
         if i + 1 == len(A):
             return A[i][j]
         if (i, j) in self.ht:
             return self.ht[(i, j)]
-        
+
         a, b, c = sys.maxsize, sys.maxsize, sys.maxsize
-        
+
         if j - 1 >= 0:
             a = self.dfs(A, i+1, j-1) + A[i][j]
-        
+
         b = self.dfs(A, i+1, j) + A[i][j]
-        
+
         if j + 1 < len(A[0]):
             c = self.dfs(A, i+1, j+1) + A[i][j]
-        
+
         cost = min(a, b, c)
         self.ht[(i, j)] = cost
         return cost
+
+
+"""
+    2nd: dynamic programming, bottom up
+    - storing the minimum running sum from bottom of the matrix
+    Time    O(RCC)
+    Space   O(RC)
+    100 ms, faster than 62.54%
+"""
+
+
+class Solution(object):
+
+    def minFallingPathSum(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        for i in range(len(A)-2, -1, -1):
+            for j in range(len(A[0])):
+                best = A[i+1][j]
+                if j - 1 >= 0:
+                    best = min(best, A[i+1][j-1])
+                if j + 1 < len(A[0]):
+                    best = min(best, A[i+1][j+1])
+                A[i][j] += best
+        res = sys.maxsize
+        for j in range(len(A[0])):
+            res = min(res, A[0][j])
+        return res
