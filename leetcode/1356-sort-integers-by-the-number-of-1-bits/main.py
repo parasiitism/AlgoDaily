12897763ca693a926a9014da3a91d2a12596b69c
calvinchankf <chan9118kin@gmail.com>
2020-02-27 08:54:56 +0800

    day545 s2

--- leetcode/1356-sort-integers-by-the-number-of-1-bits/main.py
+++ leetcode/1356-sort-integers-by-the-number-of-1-bits/main.py
@@ -3,7 +3,7 @@
     1. count the number of bits for each number from input
     2. sort the input
 
-    Time    O(NlogN * logN)
+    Time    O(NlogN * logX)
     Space   O(1)
     424 ms, faster than 5.26%
 """
@@ -29,3 +29,24 @@ class Solution(object):
             return f(a) - f(b)
 
         return sorted(arr, cmp=cmpter)
+
+
+"""
+    2nd: sort with built-in function
+    - bin(x)                to get the binary representation of x
+    - string.count(target)  to get the count of a string in a string
+    - the result must be in ascending order becos python sorting does the work
+
+    Time    O(NlogN * logX)
+    Space   O(1)
+    48 ms, faster than 96.43% 
+"""
+
+
+class Solution(object):
+    def sortByBits(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        return sorted(A, key=lambda a: [bin(a).count('1'), a])
