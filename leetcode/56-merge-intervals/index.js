a9b2b2d8d4efc72c178a8b18970c94b682fe7c14
calvinchankf <chan9118kin@gmail.com>
2020-04-11 15:41:53 +0800

    review lc56

---
+++ leetcode/56-merge-intervals/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+	if (intervals.length == 0) {
+		return [];
+	}
+	intervals = intervals.sort((a, b) => {
+		if (a[0] == b[0]) {
+			return a[1] - b[1];
+		}
+		return a[0] - b[0];
+	});
+	const res = [intervals[0]];
+	for (let i = 0; i < intervals.length; i++) {
+		const lastIdx = res.length - 1;
+		intv = intervals[i];
+		if (intv[0] <= res[lastIdx][1]) {
+			res[lastIdx][1] = Math.max(res[lastIdx][1], intv[1]);
+		} else {
+			res.push(intv);
+		}
+	}
+	return res;
+};
