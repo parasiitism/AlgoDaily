63318de170350ecdaac6898e13a44e4b4e04cca3
Calvin Chan <chan9118kin@gmail.com>
2020-05-28 18:45:22 +0800

    Revert "day651 (#132)" (#133)
    This reverts commit 1c582d13006bc4f7b22a6fa9184f5630712a6706.

--- leetcode/915-partition-array-into-disjoint-intervals/main.py
+++ leetcode/915-partition-array-into-disjoint-intervals/main.py
@@ -1,6 +1,5 @@
 """
-    1st approach: min and max 2 arrays
-    - similar to lc135
+    1st approach: min and max
     - from the front to the end, store the max at each index
     - from the end to the front, store the min at each index
     - when max[i-1] <= max[i] <= min[i+1], it is the target index of the last item in the winter period
@@ -59,20 +58,6 @@ print("-----")
     - from the front to the end, store the max at each index
     - from the end to the front, store the min at each index
     - when max[i-1] <= min[i], it is the target index of the last item in the winter period
-
-    e.g. 1
-            5, 0, 3, 8, 6
-    max ->  5, 5, 5, 8, 8
-    min <-  3, 3, 3, 6, 6
-                    ^
-                    this is the partition point
-
-    e.g. 2
-            1, 1, 1, 0, 6, 12
-    max ->  1, 1, 1, 1, 6, 12
-    min <-  0, 0, 0, 0, 6, 12
-                       ^
-                       this is the partition point
 
     Time    O(3n)
     Space   O(2n)
