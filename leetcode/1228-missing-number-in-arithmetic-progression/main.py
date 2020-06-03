eeee1446e3028877ab5facb7519c39b8d169db0d
Calvin Chan <chan9118kin@gmail.com>
2020-06-03 09:23:27 +0800

    day639 (#119)

---
+++ leetcode/1228-missing-number-in-arithmetic-progression/main.py
@@ -0,0 +1,21 @@
+"""
+    1st: math
+
+    Time    O(N)
+    Space   O(N)
+    60 ms, faster than 14.07%
+"""
+
+
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        minDiff = sys.maxsize
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i-1]
+            if abs(diff) < abs(minDiff):
+                minDiff = diff
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i-1]
+            if abs(diff) != abs(minDiff):
+                return arr[i-1] + minDiff
+        return 0
