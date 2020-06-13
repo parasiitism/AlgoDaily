996bbcb529867bf6b288e4023bbbb6883630a20c
calvinchankf <chan9118kin@gmail.com>
2020-06-13 11:11:21 +0800

    revise lc380

---
+++ leetcode/380-insert-delete-getrandom-o1/index.js
@@ -0,0 +1,73 @@
+/*
+    hashtable + array, learned from others
+	- save value: index in hashtable
+	- when delete, swap the target item and the last item in the array, and remove the last item
+    - see ./idea_add.png and ./idea_remove.png
+    
+    Insert Time				O(1)
+	Remove Time				O(1)
+	GetRandom Time		    O(1)
+	Space					O(n) the unique keys
+    120 ms beats 99.01%
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+var RandomizedSet = function () {
+	this.arr = [];
+	this.ht = {};
+};
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function (val) {
+	if (val in this.ht) {
+		return false;
+	}
+	const n = this.arr.length;
+	this.ht[val] = n;
+	this.arr.push(val);
+	return true;
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function (val) {
+	if (val in this.ht) {
+		const targetIdx = this.ht[val];
+		const lastIdx = this.arr.length - 1;
+		const lastItem = this.arr[lastIdx];
+
+		// swap
+		[this.arr[targetIdx], this.arr[lastIdx]] = [
+			this.arr[lastIdx],
+			this.arr[targetIdx],
+		];
+		// assign new idx to swapped key which was at the end of array
+		this.ht[lastItem] = targetIdx;
+
+		// clear the shits
+		delete this.ht[val];
+		this.arr.pop();
+
+		return true;
+	}
+	return false;
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function () {
+	const n = this.arr.length;
+	const idx = Math.floor(Math.random() * n);
+	return this.arr[idx];
+};
--- leetcode/380-insert-delete-getrandom-o1/main.py
+++ leetcode/380-insert-delete-getrandom-o1/main.py
@@ -1,44 +1,68 @@
+import random
+
 """
-    learned from others
-	2nd approach: hashtable + array
+    1st: hashtable
+    - straight forward approach
+
+    Insert Time				O(1)
+	Remove Time				O(1)
+	GetRandom Time		    O(N)
+	Space					O(n) the unique keys
+    256 ms, faster than 29.35% 
+"""
+
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.ht = {}
+
+    def insert(self, val):
+        if val in self.ht:
+            return False
+        self.ht[val] = True
+        return True
+
+    def remove(self, val):
+        if val not in self.ht:
+            return False
+        del self.ht[val]
+        return True
+
+    def getRandom(self):
+        idx = random.randint(0, len(self.ht)-1)
+        nums = self.ht.keys()
+        return nums[idx]
+
+
+"""
+	2nd approach: hashtable + array, learned from others
 	- save value: index in hashtable
 	- when delete, swap the target item and the last item in the array, and remove the last item
 	- see ./idea_add.png and ./idea_remove.png
 
 	Insert Time				O(1)
 	Remove Time				O(1)
-	GetRandom Time		O(1)
-	Space							O(n) the unique keys
+	GetRandom Time		    O(1)
+	Space					O(n) the unique keys
 	92 ms, faster than 78.93%
 """
+
+
 class RandomizedSet(object):
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.ht = {}
         self.nums = []
 
     def insert(self, val):
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        :type val: int
-        :rtype: bool
-        """
         if val in self.ht:
             return False
         self.ht[val] = len(self.nums)
         self.nums.append(val)
         return True
-        
 
     def remove(self, val):
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        :type val: int
-        :rtype: bool
-        """
         if val not in self.ht:
             return False
         idx = self.ht[val]
@@ -48,12 +72,7 @@ class RandomizedSet(object):
         self.nums.pop()
         del self.ht[val]
         return True
-        
 
     def getRandom(self):
-        """
-        Get a random element from the set.
-        :rtype: int
-        """
         idx = random.randint(0, len(self.nums)-1)
         return self.nums[idx]
