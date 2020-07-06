e4a553e3e6d85ea0b9745c379c73d25cdee2c6d8
calvinchankf <chan9118kin@gmail.com>
2020-07-06 16:26:53 +0800

    review

--- leetcode/66-plus-one/index.js
+++ leetcode/66-plus-one/index.js
@@ -10,14 +10,15 @@
     52 ms, faster than 82.83%
  */
 var plusOne = function (digits) {
-    carry = 1
-    for (let i = digits.length - 1; i >= 0; i--) {
-        const temp = digits[i] + carry
-        digits[i] = temp % 10
-        carry = Math.floor(temp / 10)
-    }
-    if (carry > 0) {
-        digits.splice(0, 0, carry)
-    }
-    return digits
+	let carry = 1;
+	const n = digits.length;
+	for (let i = n - 1; i >= 0; i--) {
+		const temp = digits[i] + carry;
+		digits[i] = temp % 10;
+		carry = Math.floor(temp / 10);
+	}
+	if (carry > 0) {
+		digits = [carry, ...digits];
+	}
+	return digits;
 };
