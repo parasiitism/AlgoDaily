b8ae4f80cf162681aaff1ff8ed6e1e4d05f2010d
calvinchankf <chan9118kin@gmail.com>
2020-07-16 21:27:55 +0800

    review

--- leetcode/50-powx-n/index.js
+++ leetcode/50-powx-n/index.js
@@ -1,28 +1,94 @@
 /*
-    3rd approach: dynamic programming
+    classic approach: math recursively
     - split the n (divide and conquer)
 
-               2^10
-            2^5 * 2^5
-        2^2 * 2^3 * 2^2 * 2^3
-    2^2 * 2^2 * 2 * 2^2 * 2^2 * 2
+            2^10
+        (2*2=4) ^5
+    (4 * 4) ^ 2 * 4 
 
     Time    O(logn)
     Space   O(logn) recursion tree
-    20 ms, faster than 96.88%
+    56 ms, faster than 98.58%
+*/
+const myPow = (x, n) => {
+	const N = Math.abs(n);
+	if (n >= 0) {
+		return f(x, N);
+	}
+	return 1 / f(x, N);
+};
+
+const f = (x, n) => {
+	if (n === 0) {
+		return 1;
+	}
+	const half = f(x, Math.floor(n / 2));
+	if (n % 2 == 0) {
+		return half * half;
+	}
+	return half * half * x;
+};
+
+/*
+    classic approach 2: math recursively
+    - split the n (divide and conquer)
+
+            2^10
+        (2*2=4) ^5
+    (4 * 4) ^ 2 * 4 
+
+    Time    O(logn)
+    Space   O(logn) recursion tree
+    104 ms, faster than 17.12% 
 */
 var myPow = function (x, n) {
 	if (n === 0) {
 		return 1;
 	}
 
-	if (Math.abs(n) === 1) {
-		return n > 0 ? x : 1 / x;
+	if (n === 1) {
+		return x;
+	} else if (n === -1) {
+		return 1 / x;
 	}
 
 	if (n % 2 === 0) {
-		return myPow(x * x, n / 2);
+		return myPow(x * x, Math.floor(n / 2));
 	} else {
 		return myPow(x * x, Math.floor(n / 2)) * x;
 	}
+};
+
+/*
+    classic approach: math recursively
+    - split the n (divide and conquer)
+
+            2^10
+        (2*2=4) ^5
+    (4 * 4) ^ 2 * 4 
+
+    Time    O(logn)
+    Space   O(logn) recursion tree
+    92 ms, faster than 26.11%
+*/
+var myPow = function (x, n) {
+	if (n == 0) {
+		return 1;
+	}
+
+	let N = Math.abs(n);
+
+	const mods = [];
+	while (N > 1) {
+		if (N % 2 == 1) {
+			mods.push(x);
+		}
+		x = x * x;
+		N = Math.floor(N / 2);
+	}
+	for (let m of mods) {
+		x *= m;
+	}
+
+	return n > 0 ? x : 1 / x;
 };
