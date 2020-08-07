12f2820db66370ae6015062ee53fcf7f6ee4e08a
Calvin Chan <chan9118kin@gmail.com>
2020-08-08 01:25:03 +0800

    Day708 (#192)
    * day701
    
    * review
    
    * review
    
    * day702 readme
    
    * day703
    
    * day704
    
    * day705
    
    * day706
    
    * time
    
    * day707 readme
    
    * day708

---
+++ leetcode/1522-diameter-of-n-ary-tree/index.js
@@ -0,0 +1,40 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.children = children === undefined ? [] : children;
+ * };
+ */
+
+/*
+    1st: recursion
+    - lc543, lc414
+
+    84 ms, faster than 100.00%
+*/
+const diameter = (root) => {
+	let res = 0;
+
+	const dfs = (node) => {
+		if (node == null) {
+			return 0;
+		}
+		let largest = 0;
+		let secondLargest = 0;
+		for (let child of node.children) {
+			let depth = dfs(child);
+			if (depth > largest) {
+				secondLargest = largest;
+				largest = depth;
+			} else if (depth > secondLargest) {
+				secondLargest = depth;
+			}
+		}
+		let total = largest + secondLargest;
+		res = Math.max(res, total);
+		return largest + 1;
+	};
+	dfs(root);
+
+	return res;
+};
