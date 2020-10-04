4a5ff38b068c1a5d61583b095812547320c23c44
calvinchankf <chan9118kin@gmail.com>
2020-10-04 20:29:17 +0800

    review

--- leetcode/1288-remove-covered-intervals/main.py
+++ leetcode/1288-remove-covered-intervals/main.py
@@ -30,3 +30,34 @@ class Solution(object):
                 if cur[1] > merged[-1][1]:
                     merged.append(cur)
         return len(merged)
+
+"""
+    2nd: sort
+    - but by descending end time
+
+    Time    O(NlogN)
+    Space   O(N)
+    92 ms, faster than 50.86%
+"""
+class Solution(object):
+    def removeCoveredIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        if len(intervals) == 0:
+            return 0
+        def cmpter(a, b):
+            if a[0] == b[0]:
+                return b[1] - a[1]
+            return a[0] - b[0]
+        intervals.sort(cmp=cmpter)
+        mergeds = [intervals[0]]
+        for i in range(1, len(intervals)):
+            s, e = intervals[i]
+            if s < mergeds[-1][1]:
+                if e > mergeds[-1][1]:
+                    mergeds.append([s, e])
+            else:
+                mergeds.append([s, e])
+        return len(mergeds)
