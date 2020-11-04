fcd4328f920bfa6f6977c499b4c341ad6c37eea7
calvinchankf <chan9118kin@gmail.com>
2020-11-04 10:11:18 +0800

    review

--- leetcode/1446-consecutive-characters/main.py
+++ leetcode/1446-consecutive-characters/main.py
@@ -7,6 +7,9 @@
 """
 
 
+from collections import Counter
+
+
 class Solution:
     def maxPower(self, s: str) -> int:
         maxCount = 0
@@ -28,7 +31,7 @@ class Solution:
 
     Time    O(N)
     Space   O(1)
-    44 ms, faster than 25.00%
+    28 ms, faster than 81.69%
 """
 
 
@@ -43,3 +46,34 @@ class Solution:
                 curCount = 1
             maxCount = max(maxCount, curCount)
         return maxCount
+
+
+"""
+    3rd: 2pointers + hashtable
+
+    Time    O(N)
+    Space   O(N)
+    124 ms, faster than 5.63%
+"""
+
+
+class Solution(object):
+    def maxPower(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ht = Counter()
+        j = 0
+        res = 0
+        for i in range(len(s)):
+            c = s[i]
+            ht[c] += 1
+            while len(ht) > 1:
+                left = s[j]
+                j += 1
+                ht[left] -= 1
+                if ht[left] == 0:
+                    del ht[left]
+            res = max(res, i-j+1)
+        return res
