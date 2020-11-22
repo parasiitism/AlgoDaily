6580894e2477338531ad6d93245c82551bcb4bcd
calvinchankf <chan9118kin@gmail.com>
2020-11-22 19:00:39 +0800

    review

--- leetcode/1-two-sum/main.py
+++ leetcode/1-two-sum/main.py
@@ -109,7 +109,7 @@ class Solution(object):
 
 
 """
-    followup:
+    followup1:
     - what if here might be no solution or more than one solution
     - no duplicates, still
 
@@ -147,192 +147,51 @@ print(Solution().twoSum([5, 2, 3, 6, 7, 12, 4, 15, 8, 1], 8))
 print(Solution().twoSum([5, 2, 3, 6, 7, 12, 4, 15, 8, 1], 100))
 print("-----")
 
-
 """
-    2nd approach: binary search
+    followup2: every number can be used once, print the indices
 
-    e.g.
-     0  1  2  3  4   5  6   7  8  9
-    [5, 2, 3, 6, 7, 12, 4, 15, 8, 1]
+    e.g.1
+     0  1  2  3  4  5  6
+    [5, 2, 5, 2, 2, 3, 4] 7
+    _____  ____     ____
 
-    target = 8, pairs = (1,3), (0,2), (5, 9)
-    target = 100, pairs = empty
+    result  = [[0, 1], [2, 3], [5, 6]]
+
+    e.g.2
+     0  1  2  3  4  5  6
+    [5, 5, 2, 2, 2, 3, 4] 7
+    _____  ____     ____
+
+    result  = [[1, 2], [0, 3], [5, 6]]
+    Note that [[0, 2], [1, 3], [5, 6]] is also correct
 """
 
 
 class Solution(object):
-
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        arr = []
-        for i in range(len(nums)):
-            arr.append([nums[i], i])  # [num, index]
-        arr = sorted(arr, key=lambda x: x[0])
-        # iterate through the array and binary search the partner in remaining array
         res = []
-        for i in range(len(arr)-1):
-            remain = target - arr[i][0]
-            j = self.bsearch(arr, i+1, remain)
-            if j > -1:
-                res.append([arr[i][1], arr[j][1]])
+        ht = {}
+        for i in range(len(nums)):
+            x = nums[i]
+            remain = target - x
+            if remain in ht:
+                j = ht[remain].pop()
+                res.append([j, i])
+                if len(ht[remain]) == 0:
+                    del ht[remain]
+            else:
+                if x not in ht:
+                    ht[x] = []
+                ht[x].append(i)
         return res
 
-    def bsearch(self, nums, start, target):
-        left = start
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right)//2
-            if target < nums[mid][0]:
-                right = mid - 1
-            elif target > nums[mid][0]:
-                left = mid + 1
-            else:
-                return mid
-        return -1
 
+s = Solution()
 
-print(Solution().twoSum([5, 2, 3, 6, 7, 12, 4, 15, 8, 1], 8))
-print(Solution().twoSum([5, 2, 3, 6, 7, 12, 4, 15, 8, 1], 100))
-print("-----")
+a = [5, 2, 5, 2, 2, 3, 4]
+b = 7
+print(s.twoSum(a, b))
 
-
-"""
-    followup2:
-    - there are duplicates
-    - just give me the count
-
-    e.g.
-     0  1  2  3  4  5  6  7  8  9, 10, 11
-    [5, 2, 3, 6, 7, 2, 4, 5, 8, 6, 4, 4]
-
-    5: [0, 7]
-    2: [1, 5]
-    3: [2]
-    6: [3,9]
-    7: [4]
-    4: [6, 10, 11]
-    8: [8]
-    target = 8, pairs(idx) = (0,2), (7,2), (1,3), (1,9), (5,3), (5,9), (6,10), (6,11), (10,11) total = 9
-"""
-
-
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        ht = {}
-        count = 0
-        for i in range(len(nums)):
-            num = nums[i]
-            # cal the remain
-            remain = target - num
-            if remain in ht:
-                count += len(ht[remain])
-            # put into the hashtable
-            if num not in ht:
-                ht[num] = [i]
-            else:
-                ht[num].append(i)
-        return count
-
-
-print(Solution().twoSum([5, 2, 3, 6, 7, 2, 4, 5, 8, 6, 4, 4], 8))
-print(Solution().twoSum([5, 2, 3, 6, 7, 2, 4, 5, 8, 6], 100))
-print("-----")
-
-"""
-    or store the count
-"""
-
-
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        ht = {}
-        count = 0
-        for i in range(len(nums)):
-            num = nums[i]
-            # cal the remain
-            remain = target - num
-            if remain in ht:
-                count += ht[remain]
-            # put into the hashtable
-            if num not in ht:
-                ht[num] = 1
-            else:
-                ht[num] += 1
-        return count
-
-
-print(Solution().twoSum([5, 2, 3, 6, 7, 2, 4, 5, 8, 6, 4, 4], 8))
-print(Solution().twoSum([5, 2, 3, 6, 7, 2, 4, 5, 8, 6], 100))
-print("--yo--")
-
-"""
-    followup3:
-    - 2 arrays instead of one array
-    - no duplicate on each array
-    - all pairs(indices)
-
-    e.g.
-    a = [5, 3, 9, 8, 7, 11]
-    b = [3, 6, 0, 1, 2, 12]
-    target = 9
-
-    result = [(nums1 index, nums2 index), ....]
-    = (1, 1), (2, 2), (3, 3), (4, 4)
-
-    Time    O(nlogn)
-    Space   O(n)
-"""
-
-
-class Solution(object):
-
-    def twoSum(self, nums1, nums2, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        arr = []
-        for i in range(len(nums2)):
-            arr.append([nums2[i], i])  # [num, index]
-        arr = sorted(arr, key=lambda x: x[0])
-
-        res = []
-        for i in range(len(nums1)):
-            remain = target - nums1[i]
-            idx = self.bsearch(arr, remain)
-            if idx > -1:
-                res.append([i, arr[idx][1]])
-        return res
-
-    def bsearch(self, nums, target):
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right)//2
-            if target < nums[mid][0]:
-                right = mid - 1
-            elif target > nums[mid][0]:
-                left = mid + 1
-            else:
-                return mid
-        return -1
-
-
-a = [5, 3, 9, 8, 7, 11]
-b = [3, 6, 0, 1, 2, 12]
-print(Solution().twoSum(a, b, 9))
+a = [5, 5, 2, 2, 2, 3, 4]
+b = 7
+print(s.twoSum(a, b))
