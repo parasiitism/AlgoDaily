0fda49bfacff31198fac6152b0c4688670975709
calvinchankf <chan9118kin@gmail.com>
2021-02-17 06:38:12 +0800

    review

--- leetcode/784-letter-case-permutation/main.py
+++ leetcode/784-letter-case-permutation/main.py
@@ -11,22 +11,18 @@
 
 class Solution(object):
     def letterCasePermutation(self, S):
-        """
-        :type S: str
-        :rtype: List[str]
-        """
         self.res = []
-        self.dfs(S, "")
+        self.dfs(S, '')
         return self.res
 
-    def dfs(self, remain, s):
-        if len(remain) == 0:
-            self.res.append(s)
+    def dfs(self, S, cur):
+        if len(S) == 0:
+            self.res.append(cur)
+            return
+        c = S[0]
+        remain = S[1:]
+        if c in '0123456789':
+            self.dfs(remain, cur + c)
         else:
-            first = remain[0]
-            if (ord(first) >= 65 and ord(first) <= 90) or\
-                    (ord(first) >= 97 and ord(first) <= 122):
-                self.dfs(remain[1:], s+first.lower())
-                self.dfs(remain[1:], s+first.upper())
-            else:
-                self.dfs(remain[1:], s+first)
+            self.dfs(remain, cur + c.upper())
+            self.dfs(remain, cur + c.lower())
