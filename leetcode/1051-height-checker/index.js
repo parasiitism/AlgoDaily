02cf86b7deec50a8cf535b5dd0055c5eefd23b5a
calvinchankf <chan9118kin@gmail.com>
2021-02-22 12:06:00 +0800

    review

--- leetcode/1051-height-checker/index.js
+++ leetcode/1051-height-checker/index.js
@@ -6,14 +6,13 @@
     16 ms, faster than 95.04%
 */
 var heightChecker = function (heights) {
-	const n = heights.length
-    const clone = [...heights]
+	const clone = [...heights]
     clone.sort((a, b) => a - b)
-    let res = 0
-    for (let i = 0; i < n; i++) {
-        if (heights[i] != clone[i]) {
-            res += 1
+    let count = 0
+    for (let i = 0; i < clone.length; i++) {
+        if (clone[i] != heights[i]) {
+            count += 1
         }
     }
-    return res
+    return count
 };
--- leetcode/133-clone-graph/index.js
+++ leetcode/133-clone-graph/index.js
@@ -14,25 +14,24 @@
     Space   O(N)
     76 ms, faster than 86.98%
 */
-var cloneGraph = function (root) {
-	const ht = {};
-	return buildClone(root, ht);
+var cloneGraph = function(node) {
+    return build(node, {})
 };
 
-const buildClone = (node, ht) => {
-	if (node == null) {
-		return null;
-	}
-	if (node.val in ht) {
-		return ht[node.val];
-	}
-
-	const clone = new Node(node.val);
-	ht[node.val] = clone;
-
-	for (let nb of node.neighbors) {
-		const temp = buildClone(nb, ht);
-		clone.neighbors.push(temp);
-	}
-	return clone;
-};
+const build = (node, cache) => {
+    if (node == null) {
+        return null
+    }
+    if (node.val in cache) {
+        return cache[node.val]
+    }
+    const clone = new Node(node.val)
+    cache[node.val] = clone
+    for (let nb of node.neighbors) {
+        const temp = build(nb, cache)
+        if (temp != null) {
+            clone.neighbors.push(temp)   
+        }
+    }
+    return clone
+}
