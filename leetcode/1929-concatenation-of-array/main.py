e66288d4da1dd894811ebd3a9f696fad3aa3edba
Calvin Chan <chan9118kin@gmail.com>
2021-07-20 08:26:28 +0100

    day1054 (#540)

---
+++ leetcode/1929-concatenation-of-array/main.py
@@ -0,0 +1,12 @@
+"""
+    1st: array
+
+    Time    O(N)
+    Space   O(N)
+    132 ms, faster than 20.00%
+"""
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
