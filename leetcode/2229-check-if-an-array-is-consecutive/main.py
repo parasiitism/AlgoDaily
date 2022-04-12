1cae6cbc1b20293dc98014e3a77aa87649b80c07
Calvin Chan <chan9118kin@gmail.com>
2022-04-12 07:06:03 +0100

    day1320 (#788)

---
+++ leetcode/2229-check-if-an-array-is-consecutive/main.py
@@ -0,0 +1,42 @@
+"""
+    1st: sort
+
+    Time    O(N)
+    Space   O(1)
+    727 ms, faster than 100.00%
+"""
+
+
+class Solution:
+    def isConsecutive(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            prev = nums[i-1]
+            curr = nums[i]
+            if prev+1 != curr:
+                return False
+        return True
+
+
+"""
+    2nd: hashtable
+    - put every element in a hashset
+    - iterate from the min to see mim+i exists in the hashset
+    
+    Time    O(N)
+    Space   O(N)
+    732 ms, faster than 100.00% 
+"""
+
+
+class Solution:
+    def isConsecutive(self, nums: List[int]) -> bool:
+        n = len(nums)
+        hs = set(nums)
+        if len(hs) != n:
+            return False
+        mi = min(nums)
+        for i in range(n):
+            if mi+i not in hs:
+                return False
+        return True
