32b5e22d398d6ffb167724bd417bd5068dfac1bd
Calvin Chan <chan9118kin@gmail.com>
2022-09-12 09:33:46 +0100

    day1473 (#937)

---
+++ leetcode/2396/main.py
@@ -0,0 +1,12 @@
+"""
+    https://leetcode.com/problems/strictly-palindromic-number/discuss/2524702/return-false
+    - this is not an algo problem
+
+    Time     O(1)
+    Space    O(1) 
+"""
+
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
