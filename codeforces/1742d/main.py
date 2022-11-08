e3b45b97334945c8acf0e3c01ff87faf20cd13b0
Calvin Chan <chan9118kin@gmail.com>
2022-11-08 21:42:05 +0000

    day1531 (#990)

---
+++ codeforces/1742d/main.py
@@ -0,0 +1,78 @@
+"""
+# brute-force
+# Time O(NNlogM)    LTE
+
+def f():
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        arr = [int(c) for c in input().split()]
+        print(solve(arr))
+
+
+def solve(arr):
+    n = len(arr)
+    res = -1
+    for i in range(n):
+        for j in range(n):
+            if find_gcd(arr[i], arr[j]) == 1:
+                res = max(res, i + j)
+    if res == -1:
+        return res
+    return res + 2
+
+
+def find_gcd(a, b):
+    if b == 0:
+        return a
+    return find_gcd(b, a % b)
+
+
+f()
+"""
+
+"""
+    hashtable
+
+    Time    O(1000*1000 log1000)
+    LTE in python but would work in other language according to the tutorial
+"""
+
+
+def f():
+    T = int(input())
+    for _ in range(T):
+        input()
+        arr = [int(c) for c in input().split()]
+        print(solve(arr))
+
+
+def solve(arr):
+    n = len(arr)
+    biggests = 1001 * [-1]
+    for i in range(n):
+        x = arr[i]
+        biggests[x] = max(biggests[x], i)
+    res = -1
+    for x in range(1, 1001):
+        i = biggests[x]
+        if i == -1:
+            continue
+        for y in range(1, 1001):
+            j = biggests[y]
+            if j == -1:
+                continue
+            if find_gcd(x, y) == 1:
+                res = max(res, i + j)
+    if res == -1:
+        return res
+    return res + 2
+
+
+def find_gcd(a, b):
+    if b == 0:
+        return a
+    return find_gcd(b, a % b)
+
+
+f()
--- readme.md
+++ readme.md
@@ -104,3 +104,4 @@ const start = new Date("09/01/2018"); const now = new Date(); console.log(Math.c
 | 1528 | [Sum](/codeforces/1742a) | sort | [codeforces 1742a](https://codeforces.com/problemset/problem/174a/A) | |
 | 1529 | [Increasing](/codeforces/1742b) | sort | [codeforces 1742b](https://codeforces.com/problemset/problem/1742/B) | |
 | 1530 | [Stripes](/codeforces/1742c) | array | [codeforces 1754c](https://codeforces.com/contest/1742/problem/C) | |
+| 1531 | [Coprime](/codeforces/1742d) | hashtable | [codeforces 1742d](https://codeforces.com/problemset/problem/1742/D) | |
