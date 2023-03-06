dd822f2d5f50694ce3cff7dab1b903617a5606df
calvinchankf <chan9118kin@gmail.com>
2023-03-06 14:14:01 +0000

    update

--- glassdoor/palantir/stock-portfolio/main.py
+++ glassdoor/palantir/stock-portfolio/main.py
@@ -12,36 +12,43 @@ from heapq import *
 
     
     Input:
-    [[('8/7', 20), ('9/3', 70)],[('8/12', 50)],[('8/7', 10), ('8/12', 20), ('10/8', 90)]]
+    [
+        [('8/7', 20), ('9/3', 70)],
+        [('8/12', 50)],
+        [('8/7', 10), ('8/12', 20), ('10/8', 90)]
+    ]
     
     Output:
     [('8/7', 30), ('8/12', 90), ('9/3', 140), ('10/8', 230)]
 """
+def int2date(n):
+    mm = n // 100
+    dd = n % 100
+    return str(mm) + '/' + str(dd)
+
 def f(stocks_price_by_date):
     n = len(stocks_price_by_date)
     minheap = []
-    for ticker in range(n):
-        price_by_date = stocks_price_by_date[ticker]
+    for sid in range(n):
+        price_by_date = stocks_price_by_date[sid]
         for date, price in price_by_date:
             mm, dd = date.split('/')
-            mmd = int(m) * 100 + int(d)
-            heappush(minheap, (mmdd, ticker, price))
-    tickers_price = {i: 0 for i in range(n)}
+            mmdd = int(mm) * 100 + int(dd)
+            heappush(minheap, (mmdd, sid, price))
+    stock_prices = {i: 0 for i in range(n)}
     res = []
     current_date = None
     while len(minheap) > 0:
-        date, ticker, price = heappop(minheap)
+        date, sid, price = heappop(minheap)
         if current_date == None:
             current_date = date
         if date == current_date:
-            tickers_price[ticker] = price
+            stock_prices[sid] = price
         else:
-            # print(tickers_price)
-            res.append(sum(tickers_price.values()))
+            res.append([int2date(current_date), sum(stock_prices.values())])
             current_date = date
-            tickers_price[ticker] = price
-    # print(tickers_price)
-    res.append(sum(tickers_price.values()))
+            stock_prices[sid] = price
+    res.append([int2date(current_date), sum(stock_prices.values())])
     return res
 
 a = [[('8/7', 20), ('9/3', 70)],
