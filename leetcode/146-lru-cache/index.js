95712eb77cbce170fd0c4eb6f34189367347525d
calvinchankf <chan9118kin@gmail.com>
2023-06-30 17:26:39 +0100

    lc146

--- leetcode/146-lru-cache/index.js
+++ leetcode/146-lru-cache/index.js
@@ -64,3 +64,54 @@ class LRUCache {
         }
     }
 }
+
+/*
+    2nd: use JS built-in ES6 Map()
+    
+    Time  put:O(1), get: O(1)
+*/
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.cap = capacity
+    this.map = new Map()
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (this.map.has(key)) {
+        const val = this.map.get(key)
+        this.map.delete(key)
+        this.map.set(key, val)
+        return val
+    }
+    return -1
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if (this.map.has(key)) {
+        this.map.delete(key)
+    }
+    this.map.set(key, value)
+    if (this.map.size > this.cap) {
+        const first = this.map.keys().next().value
+        this.map.delete(first)
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
