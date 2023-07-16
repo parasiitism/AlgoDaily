9ddec5460c698cb97989a9225d0070fa71de636a
calvinchankf <chan9118kin@gmail.com>
2023-07-16 19:36:48 +0100

    update

--- miscellaneous/binary-searches/main.py
+++ miscellaneous/binary-searches/main.py
@@ -126,6 +126,11 @@ def upperBsearch(nums, target):
             left = mid + 1
         else:
             right = mid
+        # logic is the same as
+        # if target < nums[mid]:
+        #     right = mid
+        # else:
+        #     left = mid + 1
     return right
 
 
