120c1ec92fdf7a1133b74e3b0a8d5515dee70433
calvinchankf <chan9118kin@gmail.com>
2023-09-04 22:14:04 +0100

    update

--- glassdoor/palantir/debug-mult-thread-queue/thread_pool_executor.py
+++ glassdoor/palantir/debug-mult-thread-queue/thread_pool_executor.py
@@ -11,3 +11,34 @@ And then iterate and see there's any thats complete
 you'd modify the code above to, instead of calling result() on packageFuture, you'd do queue.append(packageFuture)
 You'd then iterate through the que and call result() on each of them.
 """
+import time
+from concurrent.futures import ThreadPoolExecutor, wait, as_completed
+
+
+def worker(n):
+    time.sleep(2)
+    res = 2**n
+    if n == 3:
+        raise Exception("Package not found")
+    print(f"worker at {n}")
+    return res
+
+
+# with ThreadPoolExecutor(2) as executor:
+executor = ThreadPoolExecutor(2)
+futures = []
+for i in range(1, 10):
+    f = executor.submit(worker, i)
+    futures.append(f)
+
+print('Waiting for tasks to complete...')
+wait(futures)
+print('All tasks are done!')
+
+# note: the results are not in order
+for f in as_completed(futures):
+    try:
+        result = f.result()
+        print(result)
+    except Exception as err:
+        print(err)
